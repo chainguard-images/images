6119aaec98f903cfcd728200a3b8bf0efaa06eeb
Ariadne Conill <ariadne@dereferenced.org>
2022-07-06 20:02:59 +0000

    add hello-world example

---
+++ images/hello-world/Makefile
@@ -0,0 +1,5 @@
+hello:
+	gcc -o $@ hello.c
+
+install:
+	install -Dm755 hello ${DESTDIR}/usr/bin/hello
---
+++ images/hello-world/hello.c
@@ -0,0 +1,11 @@
+/* This program is in the public domain. */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char *argv[]) {
+	(void) argc;
+	(void) argv;
+
+	printf("Hello world!\n");
+	return EXIT_SUCCESS;
+}
