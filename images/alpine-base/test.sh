e2df08c25367b934d585f913e55b0cebdedacf6a
Patrick Flynn <patrick@chainguard.dev>
2022-07-11 14:22:30 +0200

    missed smoked test update and trailing new line in dockerfile
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- images/alpine-base/test.sh
+++ images/alpine-base/test.sh
@@ -4,5 +4,4 @@ set -o errexit -o nounset -o errtrace -o pipefail
 
 IMAGE_NAME=${IMAGE_NAME:-"distroless.dev/alpine-base"}
 
-docker build --build-arg BASE=${IMAGE_NAME} --tag smoke-test --file examples/Dockerfile examples
-docker run smoke-test
+docker run ${IMAGE_NAME} echo "hello distroless"
