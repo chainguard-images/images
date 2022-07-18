eb54f62f7f2e17078100159d5de0e996956fc5e3
Josh Dolitsky <josh@dolit.ski>
2022-07-18 14:08:36 -0500

    Add smoketest
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ images/nginx/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail
+
+IMAGE_NAME=${IMAGE_NAME:-"distroless.dev/nginx"}
+CONTAINER_NAME=${CONTAINER_NAME:-"distroless-nginx-smoketest-$(date +%s)"}
+
+docker run -p 8080:80 -d --name $CONTAINER_NAME $IMAGE_NAME
+trap "docker rm -f $CONTAINER_NAME" EXIT
+
+curl -v --max-time 10 http://localhost:8080/ | grep '<title>Welcome to nginx!</title>'
