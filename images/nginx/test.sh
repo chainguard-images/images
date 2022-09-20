32ef52f7a14d4dd36bce812d103972e995f451f2
Josh Dolitsky <josh@dolit.ski>
2022-09-20 12:39:22 -0500

    Cleanup repo to reflect new GitHub org etc.
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/nginx/test.sh
+++ images/nginx/test.sh
@@ -2,8 +2,8 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail
 
-IMAGE_NAME=${IMAGE_NAME:-"distroless.dev/nginx"}
-CONTAINER_NAME=${CONTAINER_NAME:-"distroless-nginx-smoketest-$(date +%s)"}
+IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/nginx"}
+CONTAINER_NAME=${CONTAINER_NAME:-"nginx-smoketest-$(date +%s)"}
 
 docker run -p 8080:80 -d --name $CONTAINER_NAME $IMAGE_NAME
 trap "docker rm -f $CONTAINER_NAME" EXIT
