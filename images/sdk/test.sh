acfeb453f301bfdf7bdc97474e53ef1aac22de36
Josh Dolitsky <josh@dolit.ski>
2022-10-17 15:16:59 -0500

    fix test.sh
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/sdk/test.sh
+++ images/sdk/test.sh
@@ -6,6 +6,7 @@ IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/sdk"}
 
 docker run --rm --entrypoint bash "${IMAGE_NAME}" -xc \
     'export PATH="/root/go/bin:${PATH}" && \
+     command -v goimports &&
      go version &&
      make --version &&
      curl --version &&
