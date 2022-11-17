4d2aa7f0a30f06ccc54a0dc9ca8eb1234f3250b2
Josh Dolitsky <josh@dolit.ski>
2022-11-17 02:32:45 -0600

    Just a bunch of CI stuff
    - Run vuln scans on latest tag
    - Generate and upload build badge and cve badges to GCS
    - Update README format to include badges
    - Only run affected images on main push
    - Enable slack alerting via webhook (DISTROLESS_SLACK_WEBHOOK)
    - Remove .chainguard/ directory
    
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- monopod/pkg/commands/options/readme.go
+++ monopod/pkg/commands/options/readme.go
@@ -5,8 +5,12 @@ import (
 )
 
 // ReadmeOptions is the wrapper for options related to readme.
-type ReadmeOptions struct{}
+type ReadmeOptions struct {
+	BadgeRootUrl string
+}
 
 func (o *ReadmeOptions) AddFlags(cmd *cobra.Command) {
-	// cmd.Flags().StringVar( ... )
+	cmd.Flags().StringVar(&o.BadgeRootUrl, "badge-root-url",
+		"https://storage.googleapis.com/chainguard-images-build-outputs/badges",
+		"root URL to obtain badges")
 }
