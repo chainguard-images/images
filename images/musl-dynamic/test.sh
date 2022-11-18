9dcc092a57f016b967d488d5d921232f996dfc75
cpanato <ctadeu@gmail.com>
2022-11-18 20:36:26 +0100

    migrate musl-dynamic
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- images/musl-dynamic/test.sh
+++ images/musl-dynamic/test.sh
@@ -2,7 +2,11 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail
 
-IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/musl-dynamic"}
+IMAGE_NAME=${IMAGE_NAME:-"unset"}
+if [[ "${IMAGE_NAME}" == "unset" ]]; then
+    echo "Must set IMAGE_NAME in the environment. Exiting."
+    exit 1
+fi
 
 docker build --build-arg BASE=${IMAGE_NAME} --tag smoke-test --file examples/Dockerfile.c examples
 docker run smoke-test
