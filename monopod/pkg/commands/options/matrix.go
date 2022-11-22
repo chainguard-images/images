0fb025049ae980dd3411b1347c339367c3fc70f9
Josh Dolitsky <josh@dolit.ski>
2022-11-22 14:00:05 -0600

    Link badges to registry UI, optional melange builds on PR
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- monopod/pkg/commands/options/matrix.go
+++ monopod/pkg/commands/options/matrix.go
@@ -7,9 +7,12 @@ import (
 // MatrixOptions is the wrapper for options related to matrix.
 type MatrixOptions struct {
 	ModifiedFiles string
+	MelangeMode   string
 }
 
 func (o *MatrixOptions) AddFlags(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&o.ModifiedFiles, "modified-files", "",
 		"a comma-separated list of files modified in the repo")
+	cmd.Flags().StringVar(&o.MelangeMode, "melange-mode", "",
+		"mode for custom melange builds. Modes: only, none")
 }
