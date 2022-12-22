7199034710338e27e7e1614b7ff2b1aad8a04c83
Jed Salazar <jedsalazar@gmail.com>
2022-12-22 11:17:54 -0700

    add ssh to golang image (#147)

--- images/go/test.sh
+++ images/go/test.sh
@@ -6,3 +6,4 @@ IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/go"}
 
 docker run $IMAGE_NAME version
 docker run --entrypoint /usr/bin/git $IMAGE_NAME version
+docker run --entrypoint /usr/bin/ssh $IMAGE_NAME -V
