63f9c1c9e14f1bcc5aa222efe8f66cd0dccc36e4
Dan Lorenc <dlorenc@chainguard.dev>
2022-12-29 10:27:46 -0500

    Add a deno image.
    This is marked as experimental. Deno doesn't actually require a base image to run, so
    we should also show some multi-stage build examples here where you use deno compile then
    a static base.
    
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

---
+++ images/deno/README.md
@@ -0,0 +1,49 @@
+# deno
+
+Minimal container image for running deno apps
+
+The image specifies a default non-root `deno` user (UID 65532), and a working directory at `/app`, owned by that `deno` user, and accessible to all users.
+
+## Get It!
+
+The image is available on `cgr.dev`:
+
+```
+docker pull cgr.dev/chainguard/deno:latest
+```
+
+## Usage Example
+
+Navigate to the [`example/`](./example/) directory:
+
+```
+cd example/
+```
+
+The Dockerfile is based on [Deno's webserver tutorial](https://deno.land/manual@v1.28.3/examples/http_server), but packages it up into a Chainguard deno image.
+
+Build the application on the deno base image.
+
+```
+docker build \
+  --tag deno-docker \
+  .
+```
+
+Then you can run the image:
+
+```
+docker run \
+  --rm -it \
+  -p 8080:8080 \
+  deno-docker
+```
+
+...and test to see that it works:
+
+```
+$ curl localhost:8080
+Your user-agent is:
+
+curl/7.84.0%
+```
---
+++ images/deno/example/webserver.ts
@@ -0,0 +1,31 @@
+// Start listening on port 8080 of localhost.
+const server = Deno.listen({ port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+
+// Connections to the server will be yielded up as an async iterable.
+for await (const conn of server) {
+  // In order to not be blocking, we need to handle each connection individually
+  // without awaiting the function
+  serveHttp(conn);
+}
+
+async function serveHttp(conn: Deno.Conn) {
+  // This "upgrades" a network connection into an HTTP connection.
+  const httpConn = Deno.serveHttp(conn);
+  // Each request sent over the HTTP connection will be yielded as an async
+  // iterator from the HTTP connection.
+  for await (const requestEvent of httpConn) {
+    // The native HTTP server uses the web standard `Request` and `Response`
+    // objects.
+    const body = `Your user-agent is:\n\n${
+      requestEvent.request.headers.get("user-agent") ?? "Unknown"
+    }`;
+    // The requestEvent's `.respondWith()` method is how we send the response
+    // back to the client.
+    requestEvent.respondWith(
+      new Response(body, {
+        status: 200,
+      }),
+    );
+  }
+}
