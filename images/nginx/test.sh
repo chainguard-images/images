05c4b4efb84278fd25620d98fa25508fd94f00ea
Josh Dolitsky <josh@dolit.ski>
2023-01-19 17:21:50 -0600

    add docker logs to test output
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/nginx/test.sh
+++ images/nginx/test.sh
@@ -6,6 +6,6 @@ IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/nginx"}
 CONTAINER_NAME=${CONTAINER_NAME:-"nginx-smoketest-$(date +%s)"}
 
 docker run -p 8080:80 -d --name $CONTAINER_NAME $IMAGE_NAME
-trap "docker rm -f $CONTAINER_NAME" EXIT
+trap "docker logs $CONTAINER_NAME && docker rm -f $CONTAINER_NAME" EXIT
 
 curl -v --max-time 10 http://localhost:8080/ | grep '<title>Welcome to nginx!</title>'
