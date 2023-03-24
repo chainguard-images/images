44763c6068dde3f0b7f1be51a227881264efe35e
Ariadne Conill <ariadne@dereferenced.org>
2023-03-24 13:50:29 +0000

    add dotnet-sdk image (experimental)
    Signed-off-by: Ariadne Conill <ariadne@dereferenced.org>

---
+++ images/dotnet-sdk/tests/01-runs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+IMAGE_DIR="$(basename "$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd )")"
+IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/${IMAGE_DIR}:latest"}
+
+docker run --rm $IMAGE_NAME dotnet --info > /dev/null
