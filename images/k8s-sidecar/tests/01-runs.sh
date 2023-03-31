f5bf0048f41edea80620f63fce89565c658256f1
Dan Lorenc <dlorenc@chainguard.dev>
2023-03-31 07:59:18 -0400

    Add k8s-sidecar image.
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

---
+++ images/k8s-sidecar/tests/01-runs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+IMAGE_DIR="$(basename "$(cd "$(dirname ${BASH_SOURCE[0]})/.." && pwd )")"
+IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/${IMAGE_DIR}:latest"}
+
+# The image needs to be in a cluster to run, so we'll look for the right error message.
+set +o pipefail
+docker run --rm "${IMAGE_NAME}" 2>&1 | grep "Starting collector
