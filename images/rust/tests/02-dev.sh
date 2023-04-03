39a430367a614f9c20d3de36b2c5b12f69b557f9
Josh Dolitsky <josh@dolit.ski>
2023-04-03 13:01:12 -0500

    update README with rust variant
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/rust/tests/02-dev.sh
+++ images/rust/tests/02-dev.sh
@@ -7,5 +7,5 @@ IMAGE_NAME=${IMAGE_NAME:-"cgr.dev/chainguard/${IMAGE_DIR}:latest"}
 
 # Dev variants should have rustup
 if [[ "${IMAGE_NAME}" = *-dev || "${IMAGE_TAG_SUFFIX}" = "-dev" ]]; then
-    docker run --rm --entrypoint /usr/bin/rustup "${IMAGE_NAME}" target list
+    docker run --rm --entrypoint rustup "${IMAGE_NAME}" --version
 fi
