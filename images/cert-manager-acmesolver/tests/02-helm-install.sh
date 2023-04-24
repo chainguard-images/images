2acb1b845865dba803b3f5d168f956874f8a472d
Josh Wolf <josh@wolfs.io>
2023-04-24 07:19:04 -0400

    add certmanager
    Signed-off-by: Josh Wolf <josh@wolfs.io>

---
+++ images/cert-manager-acmesolver/tests/02-helm-install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+if [[ "${IMAGE_NAME}" == "" ]]; then
+    echo "Must set IMAGE_NAME environment variable. Exiting."
+    exit 1
+fi
+
+IMAGE_REPOSITORY=$(echo "${IMAGE_NAME}" | cut -d: -f1)
+IMAGE_TAG="$(echo "${IMAGE_NAME}" | cut -d: -f2)"
+
+helm repo add jetstack https://charts.jetstack.io
+
+helm upgrade --install \
+    cert-manager jetstack/cert-manager \
+    --namespace cert-manager \
+    --create-namespace \
+    --set installCRDs=true \
+    --set acmesolver.image.repository="${IMAGE_REPOSITORY}" \
+    --set acmesolver.image.tag="${IMAGE_TAG}"
+
+kubectl wait --for=condition=ready pod --selector app.kubernetes.io/instance=cert-manager --namespace cert-manager
