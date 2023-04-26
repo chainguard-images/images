fea2731a9af5e3e9695da6eae0dc834673e52850
Josh Dolitsky <josh@dolit.ski>
2023-04-26 09:30:03 -0500

    Test workaround for new nginx image
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/nginx/tests/02-welcome-page.sh
+++ images/nginx/tests/02-welcome-page.sh
@@ -7,18 +7,13 @@ if [[ "${IMAGE_NAME}" == "" ]]; then
     exit 1
 fi
 
-# Temporary hack to support both images
-# Port will 8080 after May 3.
-if [[ "${IMAGE_NAME}" =~ .*next.* ]]; then
-    PORT=8080
-else
-    PORT=80
-fi
-echo "PORT $PORT"
-
 CONTAINER_NAME=${CONTAINER_NAME:-"nginx-smoketest-$(date +%s)"}
 
-docker run -p 8080:$PORT -d --name $CONTAINER_NAME $IMAGE_NAME
+# The "-p 8080:8080 -p 8081:80" is a temporary hack to support both images
+# Port will 8080 after May 3, and we can drop anything port 8081 or port 80 from this file
+docker run -p 8080:8080 -p 8081:80 -d --name $CONTAINER_NAME $IMAGE_NAME
 trap "docker logs $CONTAINER_NAME && docker rm -f $CONTAINER_NAME" EXIT
 sleep 5
-curl -v --max-time 10 http://localhost:8080/ | grep '<title>Welcome to nginx!</title>'
+
+curl -v --max-time 10 http://localhost:8080/ | grep '<title>Welcome to nginx!</title>' || \
+    curl -v --max-time 10 http://localhost:8081/ | grep '<title>Welcome to nginx!</title>'
