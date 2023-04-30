73cc913321d26519e6882d06b523cc10101a441f
Wilson E. Husin <wilson@husin.dev>
2023-04-30 11:38:12 -0700

    postgres: use UTF-8 encoding by default
    Postgres (the program) honors environment locale, which would be
    SQL_ASCII when unset. Docker library builds their image with UTF-8 by
    default[^0]. This change is to ensure users have a closer experience
    between Docker images and Chainguard images.
    
    [^0]: https://github.com/docker-library/postgres/blob/887333be0892981ffbf468fa62aa6f2e0dd6358a/15/alpine/Dockerfile#L19-L21
    
    Signed-off-by: Wilson E. Husin <wilson@husin.dev>

---
+++ images/postgres/tests/03-locale.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+if [[ "${IMAGE_NAME}" == "" ]]; then
+    echo "Must set IMAGE_NAME environment variable. Exiting."
+    exit 1
+fi
+
+CONTAINER_NAME=${CONTAINER_NAME:-"postgres-$(date +%s)"}
+
+docker run -e POSTGRES_PASSWORD=password -d --name $CONTAINER_NAME $IMAGE_NAME
+trap "docker logs $CONTAINER_NAME && docker rm -f $CONTAINER_NAME" EXIT
+sleep 5
+docker exec -it $CONTAINER_NAME /usr/bin/psql -U postgres -t --csv -c "show server_encoding;" | head -n 1 | grep UTF8
