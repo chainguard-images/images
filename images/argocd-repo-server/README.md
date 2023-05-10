b18457acf2aa1dc4de07ac295dc05a7f4a981a2b
Josh Wolf <josh@wolfs.io>
2023-05-10 12:06:17 -0400

    add argocd and argocd-repo-server
    Signed-off-by: Josh Wolf <josh@wolfs.io>

---
+++ images/argocd-repo-server/README.md
@@ -0,0 +1,26 @@
+<!--monopod:start-->
+# argocd-repo-server
+| | |
+| - | - |
+| **Status** | stable |
+| **OCI Reference** | `cgr.dev/chainguard/argocd-repo-server` |
+| **Variants/Tags** | ![](https://storage.googleapis.com/chainguard-images-build-outputs/summary/argocd-repo-server.svg) |
+
+*[Contact Chainguard](https://www.chainguard.dev/chainguard-images) for enterprise support, SLAs, and access to older tags.*
+
+---
+<!--monopod:end-->
+
+[argocd](https://argo-cd.readthedocs.io/en/stable/) Declarative continuous deployment for Kubernetes.
+
+## Get It!
+
+The image is available on `cgr.dev`:
+
+```
+docker pull cgr.dev/chainguard/argocd-repo-server
+```
+
+## Using argocd-repo-server
+
+This image is part of the [ArgoCD](../argocd/README.md) image, and should be used in conjunction with that.
---
+++ images/argocd-repo-server/tests/02-install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+function preflight() {
+	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
+		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
+		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_TAG}" == "" ]]; then
+		echo "Must set IMAGE_TAG environment variable. Exiting."
+		exit 1
+	fi
+}
+
+preflight
+
+helm repo add argo https://argoproj.github.io/argo-helm
+helm upgrade -i argocd argo/argo-cd \
+	--namespace argocd \
+	--create-namespace \
+	--set redis.image.repository="cgr.dev/chainguard/redis" \
+	--set redis.image.tag="latest" \
+	--set dex.image.repository="cgr.dev/chainguard/dex" \
+	--set dex.image.tag="latest" \
+	--set repoServer.image.repository="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}" \
+	--set repoServer.image.tag="${IMAGE_TAG}"
+
+sleep 3
+
+kubectl wait --for=condition=ready pod -n argocd --selector app.kubernetes.io/instance=argocd --timeout=120s
