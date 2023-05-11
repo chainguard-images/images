689ff04b4d89b704ad0d48345c1106e1fec13e87
Josh Wolf <josh@wolfs.io>
2023-05-11 11:23:07 -0400

    add dex
    Signed-off-by: Josh Wolf <josh@wolfs.io>

---
+++ images/dex/tests/02-runs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+if [[ "${IMAGE_NAME}" == "" ]]; then
+	echo "Must set IMAGE_NAME environment variable. Exiting."
+	exit 1
+fi
+
+PORT=5556
+
+URL="http://127.0.0.1:${PORT}"
+docker run --rm -d -p $PORT:$PORT "${IMAGE_NAME}" dex serve /etc/dex/config.docker.yaml
+
+# Maximum number of attempts to check the health check endpoint
+MAX_ATTEMPTS=30
+
+# Delay (in seconds) between attempts
+DELAY=5
+
+attempt=0
+until $(curl --output /dev/null --silent --head --fail "$URL/dex/healthz"); do
+	if [ $attempt -eq $MAX_ATTEMPTS ]; then
+		echo "Max attempts reached, exiting..."
+		exit 1
+	fi
+
+	printf '.'
+	attempt=$(($attempt + 1))
+
+	# Delay before next attempt
+	sleep $DELAY
+done
+
+echo "Health check endpoint is available!"
+
+curl -L "$URL/dex/.well-known/openid-configuration" | jq '.issuer' | grep "$URL/dex"
