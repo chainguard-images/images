16182601d53220f7c2f59cb9e2b5c0f21e9e6180
Matt Moore <mattmoor@chainguard.dev>
2023-05-12 19:34:39 -0700

    Cleanup: Move the policy checks to use terraform.
    :broom: This change moves the way we invoke our policy checks to use terraform instead of the policy tester.
    
    For now this simply applies the terraform directly over the manually supplied refs, but the way this is structured the same terraform may be invokes as a module from other terraform in the future.
    
    /kind cleanup
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- policies/README.md
+++ policies/README.md
@@ -1,3 +1,12 @@
 This folder contains
 [`ClusterImagePolicy`](https://docs.sigstore.dev/policy-controller/overview/)
 resources which should apply to all Chainguard Images.
+
+You can run these policies over a list of image references with:
+
+```shell
+export TF_VAR_image_refs='["foo", "bar", "baz"]'
+
+terraform init
+terraform apply
+```
---
+++ policies/main.tf
@@ -0,0 +1,41 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+terraform {
+  required_providers {
+    cosign = {
+      source = "chainguard-dev/cosign"
+    }
+  }
+}
+
+variable "image_refs" {
+  type = list(string)
+  description = "The image references to verify against our policies."
+}
+
+data "cosign_verify" "is-signed" {
+  for_each = toset(var.image_refs)
+  image    = each.key
+  policy   = file("${path.module}/01-is-signed.yaml")
+}
+
+data "cosign_verify" "has-vuln-att" {
+  for_each = toset(var.image_refs)
+  image    = each.key
+  policy   = file("${path.module}/02-has-vuln-attestation.yaml")
+}
+
+data "cosign_verify" "has-sbom-att" {
+  for_each = toset(var.image_refs)
+  image    = each.key
+  policy   = file("${path.module}/03-has-sbom-attestation.yaml")
+}
+
+data "cosign_verify" "has-slsa-att" {
+  for_each = toset(var.image_refs)
+  image    = each.key
+  policy   = file("${path.module}/04-has-slsa-attestation.yaml")
+}
