69a8c14a2a81d17eb30ae6aaf4ea5283669cfd2b
Matt Moore <mattmoor@chainguard.dev>
2023-05-13 15:18:39 -0700

    Feature: Add a new Hugo image. (#596)
    :gift: This change add a new image for the Hugo package, including a `-dev` variant.
    
    /kind feature
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/hugo/tests/01-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+if [[ "${IMAGE_NAME}" == "" ]]; then
+    echo "Must set IMAGE_NAME environment variable. Exiting."
+    exit 1
+fi
+
+docker run --rm "${IMAGE_NAME}" version
