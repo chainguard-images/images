a1c29de5a0d5f3cc1e36cdf02d9c815b810a0e80
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2023-05-13 09:56:23 +0300

    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- images/envoy-ratelimit/tests/02-helm.sh
+++ images/envoy-ratelimit/tests/02-helm.sh
@@ -37,6 +37,7 @@ helm repo add xyctruth https://xyctruth.github.io/istio-ratelimit
 helm install ratelimit xyctruth/istio-ratelimit \
     --set ratelimit.image.repository="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}" \
     --set ratelimit.image.tag="${IMAGE_TAG}"
+	
 kubectl set env deployment/ratelimit-istio-ratelimit REDIS_URL=ratelimit-redis-master:6379
 
 kubectl wait --for=condition=ready pod --selector app.kubernetes.io/instance=ratelimit --namespace default
