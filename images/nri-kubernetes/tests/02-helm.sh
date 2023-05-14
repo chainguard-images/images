b9ddda70673301db26f02b2415fb0d366ef0cb00
dlorenc <lorenc.d@gmail.com>
2023-05-14 07:09:47 -0400

    Add New Relic Kubernetes image and tests. (#593)
    * Add New Relic Kubernetes image and tests.
    
    This test requires a New Relic license key to run. We're going to have a lot more of these images,
    so I created a license key and added it as a repository secret.
    
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>
    
    * Update images/nri-kubernetes/README.md
    
    Co-authored-by: Jason Hall <jason@chainguard.dev>
    Signed-off-by: dlorenc <lorenc.d@gmail.com>
    
    ---------
    
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>
    Signed-off-by: dlorenc <lorenc.d@gmail.com>
    Co-authored-by: Jason Hall <jason@chainguard.dev>

---
+++ images/nri-kubernetes/tests/02-helm.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+function preflight() {
+  if [[ "${IMAGE_REGISTRY}" == "" ]]; then
+    echo "Must set IMAGE_REGISTRY environment variable. Exiting."
+    exit 1
+  fi
+
+  if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
+    echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
+    exit 1
+  fi
+
+  if [[ "${IMAGE_TAG}" == "" ]]; then
+    echo "Must set IMAGE_TAG environment variable. Exiting."
+    exit 1
+  fi
+  helm repo add nri-kubernetes https://newrelic.github.io/nri-kubernetes
+}
+
+
+function TEST_basic_helm_install() {
+ helm upgrade \
+    --install \
+    newrelic-infrastructure nri-kubernetes/newrelic-infrastructure \
+    --set images.integration.registry=${IMAGE_REGISTRY} \
+    --set images.integration.repository=${IMAGE_REPOSITORY} \
+    --set images.integration.tag=${IMAGE_TAG}" \
+    --set cluster=test \
+    --set licenseKey=${NEWRELIC_LICENSE_KEY}
+
+  # Wait for helm to catch up
+  sleep 3
+
+  if kubectl wait --for=condition=ready pod --selector app.kubernetes.io/instance=newrelic-infrastructure; then
+    echo "Success"
+  else
+    echo "Failed"
+    kubectl describe pod --selector app.kubernetes.io/instance=newrelic-infrastructure
+    kubectl logs --selector app.kubernetes.io/instance=newrelic-infrastructure
+    exit 1
+  fi
+  helm uninstall newrelic-infrastructure
+}
+
+preflight
+TEST_basic_helm_install
