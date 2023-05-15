268eb81b3537445957a25eefb00662c69a09b915
dlorenc <lorenc.d@gmail.com>
2023-05-14 20:31:55 -0400

    Add debug logging to keda tests. (#604)

--- images/keda/tests/02-helm.sh
+++ images/keda/tests/02-helm.sh
@@ -23,6 +23,14 @@ function preflight() {
 
 preflight
 
+function cleanup() {
+    # Get the logs from keda before exiting
+    kubectl describe pod --selector app.kubernetes.io/instance=keda --namespace keda
+    kubectl logs --selector app.kubernetes.io/instance=keda --namespace keda
+}
+
+trap cleanup EXIT
+
 helm repo add kedacore https://kedacore.github.io/charts
 helm repo update
 
@@ -31,6 +39,6 @@ kubectl create namespace keda
 helm install keda kedacore/keda --namespace keda \
     --set image.keda.repository="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}" \
     --set image.keda.tag="${IMAGE_TAG}" \
-    --set image.pullPolicy=Never
+    --set image.pullPolicy=IfNotPresent # The same pullPolicy is used for multiple images, so we have to allow the others to get pulled.
 
 kubectl wait --for=condition=ready pod --selector app.kubernetes.io/instance=keda --namespace keda --timeout=120s
