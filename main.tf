76271e0c858bf7141180c4f80aa78c64acdb235b
Matt Moore <mattmoor@chainguard.dev>
2023-05-26 08:35:44 -0400

    Expose the "extra" settings as TF variables.
    This will let us pass different values for these across contexts as we continue to evolve these workflows
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- main.tf
+++ main.tf
@@ -25,22 +25,41 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+variable "extra_repositories" {
+  type        = list(string)
+  default     = ["https://packages.wolfi.dev/os"]
+  description = "The list of additional repositories to append to the apko configuration."
+}
+
+variable "extra_keyring" {
+  type        = list(string)
+  default     = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
+  description = "The list of additional keyring entries to append to the apko configuration."
+}
+
+variable "extra_packages" {
+  type        = list(string)
+  default     = ["wolfi-baselayout"]
+  description = "The list of additional packages to append to the apko configuration."
+}
+
 variable "extract_package" {
   description = "TODO."
 }
 
 provider "apko" {
-  extra_repositories = ["https://packages.wolfi.dev/os"]
-  extra_keyring      = ["https://packages.wolfi.dev/os/wolfi-signing.rsa.pub"]
+  extra_repositories = var.extra_repositories
+  extra_keyring      = var.extra_keyring
   default_archs      = ["x86_64", "aarch64"]
-  extra_packages     = ["wolfi-baselayout"]
 }
 
 module "image" {
   source  = "chainguard-dev/apko/publisher"
+  version = "0.0.2"
 
   target_repository = var.target_repository
-  config = file(var.apko_config_path)
+  config            = file(var.apko_config_path)
+  extra_packages    = var.extra_packages
 }
 
 output "image_ref" {
