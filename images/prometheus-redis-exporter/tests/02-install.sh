3dce93188a7ad017049db6727c172b539ad47382
Ajay Kemparaj <ajaykemparaj@gmail.com>
2023-05-26 09:15:53 -0700

    Add prometheus-redis-exporter image (#663)
    Signed-off-by: ajayk <ajaykemparaj@gmail.com>

---
+++ images/prometheus-redis-exporter/tests/02-install.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+function preflight() {
+	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
+		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
+		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_TAG}" == "" ]]; then
+		echo "Must set IMAGE_TAG environment variable. Exiting."
+		exit 1
+	fi
+}
+
+preflight
+
+cat >deploy.yaml <<EOF
+---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: redis
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  namespace: redis
+  name: redis
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: redis
+  template:
+    metadata:
+      annotations:
+        prometheus.io/scrape: "true"
+        prometheus.io/port: "9121"
+      labels:
+        app: redis
+    spec:
+      containers:
+      - name: redis
+        image: cgr.dev/chainguard/redis:latest
+        resources:
+          requests:
+            cpu: 100m
+            memory: 100Mi
+        ports:
+        - containerPort: 6379
+      - name: redis-exporter
+        image: ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}
+        securityContext:
+          runAsUser: 65532
+          runAsGroup: 65532
+          allowPrivilegeEscalation: false
+          capabilities:
+            drop:
+            - ALL
+        resources:
+          requests:
+            cpu: 100m
+            memory: 100Mi
+        ports:
+        - containerPort: 9121
+EOF
+
+kubectl apply -f deploy.yaml -n redis
+
+kubectl wait --for=condition=ready pod --selector app=redis -n redis
+
+latest_pod_name="$(kubectl get pods --selector app=redis -n redis -o jsonpath="{.items[0].metadata.name}")"
+kubectl port-forward "pod/${latest_pod_name}" 9121:9121 -n redis &
+
+
+pid=$!
+trap "kill $pid" EXIT
+sleep 10
+curl localhost:9121/metrics | grep "redis_up 1"
+
