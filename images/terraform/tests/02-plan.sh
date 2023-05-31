09b22c7f5d16a259e47b46977d286d73fc2d1553
Jason Hall <jason@chainguard.dev>
2023-05-31 13:02:32 -0400

    define example in tmpdir
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/terraform/tests/02-plan.sh
+++ images/terraform/tests/02-plan.sh
@@ -7,12 +7,34 @@ if [[ "${IMAGE_NAME}" == "" ]]; then
     exit 1
 fi
 
+TMPDIR="$(mktemp -d)"
+
+cat > ${TMPDIR}/main.tf <<EOF
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_string" "random" {
+  length = 16
+}
+
+output "random" {
+  value = random_string.random.result
+}
+EOF
+
 docker run --rm \
-  -v "${PWD}/example:/work" \
+  -v "${TMPDIR}:/work" \
   -w /work \
   "${IMAGE_NAME}" init
 
 docker run --rm \
-  -v "${PWD}/example:/work" \
+  -v "${TMPDIR}:/work" \
   -w /work \
   "${IMAGE_NAME}" apply --auto-approve
