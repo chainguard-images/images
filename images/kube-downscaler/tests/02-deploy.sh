ddc27767cef0f1e9344e639d15d0eaa1e1cf7397
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2023-06-02 10:48:32 +0200

    add kube-downscaler image (#715)
    Signed-off-by: cpanato <ctadeu@gmail.com>

---
+++ images/kube-downscaler/tests/02-deploy.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+function preflight() {
+	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
+		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
+		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_TAG}" == "" ]]; then
+		echo "Must set IMAGE_TAG environment variable. Exiting."
+		exit 1
+	fi
+}
+
+preflight
+
+function cleanup() {
+	# Print debug logs and status
+	kubectl get pods
+	kubectl describe pods
+
+	# Seeing intermittent failures if we don't wait for a bit here
+	# The `rollout status`` below should wait for terminated pods to be removed
+	# However, we still occasionally see a terminating pod which fails when checking logs
+	sleep 10
+	kubectl logs --selector application=kube-downscaler
+}
+
+trap cleanup EXIT
+
+# Deploy the kube-downscaler yaml
+kubectl apply -f https://codeberg.org/hjacobs/kube-downscaler/raw/tag/23.2.0/deploy/config.yaml
+kubectl apply -f https://codeberg.org/hjacobs/kube-downscaler/raw/tag/23.2.0/deploy/rbac.yaml
+kubectl apply -f https://codeberg.org/hjacobs/kube-downscaler/raw/tag/23.2.0/deploy/deployment.yaml
+
+
+# The pod can take a few seconds to appear after the deployment and the replicaset are all created and reconcile
+sleep 10
+
+# Wait for the default kube-downscaler to be healthy
+kubectl wait --for=condition=ready pod --selector application=kube-downscaler --timeout=120s
+
+# Swap out the image
+kubectl set image deployment/kube-downscaler downscaler="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+
+# Wait for the replicaset to be ready and old pods to terminated
+kubectl rollout status deployment/kube-downscaler --timeout=120s
+
+# Check the kube-downscaler pods are deployed and healthy
+kubectl wait --for=condition=ready pod --selector application=kube-downscaler --timeout=120s
