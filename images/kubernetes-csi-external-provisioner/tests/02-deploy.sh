33d29b3f694ba8c494497dccf2fe6136abc16675
Jason Hall <jason@chainguard.dev>
2023-06-03 11:31:32 -0400

    tests: standardize on using IMAGE_NAME, where possible (#778)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/kubernetes-csi-external-provisioner/tests/02-deploy.sh
+++ images/kubernetes-csi-external-provisioner/tests/02-deploy.sh
@@ -4,24 +4,10 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-function preflight() {
-	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
-		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
-		exit 1
-	fi
-
-	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
-		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
-		exit 1
-	fi
-
-	if [[ "${IMAGE_TAG}" == "" ]]; then
-		echo "Must set IMAGE_TAG environment variable. Exiting."
-		exit 1
-	fi
-}
-
-preflight
+if [[ "${IMAGE_NAME}" == "" ]]; then
+    echo "Must set IMAGE_NAME environment variable. Exiting."
+    exit 1
+fi
 
 function cleanup() {
 	# Print debug logs and status
@@ -48,7 +34,7 @@ sleep 10
 kubectl wait --for=condition=ready pod --selector app=csi-provisioner --timeout=120s
 
 # Swap out the image
-kubectl set image deployment/csi-provisioner csi-provisioner="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+kubectl set image deployment/csi-provisioner csi-provisioner="${IMAGE_NAME}"
 
 # Wait for the replicaset to be ready and old pods to terminated
 kubectl rollout status deployment/csi-provisioner --timeout=120s
