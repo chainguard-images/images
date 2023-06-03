fb46662f584dd006d06f36b77f8917e1207fae8f
Matt Moore <mattmoor@chainguard.dev>
2023-06-03 12:58:19 -0700

    Move towards dedicated TF modules per image (#781)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/ko/main.tf
+++ images/ko/main.tf
@@ -1,40 +1,12 @@
-/*
-Copyright 2023 Chainguard, Inc.
-SPDX-License-Identifier: Apache-2.0
-*/
-
 terraform {
   required_providers {
-    apko = {
-      source = "chainguard-dev/apko"
-    }
-    cosign = {
-      source = "chainguard-dev/cosign"
-    }
-    oci = {
-      source = "chainguard-dev/oci"
-    }
+    apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
   }
-}
-
-variable "apko_config_path" {
-  description = "The path to the apko image config file."
-}
-
-variable "test_dir" {
-  description = "The directory from which to run tests."
-}
-
-variable "test_cmd" {
-  description = "The command to run to invoke tests."
 }
 
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
-}
-
-variable "target_tag" {
-  description = "The target tag for this image."
 }
 
 variable "tag_suffix" {
@@ -59,44 +31,36 @@ variable "extra_packages" {
   description = "The list of additional packages to append to the apko configuration."
 }
 
-variable "extract_package" {
-  description = "TODO."
-}
-
 provider "apko" {
   extra_repositories = var.extra_repositories
   extra_keyring      = var.extra_keyring
   default_archs      = ["x86_64", "aarch64"]
 }
 
-module "image" {
+module "latest" {
   source  = "chainguard-dev/apko/publisher"
-  version = "0.0.4"
+  version = "0.0.3"
 
   target_repository = var.target_repository
-  config            = file(var.apko_config_path)
+  config            = file("${path.module}/configs/latest.apko.yaml")
   extra_packages    = var.extra_packages
 }
 
-data "oci_exec_test" "this" {
-  digest = module.image.image_ref
-  script = <<EOF
-    export IMAGE_TAG_SUFFIX="${var.tag_suffix}"
-    cd "${path.module}/${var.test_dir}"
-    ${var.test_cmd}
-  EOF
-}
-
 module "version-tags" {
-  source  = "./tflib/version-tags"
-  package = var.extract_package
-  config  = module.image.config
+  source  = "../../tflib/version-tags"
+  package = "ko"
+  config  = module.latest.config
 }
 
-resource "oci_tag" "this" {
-  depends_on = [ data.oci_exec_test.this ]
+module "test-latest" {
+  source = "./tests"
+  digest = module.latest.image_ref
+}
+
+resource "oci_tag" "version-tags" {
+  depends_on = [ module.test-latest ]
   for_each   = toset(concat(["latest"], module.version-tags.tag_list))
 
-  digest_ref = module.image.image_ref
+  digest_ref = module.latest.image_ref
   tag        = "${each.key}${var.tag_suffix}"
 }
