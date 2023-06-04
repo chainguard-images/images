b9b3b028e9cf83966423f68f05a90bd0e770474a
Dan Lorenc <dlorenc@chainguard.dev>
2023-06-04 07:54:50 -0400

    Add oidc provider test and fix image with new packaging scheme.
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

---
+++ images/oidc-discovery-provider/tests/02-helm.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# monopod:tag:k8s
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+function preflight() {
+	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
+		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
+		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
+		exit 1
+	fi
+
+	if [[ "${IMAGE_TAG}" == "" ]]; then
+		echo "Must set IMAGE_TAG environment variable. Exiting."
+		exit 1
+	fi
+}
+
+function ensure_spire_is_installed(){
+   helm repo add spiffe https://spiffe.github.io/helm-charts/
+   helm repo update
+   helm install spire spiffe/spire \
+   --namespace spire-system \
+   --create-namespace \
+   --set spiffe-oidc-discovery-provider.enabled=true \
+   --set spiffe-oidc-discovery-provider.config.acme.tosAccepted=true \
+   --set spiffe-oidc-discovery-provider.image.registry="${IMAGE_REGISTRY}" \
+   --set spiffe-oidc-discovery-provider.image.repository="${IMAGE_REPOSITORY}" \
+   --set spiffe-oidc-discovery-provider.image.tag="${IMAGE_TAG}"
+}
+
+function test(){
+    kubectl wait --for=condition=ready pod -n spire-system --selector app.kubernetes.io/instance=spire --timeout=120s
+}
+
+preflight
+ensure_spire_is_installed
+sleep 3
+test
