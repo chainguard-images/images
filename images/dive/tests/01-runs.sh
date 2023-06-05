4c3865e3584da8b11f49cf9547e43a29f6c96ae3
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2023-06-05 17:26:16 -0500

    Update 01-runs.sh to fix permissions error
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>

--- images/dive/tests/01-runs.sh
+++ images/dive/tests/01-runs.sh
@@ -9,6 +9,12 @@ fi
 
 docker run --rm $IMAGE_NAME --version > /dev/null
 
+TMPDIR="$(mktemp -d)"
+chmod go+wrx "${TMPDIR}"
+cd "${TMPDIR}"
+
 docker pull cgr.dev/chainguard/static
 docker save cgr.dev/chainguard/static > static.tar
-docker run --rm -v "$PWD":/work --workdir=/work $IMAGE_NAME --source docker-archive static.tar -j out.json; cat out.json | grep "efficiencyScore"
+
+docker run --rm -v "$PWD":/work --workdir=/work $IMAGE_NAME --source docker-archive static.tar -j out.json
+cat out.json | grep "efficiencyScore"
