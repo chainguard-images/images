3a8ea308cd77e23904556b809226491d936ccd5b
Jason Hall <jason@chainguard.dev>
2023-06-23 14:10:13 -0400

    Update images/guacamole-server/tests/01-runs.sh
    Co-authored-by: Ajay Kemparaj <ajaykemparaj@gmail.com>
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/guacamole-server/tests/01-runs.sh
+++ images/guacamole-server/tests/01-runs.sh
@@ -2,5 +2,4 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-# The image needs to be in a cluster to run, so we'll look for the right error message.
 docker run --rm "${IMAGE_NAME}" 2>&1 | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
