f554d197274397ada28b183059bdbc8ec83c7b33
Batuhan Apaydın <batuhan.apaydin@chainguard.dev>
2023-06-23 21:30:50 +0300

    add guacamole-server image (#913)
    * add guacamole-server image
    
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
    Co-authored-by: Furkan Turkal <furkan.turkal@chainguard.dev>
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
    
    * Fix readme
    
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    
    * Update images/guacamole-server/tests/01-runs.sh
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Update images/guacamole-server/tests/01-runs.sh
    
    Co-authored-by: Ajay Kemparaj <ajaykemparaj@gmail.com>
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Update images/guacamole-server/tests/01-runs.sh
    
    Co-authored-by: Ajay Kemparaj <ajaykemparaj@gmail.com>
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Update images/guacamole-server/tests/01-runs.sh
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    Co-authored-by: Furkan Turkal <furkan.turkal@chainguard.dev>
    Co-authored-by: Jason Hall <jason@chainguard.dev>
    Co-authored-by: Ajay Kemparaj <ajaykemparaj@gmail.com>

---
+++ images/guacamole-server/tests/01-runs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+docker run --rm -d --name=guacamole "${IMAGE_NAME}"
+docker logs guacamole 2>&1  | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
+trap "docker rm -f guacamole" EXIT
