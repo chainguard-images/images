0419ac42490be191e7bf00c78f3cb7e60ce8fc87
Matt Moore <mattmoor@chainguard.dev>
2023-06-22 17:29:05 -0700

    Post Rust to TF (#914)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/rust/tests/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+variable "check-dev" {
+  default     = false
+  description = "Whether to check for dev extensions"
+}
+
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "${path.module}/01-version.sh"
+}
+
+data "oci_exec_test" "dev" {
+  count  = var.check-dev ? 1 : 0
+  digest = var.digest
+  script = "${path.module}/02-dev.sh"
+}
