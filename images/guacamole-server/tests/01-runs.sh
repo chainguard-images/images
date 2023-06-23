02d5d504aa2a5a841e3522f965aedd30a7018010
Ajay Kemparaj <ajaykemparaj@gmail.com>
2023-06-23 11:28:04 -0700

    Update 01-runs.sh
    Signed-off-by: Ajay Kemparaj <ajaykemparaj@gmail.com>

--- images/guacamole-server/tests/01-runs.sh
+++ images/guacamole-server/tests/01-runs.sh
@@ -3,5 +3,5 @@
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
 docker run --rm -d --name=guacamole "${IMAGE_NAME}"
-docker logs guacamole | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
+docker logs guacamole 2>&1  | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
 trap "docker rm -f guacamole" EXIT
