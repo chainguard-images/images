04b76cddb8a2368146563be173f72241f99feaeb
Jason Hall <jason@chainguard.dev>
2023-06-23 14:12:32 -0400

    Update images/guacamole-server/tests/01-runs.sh
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/guacamole-server/tests/01-runs.sh
+++ images/guacamole-server/tests/01-runs.sh
@@ -2,4 +2,6 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-docker run --rm "${IMAGE_NAME}" 2>&1 | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
+docker run --rm -d --name=guacamole "${IMAGE_NAME}"
+docker logs guacamole | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
+trap "docker rm -f guacamole" EXIT
