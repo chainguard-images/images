b7c452b48ec1ad13c84acfb8f133458b2cb4f57b
ajayk <ajaykemparaj@gmail.com>
2023-06-23 00:34:49 -0700

    test
    Signed-off-by: ajayk <ajaykemparaj@gmail.com>

--- images/fluentd/tests/01-test.sh
+++ images/fluentd/tests/01-test.sh
@@ -18,7 +18,7 @@ start_fluentd_detached() {
   docker run \
     --detach --rm \
     --publish 127.0.0.1:${FREE_PORT}:24224 \
-    --volume "${pwd}/images/fluentd/examples/basic_docker.conf:/etc/fluent/fluent.conf" \
+    --volume "$(pwd)/images/fluentd/examples/basic_docker.conf:/etc/fluent/fluent.conf" \
     "${IMAGE_NAME}"
   sleep 2
 }
