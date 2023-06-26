fc36d585e15800d3e545877d01512f25d15f4719
Jason Hall <jason@chainguard.dev>
2023-06-26 11:08:39 -0400

    migrate aws-load-balancer-controller to TF w/ helm test (#939)
    * migrate aws-load-balancer-controller to TF w/ helm test
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * monopod readme
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ images/aws-load-balancer-controller/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+module "latest" {
+  source = "../../tflib/publisher"
+
+  target_repository = var.target_repository
+  config            = file("${path.module}/configs/latest.apko.yaml")
+}
+
+module "dev" { source = "../../tflib/dev-subvariant" }
+
+module "latest-dev" {
+  source = "../../tflib/publisher"
+
+  target_repository = var.target_repository
+  # Make the dev variant an explicit extension of the
+  # locked original.
+  config         = jsonencode(module.latest.config)
+  extra_packages = module.dev.extra_packages
+}
+
+module "version-tags" {
+  source  = "../../tflib/version-tags"
+  package = "aws-load-balancer-controller"
+  config  = module.latest.config
+}
+
+module "test-latest" {
+  source = "./tests"
+  digest = module.latest.image_ref
+}
+
+module "tagger" {
+  source = "../../tflib/tagger"
+
+  // The helm test installs into kube-system, and installs CRDs, so we can't run two of them at once.
+  // Instead, we just run the test for :latest, and expect that it means :latest-dev will also work.
+  depends_on = [
+    module.test-latest,
+    // module.test-latest-dev,
+  ]
+
+  tags = merge(
+    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
+    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest-dev.image_ref },
+  )
+}
