f93bc867ec86abe771d3238128e30c907e5e555c
Jason Hall <jason@chainguard.dev>
2023-06-26 10:57:39 -0400

    migrate dex to TF w/ helm test
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/dex/tests/runs.sh
+++ images/dex/tests/runs.sh
@@ -5,13 +5,10 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 PORT=5556
 
 URL="http://127.0.0.1:${PORT}"
-docker run --rm -d -p $PORT:$PORT "${IMAGE_NAME}" dex serve /etc/dex/config.docker.yaml
+docker run --rm -d -p ${FREE_PORT}:$PORT "${IMAGE_NAME}" dex serve /etc/dex/config.docker.yaml
 
-# Maximum number of attempts to check the health check endpoint
-MAX_ATTEMPTS=30
-
-# Delay (in seconds) between attempts
-DELAY=5
+MAX_ATTEMPTS=20 # Maximum number of attempts to check the health check endpoint
+DELAY=3         # Delay (in seconds) between attempts
 
 attempt=0
 until $(curl --output /dev/null --silent --head --fail "$URL/dex/healthz"); do
