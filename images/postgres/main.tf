a686a46e0a7125fbed07241922c1ca29780912f4
James Petersen <jpetersenames@gmail.com>
2023-06-27 15:23:56 -0600

    add postgres dev variant
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/postgres/main.tf
+++ images/postgres/main.tf
@@ -8,7 +8,16 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "dev" { source = "../../tflib/dev-subvariant" }
+
 module "latest" {
+  source = "../../tflib/publisher"
+
+  target_repository = var.target_repository
+  config            = file("${path.module}/configs/latest.apko.yaml")
+}
+
+module "latest-dev" {
   source = "../../tflib/publisher"
 
   target_repository = var.target_repository
@@ -26,12 +35,21 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
+module "test-latest-dev" {
+  source = "./tests"
+  digest = module.latest-dev.image_ref
+}
+
 module "tagger" {
   source = "../../tflib/tagger"
 
-  depends_on = [module.test-latest]
+  depends_on = [
+    module.test-latest,
+    module.test-latest-dev,
+  ]
 
   tags = merge(
     { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
+    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest-dev.image_ref },
   )
 }
