f125a1c17526aa29b9259d38ec44faa27d4360f6
Dan Lorenc <dlorenc@chainguard.dev>
2023-06-27 21:51:46 -0400

    Add ffpmeg test for h264.
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

--- images/ffmpeg/tests/02-convert.sh
+++ images/ffmpeg/tests/02-convert.sh
@@ -8,3 +8,13 @@ docker run --rm \
     "${IMAGE_NAME}" \
     -i sample.mov \
     /tmp/sample.mp4
+
+# Explicitly test h264 and aac codecs
+docker run --rm \
+    -v "${PWD}:/work" \
+    -w /work \
+    "${IMAGE_NAME}" \
+    -i sample.mov \
+    -vcodec h264 \
+    -acodec aac \
+    /tmp/sample.mp4
