9ef263fb34276698c98e4d40254a2e156604d79f
Jason Hall <jason@chainguard.dev>
2023-06-29 15:57:07 -0400

    migrate k8sgpt-operator to TF (#1010)
    * migrate k8sgpt-operator to TF
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * only test latest
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ images/k8sgpt-operator/tests/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+resource "helm_release" "k8sgpt-operator" {
+  name = "k8sgpt-operator"
+
+  repository       = "https://charts.k8sgpt.ai/"
+  chart            = "k8sgpt-operator"
+  create_namespace = true
+
+  values = [jsonencode({
+    manager = {
+      image = {
+        repository = data.oci_string.ref.registry_repo
+        tag        = data.oci_string.ref.pseudo_tag
+      }
+    }
+  })]
+}
