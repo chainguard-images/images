0a7302447e888099364844996046fc822ada6e8e
Matt Moore <mattmoor@chainguard.dev>
2023-06-29 12:37:22 -0700

    Migrate remaining prometheus exporters to TF
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/prometheus/tests/node-runs.sh
+++ images/prometheus/tests/node-runs.sh
@@ -7,9 +7,9 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 kubectl run prometheus-node-exporter --image=${IMAGE_NAME}
 kubectl wait --for=condition=ready pod prometheus-node-exporter
 
-kubectl port-forward pod/prometheus-node-exporter 9100:9100 &
+kubectl port-forward pod/prometheus-node-exporter ${FREE_PORT}:9100 &
 pid=$!
 trap "kill $pid" EXIT
 sleep 5
-curl http://localhost:9100/metrics | grep "node_"
+curl http://localhost:${FREE_PORT}/metrics | grep "node_"
 
