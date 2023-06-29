d0a327c783060339ae8033828cb9773e99a2f1c1
Jason Hall <jason@chainguard.dev>
2023-06-29 16:46:18 -0400

    lint check that all modules are used in the main module (#1014)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+for m in `find . -name main.tf | grep ./images/ | cut -d/ -f3 | uniq`; do
+  if ! grep -q "module.*$m" main.tf; then
+    echo "module $m is not used"
+    exit 1
+  fi
+done
