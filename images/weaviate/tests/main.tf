56f69e123bcad15ef07eac477b288ab4b81d63c5
Matt Moore <mattmoor@chainguard.dev>
2023-06-29 13:45:13 -0700

    Port weaviate to TF
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/weaviate/tests/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    oci    = { source = "chainguard-dev/oci" }
+    random = { source = "hashicorp/random" }
+    helm   = { source = "hashicorp/helm" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+data "oci_exec_test" "help" {
+  digest = var.digest
+  script = "${path.module}/01-help.sh"
+}
+
+resource "random_pet" "suffix" {}
+
+resource "helm_release" "weaviate" {
+  name = "weaviate-${random_pet.suffix.id}"
+
+  repository = "https://weaviate.github.io/weaviate-helm"
+  chart      = "weaviate"
+
+  namespace        = "weaviate-${random_pet.suffix.id}"
+  create_namespace = true
+
+  # Split the digest ref into repository and digest. The helm chart expects a
+  # tag, but it just appends it to the repository again, so we just specify a
+  # dummy tag and the digest to test.
+  set {
+    name  = "image.registry"
+    value = data.oci_string.ref.registry
+  }
+  set {
+    name  = "image.repo"
+    value = data.oci_string.ref.repo
+  }
+  set {
+    name  = "image.tag"
+    value = data.oci_string.ref.pseudo_tag
+  }
+}
