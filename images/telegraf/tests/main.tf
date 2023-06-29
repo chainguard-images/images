fd611f5f566279e97b27c0f13a8bcd5a521067b0
Josh Wolf <josh@wolfs.io>
2023-06-29 11:35:33 -0400

    migrate telegraf to tf
    Signed-off-by: Josh Wolf <josh@wolfs.io>

---
+++ images/telegraf/tests/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    oci    = { source = "chainguard-dev/oci" }
+    random = { source = "hashicorp/random" }
+    helm   = { source = "hashicorp/helm" }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" {
+  input = var.digest
+}
+
+resource "random_pet" "suffix" {}
+
+resource "helm_release" "telegraf" {
+  name = "telegraf-${random_pet.suffix.id}"
+
+  repository = "https://helm.influxdata.com"
+  chart      = "telegraf"
+
+  namespace        = "telegraf-${random_pet.suffix.id}"
+  create_namespace = true
+
+  values = [
+    jsonencode({
+      image = {
+        repo = data.oci_string.ref.registry_repo
+        tag  = data.oci_string.ref.pseudo_tag
+      }
+    }),
+  ]
+}
