e20db17b1a4fa99b0d0936a2b51ac4f5a24c27ec
Matt Moore <mattmoor@chainguard.dev>
2023-06-29 16:27:18 -0700

    Post envoy-ratelimit to TF (#1020)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/envoy-ratelimit/tests/02-helm.sh
+++ images/envoy-ratelimit/tests/02-helm.sh
@@ -1,27 +1,6 @@
 #!/usr/bin/env bash
 
-# monopod:tag:k8s
-
 set -o errexit -o nounset -o errtrace -o pipefail -x
-
-function preflight() {
-	if [[ "${IMAGE_REGISTRY}" == "" ]]; then
-		echo "Must set IMAGE_REGISTRY environment variable. Exiting."
-		exit 1
-	fi
-
-	if [[ "${IMAGE_REPOSITORY}" == "" ]]; then
-		echo "Must set IMAGE_REPOSITORY environment variable. Exiting."
-		exit 1
-	fi
-
-	if [[ "${IMAGE_TAG}" == "" ]]; then
-		echo "Must set IMAGE_TAG environment variable. Exiting."
-		exit 1
-	fi
-}
-
-preflight
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm upgrade --install redis bitnami/redis \
@@ -38,7 +17,7 @@ helm repo add xyctruth https://xyctruth.github.io/istio-ratelimit
 helm install ratelimit xyctruth/istio-ratelimit \
     --set ratelimit.image.repository="${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}" \
     --set ratelimit.image.tag="${IMAGE_TAG}"
-	
+
 kubectl set env deployment/ratelimit-istio-ratelimit REDIS_URL=ratelimit-redis-master:6379
 
 # This needs a long time to come up, the redis deployment is pretty slow.
