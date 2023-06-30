f3b0cacee1613e73d9b3fd71ae263191d034dd52
Matt Moore <mattmoor@chainguard.dev>
2023-06-30 09:32:20 -0700

    Port trust-manager to TF (#1030)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/trust-manager/tests/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "${path.module}/01-version.sh"
+}
+
+data "oci_exec_test" "helm-install" {
+  digest = var.digest
+  script = "${path.module}/02-helm-install.sh"
+
+  env {
+    name  = "IMAGE_REGISTRY"
+    value = data.oci_string.ref.registry
+  }
+  env {
+    name  = "IMAGE_REPOSITORY"
+    value = data.oci_string.ref.repo
+  }
+  env {
+    name  = "IMAGE_TAG"
+    value = data.oci_string.ref.pseudo_tag
+  }
+}
