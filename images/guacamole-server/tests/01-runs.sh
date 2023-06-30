68a3d4f232377f35c89df2d21b7a82a03d2c0e6f
James Petersen <jpetersenames@gmail.com>
2023-06-30 12:41:43 -0600

    add the client libraries for guacamole (#1032)
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/guacamole-server/tests/01-runs.sh
+++ images/guacamole-server/tests/01-runs.sh
@@ -6,4 +6,13 @@ container_name="guacamole-${RANDOM}"
 docker run --rm -d --name="${container_name}" "${IMAGE_NAME}"
 trap "docker rm -f ${container_name}" EXIT
 
-docker logs "${container_name}" 2>&1  | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"
+# Poll the logs for the startup string
+for i in {1..5}; do
+  if docker logs "${container_name}" 2>&1  | grep "Guacamole proxy daemon (guacd) version 1.5.2 started"; then
+    exit 0
+  fi
+  sleep 1
+done
+
+# Fail if the startup string was not found
+exit 1
