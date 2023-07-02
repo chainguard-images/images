22cc13134a395f85ded3c97b2ce3426453147890
dlorenc <lorenc.d@gmail.com>
2023-07-02 08:44:50 -0400

    Add wasmer image (#1042)

--- main.tf
+++ main.tf
@@ -712,6 +712,11 @@ module "wait-for-it" {
   target_repository = "${var.target_repository}/wait-for-it"
 }
 
+module "wasmer" {
+  source            = "./images/wasmer"
+  target_repository = "${var.target_repository}/wasmer"
+}
+
 module "wasmtime" {
   source            = "./images/wasmtime"
   target_repository = "${var.target_repository}/wasmtime"
