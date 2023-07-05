76d11c61639797e437b8880059db60a2706e7216
Jason Hall <jason@chainguard.dev>
2023-07-05 14:41:02 -0400

    add pulumi tests to TF (#1057)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/pulumi/tests/main.tf
+++ images/pulumi/tests/main.tf
@@ -12,3 +12,9 @@ data "oci_exec_test" "runs" {
   digest = var.digest
   script = "${path.module}/01-runs.sh"
 }
+
+data "oci_exec_test" "install" {
+  digest          = var.digest
+  script          = "${path.module}/02-k8s-install-all-languages.sh"
+  timeout_seconds = 10 * 60
+}
