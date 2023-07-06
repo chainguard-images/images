d78d04dabd0419cb021b8aafe2ba0fc7f5784658
Josh Wolf <josh@wolfs.io>
2023-07-06 10:50:18 -0400

    ensure k3s-embedded variant is tagged appropriately (#1064)
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- images/k3s/main.tf
+++ images/k3s/main.tf
@@ -67,8 +67,8 @@ module "tagger" {
   tags = merge(
     { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
     { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest-dev.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags-embedded.tag_list)) : "${t}-embedded" => module.latest-embedded.image_ref },
     { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-images" => module.latest-images.image_ref },
     { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-images-dev" => module.latest-images-dev.image_ref },
+    { for t in toset(concat(["latest"], module.version-tags-embedded.tag_list)) : t => module.latest-embedded.image_ref },
   )
 }
