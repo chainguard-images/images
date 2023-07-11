ff1d3ccf663855c13f0384240ce078ac6622424e
Furkan Türkal <furkan.turkal@chainguard.dev>
2023-07-11 22:33:29 +0300

    Add aws-for-fluent-bit
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>

---
+++ images/aws-for-fluent-bit/tests/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "docker run --rm $IMAGE_NAME --version"
+}
+
+resource "helm_release" "fluent-bit" {
+  name = "fluent-bit"
+
+  repository = "https://fluent.github.io/helm-charts"
+  chart      = "fluent-bit"
+
+  values = [jsonencode({
+    image = {
+      repository = data.oci_string.ref.registry_repo
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  })]
+}
