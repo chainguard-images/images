69ac880276765b83bed3d8d5f221511a9d696cde
Erika Heidi <erika@erikaheidi.com>
2023-07-17 11:12:38 -0300

    Increasing sleep time
    Signed-off-by: Erika Heidi <erika@erikaheidi.com>

--- images/php/tests/fpm-server.sh
+++ images/php/tests/fpm-server.sh
@@ -5,7 +5,7 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 LOGFILE=$(mktemp)
 ID=$(docker run --rm -d $IMAGE_NAME)
 docker attach $ID 2> $LOGFILE &
-sleep 3
+sleep 5
 docker stop $ID
 
 grep "ready to handle connections" $LOGFILE
