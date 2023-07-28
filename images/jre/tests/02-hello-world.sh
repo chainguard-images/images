b08d36f5caf0bc26c3061b98125b659eed674c7a
Matt Moore <mattmoor@chainguard.dev>
2023-07-28 09:34:29 -0700

    Add support for earlier JREs
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/jre/tests/02-hello-world.sh
+++ images/jre/tests/02-hello-world.sh
@@ -18,8 +18,8 @@ chmod 777 "${TMP}"
 docker run --rm -v $(pwd)/HelloWorld.java:/src/HelloWorld.java -v "${TMP}:/tmp" \
   `# Build using the latest JDK image` \
   --entrypoint "javac" "${SDK_IMAGE}" \
-  `# Targeting Java 11 so that all our JREs can run the produced .class file` \
-  -source 11 -target 11 \
+  `# Targeting Java 8 so that all our JREs can run the produced .class file` \
+  -source 8 -target 8 \
   /src/HelloWorld.java -d /tmp
 
 # Now we have the .class file, run it to test our JRE.
