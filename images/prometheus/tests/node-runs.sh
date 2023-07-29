10133efeddce31cbcdcf398e73b78e5f844c45ba
Matt Moore <mattmoor@chainguard.dev>
2023-07-29 13:41:28 -0700

    Expand the list of images tested with prometheus helm chart
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/prometheus/tests/node-runs.sh
+++ images/prometheus/tests/node-runs.sh
@@ -2,12 +2,8 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-kubectl run prometheus-node-exporter --image=${IMAGE_NAME}
-kubectl wait --for=condition=ready pod prometheus-node-exporter
-
-kubectl port-forward pod/prometheus-node-exporter ${FREE_PORT}:9100 &
+kubectl port-forward -n prometheus service/prometheus-prometheus-node-exporter ${FREE_PORT}:9100 &
 pid=$!
 trap "kill $pid" EXIT
 sleep 5
 curl http://localhost:${FREE_PORT}/metrics | grep "node_"
-
