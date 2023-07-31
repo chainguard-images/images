452e8adf419f4bb91f9ba6ffebb002d7ec31c8ec
Josh Wolf <josh@wolfs.io>
2023-07-31 15:41:27 -0400

    add k3s-embedded-dev, remove k3s-images-dev
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- images/k3s/embedded.tf
+++ images/k3s/embedded.tf
@@ -7,6 +7,17 @@ module "latest-embedded" {
   config            = file("${path.module}/configs/latest.embedded.apko.yaml")
 }
 
+module "latest-embedded-dev" {
+  source = "../../tflib/publisher"
+
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  config            = jsonencode(module.latest.config)
+  # k3s-embedded already contains the [critctl, kubectl, ctr]
+  # packages, but we add the default CG dev packages here
+  extra_packages = module.dev.extra_packages
+}
+
 module "test-embedded" {
   source = "./tests"
   digest = module.latest-embedded.image_ref
