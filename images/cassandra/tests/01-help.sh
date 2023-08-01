aa6195702760fd9b7fce3d86682cb739e3667531
Mritunjay <mritunjay.sharma@chainguard.dev>
2023-08-01 19:22:32 +0530

    testing with new chanegs
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>

--- images/cassandra/tests/01-help.sh
+++ images/cassandra/tests/01-help.sh
@@ -2,4 +2,17 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-docker run --rm $IMAGE_NAME --help
+container_name="cassandra-${RANDOM}"
+docker run --rm -d --name="${container_name}" "${IMAGE_NAME}"
+trap "docker rm -f ${container_name}" EXIT
+
+# Poll the logs for the startup string
+for i in {1..15}; do
+  if docker logs "${container_name}" 2>&1  | grep "Startup complete"; then
+    exit 0
+  fi
+  sleep 1
+done
+
+# Fail if the startup string was not found
+exit 1
