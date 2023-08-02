c50b03fa81e034c4ac8818b565fe3dadbd06432f
Josh Wolf <josh@wolfs.io>
2023-08-02 07:22:49 -0400

    add k3s-aio, remove k3s-{embedded,images}
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- images/k3s/main.tf
+++ images/k3s/main.tf
@@ -25,19 +25,7 @@ module "latest-dev" {
   name              = basename(path.module)
   target_repository = var.target_repository
   config            = jsonencode(module.latest.config)
-  extra_packages = concat(module.dev.extra_packages, [
-    "crictl",
-    "kubectl",
-    "ctr",
-  ])
-}
-
-module "latest-images" {
-  source = "../../tflib/publisher"
-
-  name              = basename(path.module)
-  target_repository = "${var.target_repository}-images"
-  config            = file("${path.module}/configs/latest.images.apko.yaml")
+  extra_packages    = module.dev.extra_packages
 }
 
 module "version-tags" {
@@ -56,7 +44,6 @@ module "tagger" {
 
   depends_on = [
     module.test-latest,
-    module.test-embedded,
   ]
 
   tags = merge(
@@ -65,29 +52,17 @@ module "tagger" {
   )
 }
 
-module "tagger-images" {
+module "tagger-aio" {
   source = "../../tflib/tagger"
 
+  # k3s-aio is identical to k3s except for the included image bits, so
+  # functionally we can share the same test
   depends_on = [
     module.test-latest,
-    module.test-embedded,
   ]
 
   tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest-images.image_ref },
-  )
-}
-
-module "tagger-embedded" {
-  source = "../../tflib/tagger"
-
-  depends_on = [
-    module.test-latest,
-    module.test-embedded,
-  ]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags-embedded.tag_list)) : t => module.latest-embedded.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags-embedded.tag_list)) : "${t}-dev" => module.latest-embedded-dev.image_ref },
+    { for t in toset(concat(["latest"], module.version-tags-aio.tag_list)) : t => module.latest-aio.image_ref },
+    { for t in toset(concat(["latest"], module.version-tags-aio.tag_list)) : "${t}-dev" => module.latest-aio-dev.image_ref },
   )
 }
