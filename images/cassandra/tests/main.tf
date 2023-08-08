d640a1ccc3e5b72a91b616c1e36c43040c8fae49
Mritunjay <mritunjay.sharma@chainguard.dev>
2023-08-09 01:58:32 +0530

    uses kubectl manifest
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>

--- images/cassandra/tests/main.tf
+++ images/cassandra/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci     = { source = "chainguard-dev/oci" }
+    kubectl = { source = "gavinbunney/kubectl" }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.11.0"
@@ -26,16 +27,16 @@ data "oci_exec_test" "test" {
   script = "${path.module}/${each.value}"
 }
 
-resource "kubernetes_manifest" "prerequisitesS" {
+resource "kubernetes_manifest" "prerequisites" {
   for_each = fileset(path.module, "manifests/*.yaml")
   manifest = yamldecode(file("${path.module}/${each.value}"))
 }
 
-resource "kubernetes_manifest" "stateful_set" {
-  manifest = yamldecode(templatefile("${path.module}/manifests/cassandra-stateful-set.yaml.tpl",
+resource "kubectl_manifest" "stateful_set" {
+  depends_on = [kubernetes_manifest.prerequisites]
+  yaml_body = templatefile("${path.module}/manifests/cassandra-stateful-set.yaml.tpl",
     {
       cassandra_tag  = data.oci_string.ref.pseudo_tag
       cassandra_repo = data.oci_string.ref.registry_repo
-    })
-  )
+  })
 }
