72d08acfc1309564850529e5563c98b8de2db122
Jon Johnson <jon.johnson@chainguard.dev>
2023-08-09 12:15:14 -0700

    Pre-pull image in base image tests (#1196)
    Signed-off-by: Jon Johnson <jon.johnson@chainguard.dev>

--- images/go/tests/base-image-custom-entrypoint.sh
+++ images/go/tests/base-image-custom-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
+# Using registry.local:5000 as the FROM in `docker build` fails with
+# current versions of docker that use containerd under the hood.
+# Pre-pulling it uses docker's heuristics for allowing insecure registries.
+docker pull ${IMAGE_NAME}
+
 # The image can be used as a base image with a custom entrypoint.
 cat <<EOF | docker build -t go-version-entrypoint -
 FROM ${IMAGE_NAME}
