bde53effbff6db36bd5139f8ff3cdf4645b572f4
James Strong <strong.james.e@gmail.com>
2023-08-09 14:39:40 -0400

    ingress is running, now to test
    Signed-off-by: James Strong <strong.james.e@gmail.com>

--- images/ingress-nginx-controller/tests/main.tf
+++ images/ingress-nginx-controller/tests/main.tf
@@ -14,7 +14,20 @@ variable "image_tag" {
   description = "image tag to pass to helm"
 }
 
+resource "helm_release" "cert-manager" {
+ name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "1.12.0"
+  set {
+   name = "installCRDs"
+   value = true 
+  }
+}
 resource "helm_release" "ingress-nginx-controller" {
+  depends_on = [ helm_release.cert-manager ]
   name = "ingress-nginx"
 
   repository = "https://kubernetes.github.io/ingress-nginx"
@@ -32,6 +45,11 @@ resource "helm_release" "ingress-nginx-controller" {
           registry = data.oci_string.ref.registry
           digest   = data.oci_string.ref.digest
         }
+        admissionWebhooks = { 
+          certManager = {
+            enabled = true
+          }
+        } 
         livenessProbe = {}
         startupProbe = {}
         readinessProbe = {}
