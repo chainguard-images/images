6202f6fbaab3633df421eb2d09f107a6d0e1f012
Jason Hall <jason@chainguard.dev>
2023-08-09 16:38:36 -0400

    go: promote 1.21 to latest
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/go/main.tf
+++ images/go/main.tf
@@ -21,16 +21,15 @@ module "tagger" {
 
   tags = merge(
     # 1.19
-    { for t in toset(module.version-tags-19.tag_list) : t => module.nineteen.image_ref },
-    { for t in toset(module.version-tags-19.tag_list) : "${t}-dev" => module.nineteen-dev.image_ref },
-
-    # 1.21
-    # TODO(mattmoor): Move this below (and switch latest) once 1.21 is no longer an "RC"
-    { for t in toset(module.version-tags-21.tag_list) : t => module.twenty-one.image_ref },
-    { for t in toset(module.version-tags-21.tag_list) : "${t}-dev" => module.twenty-one-dev.image_ref },
+    { for t in module.version-tags-19.tag_list : t => module.nineteen.image_ref },
+    { for t in module.version-tags-19.tag_list : "${t}-dev" => module.nineteen-dev.image_ref },
 
     # 1.20
-    { for t in toset(concat(["latest"], module.version-tags-20.tag_list)) : t => module.twenty.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags-20.tag_list)) : "${t}-dev" => module.twenty-dev.image_ref },
+    { for t in module.version-tags-20.tag_list : t => module.twenty.image_ref },
+    { for t in module.version-tags-20.tag_list : "${t}-dev" => module.twenty-dev.image_ref },
+
+    # 1.21
+    { for t in concat(["latest"], module.version-tags-21.tag_list) : t => module.twenty-one.image_ref },
+    { for t in concat(["latest"], module.version-tags-21.tag_list) : "${t}-dev" => module.twenty-one-dev.image_ref },
   )
 }
