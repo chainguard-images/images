47a219ded847e5bec071a35a6f3fac1fb8e448fc
Mauren Berti <698465+stormqueen1990@users.noreply.github.com>
2023-08-10 17:57:59 -0400

    fix: use IMAGE_NAME for Gradle test script (#1201)
    Target image was previously hardcoded instead of using the current IMAGE_NAME
    environment variable. Updated to use the current IMAGE_NAME environment variable
    sent from Terraform.
    
    Signed-off-by: Mauren Berti <mauren.berti@chainguard.dev>

--- images/gradle/tests/02-build.sh
+++ images/gradle/tests/02-build.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
- docker run --rm --entrypoint "" cgr.dev/chainguard/gradle:latest sh -c "gradle init --type java-application --test-framework junit-jupiter && gradle build"
+docker run --rm --entrypoint "" "${IMAGE_NAME}" sh -c "gradle init --type java-application --test-framework junit-jupiter && gradle build"
