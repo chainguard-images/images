b07c16a7e232a94b222e1f608b64a7549db6ae78
Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
2023-08-17 00:53:28 +0300

    remove unnecassary test file
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>

--- images/kubeflow-jupyter-web-app/tests/main.tf
+++ images/kubeflow-jupyter-web-app/tests/main.tf
@@ -9,8 +9,6 @@ variable "digest" {
 }
 
 data "oci_exec_test" "test" {
-  for_each = toset(fileset(path.module, "*.sh"))
-
   digest = var.digest
-  script = "${path.module}/${each.value}"
+  script = "${path.module}/full-test.sh"
 }
