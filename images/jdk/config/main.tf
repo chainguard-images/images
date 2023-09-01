fc964c7de0ebdaa17d242efb517b3052d11b0c78
Matt Moore <mattmoor@chainguard.dev>
2023-09-01 13:36:15 -0700

    Migrate our JDK images to follow #1172 (#1296)
    This starts to reduce our reliance on version tags and eliminate boilerplate.
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/jdk/config/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+# TODO: We should have virtual packages that we use here
+# that the appropriate versions "provide".
+
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. openjdk-17)."
+  default     = ["openjdk-17", "openjdk-17-default-jvm"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
