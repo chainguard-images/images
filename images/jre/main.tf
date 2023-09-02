6a97c7b8016d4250a5493badf7ab05db08910b6a
Matt Moore <mattmoor@chainguard.dev>
2023-09-02 12:02:27 -0600

    Migrate our JRE images to follow #1172 (#1305)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/jre/main.tf
+++ images/jre/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -8,28 +9,44 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "latest-config" { source = "./config" }
+
+module "latest" {
+  source = "../../tflib/publisher"
+
+  name = basename(path.module)
+
+  target_repository = var.target_repository
+  config            = module.latest-config.config
+}
+
 module "dev" { source = "../../tflib/dev-subvariant" }
 
-module "tagger" {
-  source = "../../tflib/tagger"
+module "latest-dev" {
+  source = "../../tflib/publisher"
 
-  depends_on = [
-    module.test-eleven,
-    module.test-seventeen,
-  ]
+  name = basename(path.module)
 
-  tags = merge(
-    # jre 11.0.20.4
-    { for t in toset(module.version-tags-eleven-zero-twenty-four.tag_list) : "openjdk-${t}" => module.eleven-zero-twenty-four.image_ref },
+  target_repository = var.target_repository
+  # Make the dev variant an explicit extension of the
+  # locked original.
+  config         = jsonencode(module.latest.config)
+  extra_packages = module.dev.extra_packages
+}
 
-    # jre 17.0.8.2
-    { for t in toset(module.version-tags-seventeen-zero-eight-two.tag_list) : "openjdk-${t}" => module.seventeen-zero-eight-two.image_ref },
+module "test-latest" {
+  source = "./tests"
+  digest = module.latest.image_ref
+}
 
-    { for t in toset(module.version-tags-11.tag_list) : "openjdk-${t}" => module.eleven.image_ref },
-    { for t in toset(module.version-tags-11.tag_list) : "openjdk-${t}-dev" => module.eleven-dev.image_ref },
-    { for t in toset(module.version-tags-17.tag_list) : "openjdk-${t}" => module.seventeen.image_ref },
-    { for t in toset(module.version-tags-17.tag_list) : "openjdk-${t}-dev" => module.seventeen-dev.image_ref },
+resource "oci_tag" "latest" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
 
-    { "latest" : module.seventeen.image_ref, "latest-dev" : module.seventeen-dev.image_ref },
-  )
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest-dev.image_ref
+  tag        = "latest-dev"
 }
