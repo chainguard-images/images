c8a18419c1c5446bffac82c65fea92511929ce7a
Dan Lorenc <dlorenc@chainguard.dev>
2023-09-02 09:45:28 -0400

    Add promtail image.
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

---
+++ images/promtail/tests/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+resource "helm_release" "promtail" {
+  name = "promtail"
+
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "promtail"
+
+  values = [jsonencode({
+    image = {
+      registry   = data.oci_string.ref.registry
+      repository = data.oci_string.ref.repo
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  })]
+}
+
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "docker run --rm $IMAGE_NAME --version"
+}
+
+data "oci_exec_test" "use" {
+  digest = var.digest
+  script = "${path.module}/01-use.sh"
+}
