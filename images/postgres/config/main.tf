51966389db58a3998320177fe51c1f4abcfea912
Matt Moore <mattmoor@chainguard.dev>
2023-09-02 13:06:40 -0600

    Migrate our Postgres image to follow #1172
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/postgres/config/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. postgresql-15)."
+  default     = ["postgresql-15", "postgresql-15-client", "postgresql-15-oci-entrypoint", "postgresql-15-contrib", "libpq-15"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
