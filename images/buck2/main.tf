62c8b677628d1c0ecc7faf7ddda0aabcf83d08c1
Matt Moore <mattmoor@chainguard.dev>
2023-09-04 19:32:37 -0700

    Refactor buck2 to drop version tags
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/buck2/main.tf
+++ images/buck2/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -8,13 +9,15 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "latest-config" { source = "./config" }
+
 module "latest" {
   source = "../../tflib/publisher"
 
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.latest-config.config
 }
 
 module "dev" { source = "../../tflib/dev-subvariant" }
@@ -31,24 +34,19 @@ module "latest-dev" {
   extra_packages = module.dev.extra_packages
 }
 
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "buck2"
-  config  = module.latest.config
-}
-
 module "test-latest" {
   source = "./tests"
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
   depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
 
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest-dev.image_ref },
-  )
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest-dev.image_ref
+  tag        = "latest-dev"
 }
