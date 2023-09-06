94cba7147905bc349986d9bf276d718b4b8d5a9c
Matt Moore <mattmoor@chainguard.dev>
2023-09-06 12:54:21 -0700

    Refactor secret-store-csi-driver-gcp, skaffold, slim-toolkit-debug, terraform, trust-manager to drop version tags
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/slim-toolkit-debug/main.tf
+++ images/slim-toolkit-debug/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -8,31 +9,24 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "slim-toolkit-debug-latest" {
+module "latest-config" { source = "./config" }
+
+module "latest" {
   source = "../../tflib/publisher"
 
-  name              = basename(path.module)
+  name = basename(path.module)
+
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.latest-config.config
 }
 
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "bash"
-  config  = module.slim-toolkit-debug-latest.config
-}
-
-module "test-slim-toolkit-debug-latest" {
+module "test-latest" {
   source = "./tests"
-  digest = module.slim-toolkit-debug-latest.image_ref
+  digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
-  depends_on = [module.test-slim-toolkit-debug-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.slim-toolkit-debug-latest.image_ref },
-  )
+resource "oci_tag" "latest" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
