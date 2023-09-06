5c8bd2889a4e7ba2fad72405044bc1ff8b3125a8
Matt Moore <mattmoor@chainguard.dev>
2023-09-06 13:15:41 -0700

    Refactor vela-cli, vt, wasmer, wasmtime, wazero, weaviate, zig, zot to drop version tags
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/vela-cli/main.tf
+++ images/vela-cli/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     apko = { source = "chainguard-dev/apko" }
+    oci  = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -8,19 +9,15 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "latest-config" { source = "./config" }
+
 module "latest" {
   source = "../../tflib/publisher"
 
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
-}
-
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "kubevela"
-  config  = module.latest.config
+  config            = module.latest-config.config
 }
 
 module "test-latest" {
@@ -28,12 +25,8 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
   depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-  )
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
