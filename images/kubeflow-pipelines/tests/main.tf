32a06d32fb638a503afd6328ea704aa15f670ae3
Furkan Türkal <furkan.turkal@chainguard.dev>
2023-09-09 00:44:50 +0300

    add tests for kubeflow-pipeline
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    Co-authored-by: Batuhan <batuhan.apaydin@chainguard.dev>

--- images/kubeflow-pipelines/tests/main.tf
+++ images/kubeflow-pipelines/tests/main.tf
@@ -20,3 +20,37 @@ data "oci_string" "ref" {
   for_each = var.digests
   input    = each.value
 }
+
+data "oci_exec_test" "smoke" {
+  digest = var.digests.controller # This doesn't actually matter here, just pass it something valid
+  script = "${path.module}/smoke-test.sh"
+
+  env {
+    name  = "IMAGE_REPOSITORY_APISERVER"
+    value = var.digests["api-server"]
+  }
+  env {
+    name  = "IMAGE_REPOSITORY_CACHESERVER"
+    value = var.digests["cache-server"]
+  }
+  env {
+    name  = "IMAGE_REPOSITORY_METADATAWRITER"
+    value = var.digests["metadata-writer"]
+  }
+  env {
+    name  = "IMAGE_REPOSITORY_PERSISTENCEAGENT"
+    value = var.digests["persistenceagent"]
+  }
+  env {
+    name  = "IMAGE_REPOSITORY_SCHEDULEDWORKFLOW"
+    value = var.digests["scheduledworkflow"]
+  }
+  env {
+    name  = "IMAGE_REPOSITORY_VIEWERCRDCONTROLLER"
+    value = var.digests["viewer-crd-controller"]
+  }
+  env {
+    name  = "IMAGE_TAG"
+    value = "latest"
+  }
+}
---
+++ images/kubeflow-pipelines/tests/smoke-test.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+# Ref: https://www.kubeflow.org/docs/components/pipelines/v1/installation/localcluster-deployment/#deploying-kubeflow-pipelines
+
+NAMESPACE=kubeflow
+PIPELINE_VERSION=2.0.1
+
+function manifests() {
+  cat <<EOF > kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=${PIPELINE_VERSION}
+images:
+  - name: gcr.io/ml-pipeline/api-server
+    newName: ${IMAGE_REPOSITORY_APISERVER}
+    newTag: ${IMAGE_TAG}
+  - name: gcr.io/ml-pipeline/cache-server
+    newName: ${IMAGE_REPOSITORY_CACHESERVER}
+    newTag: ${IMAGE_TAG}
+  - name: gcr.io/ml-pipeline/metadata-writer
+    newName: ${IMAGE_REPOSITORY_METADATAWRITER}
+    newTag: ${IMAGE_TAG}
+  - name: gcr.io/ml-pipeline/persistenceagent
+    newName: ${IMAGE_REPOSITORY_PERSISTENCEAGENT}
+    newTag: ${IMAGE_TAG}
+  - name: gcr.io/ml-pipeline/scheduledworkflow
+    newName: ${IMAGE_REPOSITORY_SCHEDULEDWORKFLOW}
+    newTag: ${IMAGE_TAG}
+  - name: gcr.io/ml-pipeline/viewer-crd-controller
+    newName: ${IMAGE_REPOSITORY_VIEWERCRDCONTROLLER}
+    newTag: ${IMAGE_TAG}
+namespace: ${NAMESPACE}
+EOF
+}
+
+kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io --namespace ${NAMESPACE}
+
+manifests
+kubectl apply -k .
+
+kubectl wait --for=condition=ready pod --selector app=ml-pipeline
+kubectl wait --for=condition=ready pod --selector app=cache-server
+kubectl wait --for=condition=ready pod --selector app=metadata-writer
+kubectl wait --for=condition=ready pod --selector app=ml-pipeline-persistenceagent
+kubectl wait --for=condition=ready pod --selector app=ml-pipeline-scheduledworkflow
+kubectl wait --for=condition=ready pod --selector app=ml-pipeline-viewer-crd
