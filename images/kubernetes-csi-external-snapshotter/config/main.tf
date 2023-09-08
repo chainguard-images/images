930de9a067b3c980d247debf5afceaf2527bf765
Matt Moore <mattmoor@chainguard.dev>
2023-09-07 18:09:16 -0600

    Refactor a number of images to drop version tags (#1324)
    * Refactor buck2 to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor cadvisor to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor cedar, clang, conda, configmap-reload, crane to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor dive, ffmpeg, gcloud, helm-chartmuseum to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Drop the clang packages from the template
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor helm, hugo, k8s-sidecar, k8sgpt, k8sgpt-operator to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Add virtual packages to templates
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor kafka, kube-downscaler, kubeflow-jupyter-web-app, kubernetes-csi-external-{attacher,provisioner,snapshotter} to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor kubewatch, loki, mdbook, meilisearch, memcached-exporter to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor metacontroller, netcat, ntpd-rs, nvidia-device-plugin, openai, opensearch to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor paranoia, promtail, r-base, rqlite to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor secret-store-csi-driver-gcp, skaffold, slim-toolkit-debug, terraform, trust-manager to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor vela-cli, vt, wasmer, wasmtime, wazero, weaviate, zig, zot to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Refactor opentf to drop version tags
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    ---------
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/kubernetes-csi-external-snapshotter/config/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. kubernetes-csi-external-snapshotter)."
+  default     = ["kubernetes-csi-external-snapshotter"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
