3dd5bcbd7526ba635cdf0389ea7daec9f50bc8f7
Jason Hall <jason@chainguard.dev>
2023-09-14 10:47:39 -0400

    add template directory to be used for new image configs (#1366)
    * add template directory to be used for new image configs
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * ignore template in lint.sh
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * review feedback, better version stream image TODOs
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * review feedback
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- lint.sh
+++ lint.sh
@@ -3,6 +3,9 @@
 set -e
 
 for m in `find . -name main.tf | grep ./images/ | cut -d/ -f3 | uniq`; do
+  if [ "$m" == "TEMPLATE" ]; then
+    continue
+  fi
   if ! grep -q "module.*$m" main.tf; then
     echo "module $m is not used"
     exit 1
