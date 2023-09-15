e370cf67ef079363736c35da9d8b4c6411fe13a5
Nghia Tran <tcnghia@gmail.com>
2023-09-15 13:08:18 -0700

    Add namespace to csi-external-attacher tests (#1393)
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/kubernetes-csi-external-attacher/tests/deploy.sh
+++ images/kubernetes-csi-external-attacher/tests/deploy.sh
@@ -2,11 +2,17 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-curl https://raw.githubusercontent.com/kubernetes-csi/external-attacher/master/deploy/kubernetes/deployment.yaml | \
-    sed "s|registry.k8s.io/k8s-staging-sig-storage/csi-attacher:.*|${IMAGE_NAME}|g" | \
-    kubectl apply -f -
-kubectl apply -f  https://raw.githubusercontent.com/kubernetes-csi/external-attacher/master/deploy/kubernetes/rbac.yaml
+: "${NAMESPACE:=default}"
+kubectl create ns $NAMESPACE || true
+
+curl https://raw.githubusercontent.com/kubernetes-csi/external-attacher/master/deploy/kubernetes/deployment.yaml \
+    | sed "s|registry.k8s.io/k8s-staging-sig-storage/csi-attacher:.*|${IMAGE_NAME}|g" \
+    | kubectl apply -n $NAMESPACE -f -
+
+curl https://raw.githubusercontent.com/kubernetes-csi/external-attacher/master/deploy/kubernetes/rbac.yaml \
+    | sed "s|namespace: default|namespace: $NAMESPACE|g" \
+    | kubectl apply -n $NAMESPACE -f -
 
 sleep 5
 
-kubectl rollout status deployment csi-attacher --timeout=120s
+kubectl rollout status deployment -n $NAMESPACE csi-attacher --timeout=120s
--- images/kubernetes-csi-external-attacher/tests/main.tf
+++ images/kubernetes-csi-external-attacher/tests/main.tf
@@ -8,7 +8,13 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+resource "random_pet" "suffix" {}
+
 data "oci_exec_test" "deploy" {
   digest = var.digest
   script = "${path.module}/deploy.sh"
+  env {
+    name  = "NAMESPACE"
+    value = "csi-attacher-${random_pet.suffix.id}"
+  }
 }
