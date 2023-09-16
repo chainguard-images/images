781cdd02d8031d024882dac524c93119d791fdb6
Tuan Anh Tran <me@tuananh.org>
2023-09-16 16:00:24 +0700

    sleep before kubectl wait test
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- images/bank-vaults/tests/full-test.sh
+++ images/bank-vaults/tests/full-test.sh
@@ -13,5 +13,11 @@ helm upgrade --install --wait vault-operator oci://ghcr.io/bank-vaults/helm-char
 kubectl kustomize https://github.com/bank-vaults/vault-operator/deploy/rbac | kubectl apply -f -
 
 kubectl apply -f https://raw.githubusercontent.com/bank-vaults/vault-operator/v1.21.0/deploy/examples/cr-raft.yaml
-    
-kubectl wait pods -n default -l "app.kubernetes.io/name=vault" --for=condition=Ready --timeout=300s
+
+# when we create Vault instance, the new statefulset & pod takes awhile to be created by the operator
+# so we sleep 10 seconds here. Otherwise, the next command will fail (no pod with that label)
+# not sure what's the best practices here
+
+sleep 10
+
+kubectl wait -n default -l statefulset.kubernetes.io/pod-name=vault-0 --for=condition=ready pod --timeout=300s
