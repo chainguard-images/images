21c719cfc07a135e8e4f6ee1d72408b519e4b61d
Tuan Anh Tran <me@tuananh.org>
2023-09-16 21:16:56 +0700

    wait for custom resource vault become healthy
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- images/bank-vaults/tests/full-test.sh
+++ images/bank-vaults/tests/full-test.sh
@@ -14,10 +14,4 @@ kubectl kustomize https://github.com/bank-vaults/vault-operator/deploy/rbac | ku
 
 kubectl apply -f https://raw.githubusercontent.com/bank-vaults/vault-operator/v1.21.0/deploy/examples/cr-raft.yaml
 
-# when we create Vault instance, the new statefulset & pod takes awhile to be created by the operator
-# so we sleep 10 seconds here. Otherwise, the next command will fail (no pod with that label)
-# not sure what's the most reliable way for doing this?
-
-sleep 30
-
-kubectl wait -n default -l statefulset.kubernetes.io/pod-name=vault-0 --for=condition=ready pod --timeout=300s
+kubectl -n default wait --for=condition=healthy vault vault
