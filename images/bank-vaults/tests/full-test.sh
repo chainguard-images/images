697d45cb331d642d8ae9f2948de31ba9b7fb8abb
Tuan Anh Tran <me@tuananh.org>
2023-09-16 16:09:24 +0700

    sleep 30
    Signed-off-by: Tuan Anh Tran <me@tuananh.org>

--- images/bank-vaults/tests/full-test.sh
+++ images/bank-vaults/tests/full-test.sh
@@ -16,8 +16,8 @@ kubectl apply -f https://raw.githubusercontent.com/bank-vaults/vault-operator/v1
 
 # when we create Vault instance, the new statefulset & pod takes awhile to be created by the operator
 # so we sleep 10 seconds here. Otherwise, the next command will fail (no pod with that label)
-# not sure what's the best practices here
+# not sure what's the most reliable way for doing this?
 
-sleep 10
+sleep 30
 
 kubectl wait -n default -l statefulset.kubernetes.io/pod-name=vault-0 --for=condition=ready pod --timeout=300s
