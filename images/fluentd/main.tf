a3682adef397a2482945047ce2563d32d96780de
Christian Johannsen <christian.johannsen@chainguard.dev>
2023-09-18 10:17:12 -0700

    Update main.tf
    removed extra packages for testing 
    
    Signed-off-by: Christian Johannsen <christian.johannsen@chainguard.dev>

--- images/fluentd/main.tf
+++ images/fluentd/main.tf
@@ -77,7 +77,7 @@ module "latest-splunk-dev" {
   # Make the dev variant an explicit extension of the
   # locked original.
   config         = jsonencode(module.latest.config)
-  extra_packages = ["ruby3.2-fluent-plugin-splunk-hec", "fluent-plugin-out-http", local.fluent_dev]
+  extra_packages = local.fluent_dev
 }
 
 module "version-tags-latest" {
