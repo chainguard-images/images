f2ce50f23208ded50e544a1329dbd6770ce37ea5
Dan Lorenc <dlorenc@chainguard.dev>
2023-09-18 12:54:19 -0400

    Add istio operator image.
    Signed-off-by: Dan Lorenc <dlorenc@chainguard.dev>

--- images/istio/main.tf
+++ images/istio/main.tf
@@ -14,8 +14,9 @@ module "dev" { source = "../../tflib/dev-subvariant" }
 module "test-latest" {
   source = "./tests"
   digests = {
-    proxy = module.proxy.image_ref
-    pilot = module.proxy.image_ref
+    proxy    = module.proxy.image_ref
+    pilot    = module.pilot.image_ref
+    operator = module.operator.image_ref
   }
 }
 
@@ -24,6 +25,7 @@ resource "oci_tag" "latest" {
   for_each = {
     "proxy" : module.proxy.image_ref,
     "pilot" : module.pilot.image_ref,
+    "operator" : module.operator.image_ref,
   }
   digest_ref = each.value
   tag        = "latest"
@@ -34,6 +36,7 @@ resource "oci_tag" "latest-dev" {
   for_each = {
     "proxy" : module.proxy-dev.image_ref,
     "pilot" : module.pilot-dev.image_ref,
+    "operator" : module.operator-dev.image_ref,
   }
   digest_ref = each.value
   tag        = "latest-dev"
--- images/istio/tests/main.tf
+++ images/istio/tests/main.tf
@@ -1,14 +1,16 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
   }
 }
 
 variable "digests" {
   description = "The image digest to run tests over."
   type = object({
-    proxy = string
-    pilot = string
+    proxy    = string
+    pilot    = string
+    operator = string
   })
 }
 
@@ -20,4 +22,26 @@ data "oci_exec_test" "proxy-version" {
 data "oci_exec_test" "pilot-version" {
   digest = var.digests.pilot
   script = "docker run --rm $IMAGE_NAME --version"
+}
+
+data "oci_exec_test" "operator-version" {
+  digest = var.digests.operator
+  script = "docker run --rm $IMAGE_NAME version"
+}
+
+data "oci_string" "ref" { input = var.digests.operator }
+
+resource "helm_release" "operator" {
+  name = "operator"
+
+  # There's no official helm chart for the istio operator
+  repository = "https://stevehipwell.github.io/helm-charts/"
+  chart      = "istio-operator"
+
+  values = [jsonencode({
+    image = {
+      repository = data.oci_string.ref.registry_repo
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  })]
 }
