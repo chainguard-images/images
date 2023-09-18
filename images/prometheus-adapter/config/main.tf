b113b03439f00778bbb005cf19cb506eaff94dd5
Priya Wadhwa <priya@chainguard.dev>
2023-09-18 13:30:37 -0700

    Define package in extra_packages so it can be overwritten
    Signed-off-by: Priya Wadhwa <priya@chainguard.dev>

--- images/prometheus-adapter/config/main.tf
+++ images/prometheus-adapter/config/main.tf
@@ -6,7 +6,7 @@ terraform {
 
 variable "extra_packages" {
   description = "The additional packages to install"
-  default     = []
+  default     = ["prometheus-adapter"]
 }
 
 data "apko_config" "this" {
