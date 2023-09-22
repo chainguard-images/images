1cf0020b65f51a6608157fb0370ee2bdd698e824
Matt Moore <mattmoor@chainguard.dev>
2023-09-22 11:43:03 -0700

    Refactor `kube-state-metrics` to the virtual pattern.
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/kube-state-metrics/config/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. kube-state-metrics, kube-state-metrics-2.8)."
+  default     = ["kube-state-metrics"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
--- images/kube-state-metrics/main.tf
+++ images/kube-state-metrics/main.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
+
+module "latest-config" { source = "./config" }
 
 module "latest" {
   source = "../../tflib/publisher"
@@ -8,7 +16,7 @@ module "latest" {
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.latest-config.config
 }
 
 module "dev" { source = "../../tflib/dev-subvariant" }
@@ -25,24 +33,19 @@ module "latest-dev" {
   extra_packages = module.dev.extra_packages
 }
 
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "kube-state-metrics"
-  config  = module.latest.config
-}
-
 module "test-latest" {
   source = "./tests"
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
   depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
 
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest-dev.image_ref },
-  )
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest-dev.image_ref
+  tag        = "latest-dev"
 }
