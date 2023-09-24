3f69cf49a5d929b8d030727b0c6823f74b48bc39
Nghia Tran <tcnghia@gmail.com>
2023-09-23 19:14:48 -0700

    Add a test of Envoy serving static rules
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/envoy/tests/main.tf
+++ images/envoy/tests/main.tf
@@ -12,3 +12,15 @@ data "oci_exec_test" "version" {
   digest = var.digest
   script = "docker run --rm $IMAGE_NAME envoy --version | grep '^envoy  version:'"
 }
+
+resource "random_pet" "suffix" {}
+
+data "oci_exec_test" "serving-static-rules" {
+  digest = var.digest
+  script = "${path.module}/static.sh"
+
+  env {
+    name  = "NAMESPACE"
+    value = "envoy-static-${random_pet.suffix.id}"
+  }
+}
