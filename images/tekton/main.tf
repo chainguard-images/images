4cc27fa47343c7dcd6611ee0f3ffceeb5953e8f6
Matt Moore <mattmoor@chainguard.dev>
2023-09-25 04:17:38 -0700

    Refactor tekton to reduce config sprawl (#1450)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/tekton/main.tf
+++ images/tekton/main.tf
@@ -1,3 +1,9 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
@@ -25,14 +31,19 @@ locals {
   })
 }
 
+module "config" {
+  for_each = local.components
+  source   = "./config"
+  package  = local.packages[each.key]
+}
+
 module "latest" {
   for_each = local.components
   source   = "../../tflib/publisher"
 
-  name = basename(path.module)
-
+  name              = basename(path.module)
   target_repository = "${var.target_repository}-${each.key}"
-  config            = file("${path.module}/configs/latest.${each.key}.apko.yaml")
+  config            = module.config[each.key].config
 }
 
 module "dev" { source = "../../tflib/dev-subvariant" }
@@ -41,8 +52,7 @@ module "latest-dev" {
   for_each = local.components
   source   = "../../tflib/publisher"
 
-  name = basename(path.module)
-
+  name              = basename(path.module)
   target_repository = "${var.target_repository}-${each.key}"
   # Make the dev variant an explicit extension of the
   # locked original.
@@ -50,26 +60,21 @@ module "latest-dev" {
   extra_packages = module.dev.extra_packages
 }
 
-module "version-tags" {
-  for_each = local.components
-  source   = "../../tflib/version-tags"
-  package  = local.packages[each.key]
-  config   = module.latest[each.key].config
-}
-
 module "test-latest" {
   source  = "./tests"
   digests = { for k, v in module.latest : k => module.latest[k].image_ref }
 }
 
-module "tagger" {
-  for_each = local.components
-  source   = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
+  for_each   = local.components
   depends_on = [module.test-latest]
+  digest_ref = module.latest[each.key].image_ref
+  tag        = "latest"
+}
 
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : t => module.latest[each.key].image_ref },
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : "${t}-dev" => module.latest-dev[each.key].image_ref },
-  )
+resource "oci_tag" "latest-dev" {
+  for_each   = local.components
+  depends_on = [module.test-latest]
+  digest_ref = module.latest-dev[each.key].image_ref
+  tag        = "latest-dev"
 }
