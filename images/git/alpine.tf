1d79d5c9d3029f837d74b8b5a7991f293588c455
Matt Moore <mattmoor@chainguard.dev>
2023-09-25 07:20:01 -0700

    Refactor the remaining dev variants.
    Now the only reference to the `dev` module is from the publisher :tada:
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/git/alpine.tf
+++ images/git/alpine.tf
@@ -2,51 +2,32 @@ module "latest-alpine" {
   providers = {
     apko = apko.alpine
   }
-  for_each = local.accounts
-  source   = "../../tflib/publisher"
-
-  name = basename(path.module)
-
+  for_each          = local.accounts
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
   target_repository = var.target_repository
   config            = file("${path.module}/configs/latest.alpine.${each.key}.apko.yaml")
   # Override the module's default wolfi packages that conflict with alpine
   extra_packages = []
-}
-
-module "latest-alpine-dev" {
-  providers = {
-    apko = apko.alpine
-  }
-  for_each = local.accounts
-  source   = "../../tflib/publisher"
-
-  name = basename(path.module)
-
-  target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.alpine.${each.key}.apko.yaml")
-  # Override the module's default wolfi packages that conflict with alpine
-  extra_packages = module.dev.extra_packages
+  build-dev      = true
 }
 
 module "version-tags-alpine" {
   for_each = local.accounts
   source   = "../../tflib/version-tags"
-
-  package = "git"
-  config  = module.latest-alpine[each.key].config
+  package  = "git"
+  config   = module.latest-alpine[each.key].config
 }
 
 module "test-latest-alpine" {
   for_each = local.accounts
   source   = "./tests"
-
-  digest = module.latest-alpine[each.key].image_ref
+  digest   = module.latest-alpine[each.key].image_ref
 }
 
 module "test-latest-alpine-dev" {
-  for_each = local.accounts
-  source   = "./tests"
-
-  digest    = module.latest-alpine-dev[each.key].image_ref
+  for_each  = local.accounts
+  source    = "./tests"
+  digest    = module.latest-alpine[each.key].dev_ref
   check-dev = true
 }
