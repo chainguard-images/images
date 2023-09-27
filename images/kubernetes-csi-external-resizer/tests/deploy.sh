94b8dd0f9199d7ed94ca7da558af781ca58dc8dc
Furkan Türkal <furkan.turkal@chainguard.dev>
2023-09-27 17:49:16 +0300

    Refactor `bank-vaults`, `external-resizer` and `tigera` to drop version tags (#1465)
    * Drop version tags for bank-vaults, external-resizer and tigera
    
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    
    * Delete images/bank-vaults/image.yaml
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Delete images/kubernetes-csi-external-resizer/image.yaml
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Delete images/tigera-operator/image.yaml
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    Co-authored-by: Jason Hall <jason@chainguard.dev>

--- images/kubernetes-csi-external-resizer/tests/deploy.sh
+++ images/kubernetes-csi-external-resizer/tests/deploy.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-VERSION=${VERSION:-v1.8.0}
+VERSION=${VERSION:-v1.9.0}
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-resizer/${VERSION}/deploy/kubernetes/rbac.yaml
 curl https://raw.githubusercontent.com/kubernetes-csi/external-resizer/${VERSION}/deploy/kubernetes/deployment.yaml \
     | sed "s|gcr.io/k8s-staging-sig-storage/csi-resizer:.*|${IMAGE_NAME}|g" \
