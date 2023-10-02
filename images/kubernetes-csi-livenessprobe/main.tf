62ba4fcb33a08a67420447afcaee7daf261ef5c9
Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
2023-10-02 22:54:58 +0300

    fix kubernetes-csi-livenessprobe
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>

--- images/kubernetes-csi-livenessprobe/main.tf
+++ images/kubernetes-csi-livenessprobe/main.tf
@@ -2,13 +2,16 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "latest-config" { source = "./config" }
+
 module "latest" {
   source = "../../tflib/publisher"
 
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.latest-config.config
+  build-dev         = true
 }
 
 module "version-tags" {
