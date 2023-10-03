fd7b2763266d674b3f595474a89bb2a76574fab2
Dan Luhring <dluhring@chainguard.dev>
2023-10-03 10:46:17 -0400

    chore: refactor aws-ebs-csi-driver configuration
    Signed-off-by: Dan Luhring <dluhring@chainguard.dev>

--- images/aws-ebs-csi-driver/main.tf
+++ images/aws-ebs-csi-driver/main.tf
@@ -2,11 +2,13 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "latest-config" { source = "./config" }
+
 module "latest" {
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.latest-config.config
   build-dev         = true
 }
 
@@ -27,7 +29,9 @@ module "tagger" {
   depends_on = [module.test-latest]
 
   tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest.dev_ref },
+    { for t in module.version-tags.tag_list : t => module.latest.image_ref },
+    { for t in module.version-tags.tag_list : "${t}-dev" => module.latest.dev_ref },
+    { "latest" : module.latest.image_ref },
+    { "latest-dev" : module.latest.dev_ref }
   )
 }
