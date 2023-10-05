8db921c1d4863c569eef40d4a25c6ee6b1731a63
Matt Moore <mattmoor@chainguard.dev>
2023-10-05 15:25:36 -0700

    Add a `node-lts` repository. (#1561)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

---
+++ images/node-lts/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "target_repository" {
+  description = "The docker repo into which the image and attestations should be published."
+}
+
+module "latest-config" {
+  source         = "../node/config"
+  extra_packages = ["nodejs-lts"]
+}
+
+module "latest" {
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  config            = module.latest-config.config
+  extra_dev_packages = [
+    "yarn",
+    "build-base",
+    "python-3.11",
+  ]
+}
+
+module "test-latest" {
+  source = "../node/tests"
+  digest = module.latest.image_ref
+}
+
+resource "oci_tag" "latest" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
+
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.dev_ref
+  tag        = "latest-dev"
+}
