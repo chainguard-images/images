9d3d198b13887796b736344456d422d1f246e02b
Nghia Tran <tcnghia@gmail.com>
2023-10-07 14:48:42 -0700

    istio test: shell to curl & fix cleanup order (#1574)
    Using a Job to curl on a over-provisioned test cluster may result in
    little retries.
    
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/istio/tests/test-gateway.sh
+++ images/istio/tests/test-gateway.sh
@@ -19,4 +19,17 @@ EOF
 
 sed "s/ISTIO_NAMESPACE/$ISTIO_NAMESPACE/g" $SCRIPT_DIR/virtualservice.yaml \
   | kubectl apply -f- -n $NAMESPACE
-kubectl wait --for=condition=complete --timeout=60s -n $NAMESPACE job/curl
+
+kubectl port-forward -n $ISTIO_NAMESPACE svc/$ISTIO_NAMESPACE-gateway "${FREE_PORT}:80" &
+pid=$!
+trap "kill -9 $pid" EXIT
+
+set +o errexit
+for i in {1..10}; do
+    curl --retry 10 localhost:$FREE_PORT -H "Host: ingress.test.foo" && s=0 && break
+    s=$?
+    sleep 15
+done
+if [ $s -ne 0 ]; then
+    exit $s
+fi
