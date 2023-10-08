a9a28ec595d779faa45aef4c40fd8ed863e5eabf
Nghia Tran <tcnghia@gmail.com>
2023-10-08 10:48:11 -0700

    istio: rename config dir for clarity
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/envoy/main.tf
+++ images/envoy/main.tf
@@ -8,7 +8,7 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "config" { source = "./configs" }
+module "config" { source = "./config" }
 
 module "latest" {
   source            = "../../tflib/publisher"
