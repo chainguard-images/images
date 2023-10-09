5a02ee91212d31ec2a23949a91815f68dfcbeef6
Jason Hall <jason@chainguard.dev>
2023-10-09 13:10:30 -0400

    fmt
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/apko/main.tf
+++ images/apko/main.tf
@@ -8,7 +8,7 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "config" {  source = "./config" }
+module "config" { source = "./config" }
 
 module "latest" {
   source = "../../tflib/publisher"
@@ -16,7 +16,7 @@ module "latest" {
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            =  module.config.config
+  config            = module.config.config
 }
 
 module "test-latest" {
