5cde1180c8ce694c045655a267c08987a83de40e
Matt Moore <mattmoor@chainguard.dev>
2023-10-11 17:24:50 -0700

    Try deploying our Postgres with Bitnami Helm chart. (#1607)
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/postgres/tests/main.tf
+++ images/postgres/tests/main.tf
@@ -8,6 +8,8 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+data "oci_string" "ref" { input = var.digest }
+
 data "oci_exec_test" "version" {
   digest = var.digest
   script = "docker run --rm $IMAGE_NAME --version | grep '^postgres (PostgreSQL) '"
@@ -24,4 +26,41 @@ data "oci_exec_test" "locale" {
 
   digest = var.digest
   script = "${path.module}/03-locale.sh"
+}
+
+resource "random_id" "hex" { byte_length = 4 }
+
+resource "helm_release" "bitnami" {
+  name       = "postgres-${random_id.hex.hex}"
+  repository = "oci://registry-1.docker.io/bitnamicharts"
+  chart      = "postgresql"
+
+  namespace        = "postgres-${random_id.hex.hex}"
+  create_namespace = true
+
+  // Point the chart at our Postgres image
+  set {
+    name  = "image.registry"
+    value = data.oci_string.ref.registry
+  }
+  set {
+    name  = "image.repository"
+    value = data.oci_string.ref.repo
+  }
+  set {
+    name  = "image.digest"
+    value = data.oci_string.ref.digest
+  }
+
+  // Fix the UID that is used to run the container
+  set {
+    name  = "primary.containerSecurityContext.runAsUser"
+    value = "70"
+  }
+}
+
+module "helm_cleanup_bitnami" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.bitnami.id
+  namespace = helm_release.bitnami.namespace
 }
