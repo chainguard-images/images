265c67bd978b773ffdea31bd9d0e55e6e7ab1772
Billy Lynch <billy@chainguard.dev>
2023-10-12 17:16:11 -0400

    buildkit: use daemonless buildkit for test.
    Signed-off-by: Billy Lynch <billy@chainguard.dev>

--- images/buildkit/tests/02-build.sh
+++ images/buildkit/tests/02-build.sh
@@ -2,20 +2,17 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-CID=$(docker run -d --privileged ${IMAGE_NAME})
-trap "docker container rm -f ${CID}" EXIT
-export BUILDKIT_HOST="docker-container://${CID}"
-
-# Wait for buildkitd to be ready
-for i in $(seq 1 10); do
-  buildctl debug workers && break
-  sleep 1
-done
-
 # Build a simple image
 TMPDIR="$(mktemp -d)"
 echo "FROM scratch" > ${TMPDIR}/Dockerfile
-buildctl build \
-    --frontend=dockerfile.v0 \
-    --local context=${TMPDIR} \
-    --local dockerfile=${TMPDIR}
+docker run \
+  -it \
+  --rm \
+  --privileged \
+  -v ${TMPDIR}:/tmp/work \
+  --entrypoint buildctl-daemonless.sh \
+  ${IMAGE_NAME} \
+      build \
+      --frontend dockerfile.v0 \
+      --local context=/tmp/work \
+      --local dockerfile=/tmp/work
