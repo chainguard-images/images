95dac5924760ff1648e9bdd9c924946dae10a71f
Matt Moore <mattmoor@chainguard.dev>
2023-10-14 10:12:55 -0700

    Work on compat with Bitnami helm chart (#1631)
    * add bitnami helm test
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * try to match bitnami upstream as much as possible
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * Run as the right user, disable RO FS.
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Drop JAVA_HOME pointing to the wrong place
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    * Try dropping additional image changes to see if they are necessary
    
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>
    Co-authored-by: Jason Hall <jason@chainguard.dev>

--- images/kafka/tests/main.tf
+++ images/kafka/tests/main.tf
@@ -13,3 +13,37 @@ data "oci_exec_test" "docker-compose" {
   script      = "./docker-compose.sh"
   working_dir = path.module
 }
+
+data "oci_string" "ref" { input = var.digest }
+
+resource "random_pet" "suffix" {}
+
+resource "helm_release" "bitnami" {
+  name             = "kafka-bitnami-${random_pet.suffix.id}"
+  repository       = "oci://registry-1.docker.io/bitnamicharts"
+  chart            = "kafka"
+  namespace        = "kafka-bitnami-${random_pet.suffix.id}"
+  create_namespace = true
+
+  values = [jsonencode({
+    image = {
+      registry   = data.oci_string.ref.registry
+      repository = data.oci_string.ref.repo
+      digest     = data.oci_string.ref.digest
+    }
+    controller = {
+      containerSecurityContext = {
+        runAsUser = 65532
+        # Running with a read-only filesystem fails for some
+        # reason I cannot explain.
+        readOnlyRootFilesystem = false
+      }
+    }
+  })]
+}
+
+module "helm-cleanup" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.bitnami.id
+  namespace = helm_release.bitnami.namespace
+}
