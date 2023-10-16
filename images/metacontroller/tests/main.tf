0a68bbe2c747dfcd249734b46bfd82fa4eaa5b00
Matt Moore <mattmoor@chainguard.dev>
2023-10-16 15:47:06 -0700

    Refactor metacontroller to `helm_release`
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/metacontroller/tests/main.tf
+++ images/metacontroller/tests/main.tf
@@ -8,16 +8,32 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+data "oci_string" "ref" { input = var.digest }
+
+resource "helm_release" "metacontroller" {
+  name       = "metacontroller"
+  repository = "oci://ghcr.io/metacontroller"
+  chart      = "metacontroller-helm"
+  version    = "v4.10.3"
+
+  values = [jsonencode({
+    image = {
+      repository = data.oci_string.ref.registry_repo
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  })]
+}
+
 data "oci_exec_test" "helm" {
+  depends_on  = [helm_release.metacontroller]
   digest      = var.digest
   script      = "./helm.sh"
   working_dir = path.module
+}
 
-  # Split the digest ref into repository and digest. The helm chart expects a
-  # tag, but it just appends it to the repository again, so we just specify a
-  # dummy tag and the digest to test.
-  env {
-    name  = "IMAGE_TAG"
-    value = "unused@${element(split("@", var.digest), 1)}"
-  }
+module "helm_cleanup" {
+  depends_on = [data.oci_exec_test.helm]
+  source     = "../../../tflib/helm-cleanup"
+  name       = helm_release.metacontroller.id
+  namespace  = helm_release.metacontroller.namespace
 }
