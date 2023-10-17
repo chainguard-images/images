a2a26298f32a930504cb9b47dbe8109870464c92
Nghia Tran <tcnghia@gmail.com>
2023-10-17 12:34:33 -0700

    cilium: use an on-demand test cluster (#1657)

--- images/cilium/tests/main.tf
+++ images/cilium/tests/main.tf
@@ -16,36 +16,7 @@ data "oci_exec_test" "operator-version" {
   digest = var.digests.operator
 }
 
-# TODO:
-# To run this test, we need to create a cluster that looks like this
-#
-# k3d cluster create -i cgr.dev/chainguard/k3s:latest \
-#     --k3s-arg '--disable=traefik@server:*' \
-#     --k3s-arg '--disable=metrics-server@server:*' \
-#     --k3s-arg '--disable-network-policy@server:*' \
-#     --k3s-arg '--flannel-backend=none@server:*' \
-#
-# for node in k3d-k3s-default-server-0; do
-#   docker exec -i $node /bin/sh <<-EOF
-#       mount bpffs -t bpf /sys/fs/bpf
-#       mount --make-shared /sys/fs/bpf
-#       mkdir -p /run/cilium/cgroupv2
-#       mount -t cgroup2 none /run/cilium/cgroupv2
-#       mount --make-shared /run/cilium/cgroupv2/
-#
-#
-# resource "helm_release" "cilium" {
-#   repository       = "https://helm.cilium.io"
-#   chart            = "cilium"
-#   name             = "cilium"
-#   namespace        = "kube-system"
-
-#   values = [jsonencode({
-#     operator = {
-#       image = {
-#         override = var.digests.operator
-#       }
-#       replicas = 1
-#     }
-#   })]
-# }
+data "oci_exec_test" "cilium-install" {
+  script = "${path.module}/cilium-install.sh"
+  digest = var.digests.operator
+}
