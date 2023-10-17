6c639b827e5e87718decfd263b1d8dd08f07fbba
Nghia Tran <tcnghia@gmail.com>
2023-10-16 20:48:40 -0700

    cilium: use an on-demand test cluster
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/cilium/tests/main.tf
+++ images/cilium/tests/main.tf
@@ -1,6 +1,14 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_context = "k3d-cilium-test"
+    config_path    = "~/.kube/config"
   }
 }
 
@@ -16,36 +24,38 @@ data "oci_exec_test" "operator-version" {
   digest = var.digests.operator
 }
 
-# TODO:
-# To run this test, we need to create a cluster that looks like this
-#
-# k3d cluster create -i cgr.dev/chainguard/k3s:latest \
-#     --k3s-arg '--disable=traefik@server:*' \
-#     --k3s-arg '--disable=metrics-server@server:*' \
-#     --k3s-arg '--disable-network-policy@server:*' \
-#     --k3s-arg '--flannel-backend=none@server:*' \
-#
-# for node in k3d-k3s-default-server-0; do
-#   docker exec -i $node /bin/sh <<-EOF
-#       mount bpffs -t bpf /sys/fs/bpf
-#       mount --make-shared /sys/fs/bpf
-#       mkdir -p /run/cilium/cgroupv2
-#       mount -t cgroup2 none /run/cilium/cgroupv2
-#       mount --make-shared /run/cilium/cgroupv2/
-#
-#
-# resource "helm_release" "cilium" {
-#   repository       = "https://helm.cilium.io"
-#   chart            = "cilium"
-#   name             = "cilium"
-#   namespace        = "kube-system"
+data "oci_exec_test" "create-cluster" {
+  script = "${path.module}/create-cluster.sh"
+  digest = var.digests.operator
+}
 
-#   values = [jsonencode({
-#     operator = {
-#       image = {
-#         override = var.digests.operator
-#       }
-#       replicas = 1
-#     }
-#   })]
-# }
+resource "helm_release" "cilium" {
+  depends_on = [data.oci_exec_test.create-cluster]
+  repository = "https://helm.cilium.io"
+  chart      = "cilium"
+  name       = "cilium"
+  namespace  = "kube-system"
+
+  values = [jsonencode({
+    operator = {
+      image = {
+        override = var.digests.operator
+      }
+      replicas = 1
+    }
+  })]
+}
+
+data "oci_exec_test" "wait-for-pods" {
+  depends_on = [helm_release.cilium]
+  # Due to lack of CNI, all pods in `kube-system` will be in `Pending` state.
+  # If they are 'Ready', then our installation is working.
+  script = "kubectl --context k3d-cilium-test wait --for condition=Ready -n kube-system pods --all --timeout=300s"
+  digest = var.digests.operator
+}
+
+data "oci_exec_test" "delete-cluster" {
+  depends_on = [data.oci_exec_test.wait-for-pods]
+  script     = "k3d cluster delete cilium-test"
+  digest     = var.digests.operator
+}
