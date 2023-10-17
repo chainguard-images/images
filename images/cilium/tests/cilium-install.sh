e6753d3c0a293e15f389bcaec6fd48d968ce4591
Nghia Tran <tcnghia@gmail.com>
2023-10-17 10:46:48 -0700

    create and teardown cluster
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

---
+++ images/cilium/tests/cilium-install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -o errexit -o errtrace -o pipefail -x
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export CLUSTER_NAME=cilium-test
+
+k3d cluster create $CLUSTER_NAME \
+    --kubeconfig-switch-context=false \
+    --config $SCRIPT_DIR/k3d.yaml
+
+# Clean up the cluster for local runs even in case of failures.
+# For CI we want it around for diagnostics.
+if [ -z "$CI" ]; then
+    trap "k3d cluster delete $CLUSTER_NAME" EXIT
+fi
+
+
+# These settings come from
+# https://docs.cilium.io/en/latest/installation/rancher-desktop/#configure-rancher-desktop
+for node in $(kubectl get --context=k3d-$CLUSTER_NAME nodes -o jsonpath='{.items[*].metadata.name}'); do
+    echo "Configuring node $node"
+    docker exec -i $node /bin/sh <<-EOF
+        mount bpffs -t bpf /sys/fs/bpf
+        mount --make-shared /sys/fs/bpf
+        mkdir -p /run/cilium/cgroupv2
+        mount -t cgroup2 none /run/cilium/cgroupv2
+        mount --make-shared /run/cilium/cgroupv2/
+EOF
+done
+
+TMPDIR=$(mktemp -d --tmpdir=$SCRIPT_DIR)
+trap "rm -rf $TMPDIR" EXIT
+
+# Download the CLI
+pushd $TMPDIR
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+GOOS=$(go env GOOS)
+GOARCH=$(go env GOARCH)
+curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum
+tar -xzvf cilium-${GOOS}-${GOARCH}.tar.gz
+rm cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
+
+# cilium
+$TMPDIR/cilium install --context k3d-$CLUSTER_NAME \
+    --helm-set operator.image.override=$IMAGE_NAME
+
+$TMPDIR/cilium status --context k3d-$CLUSTER_NAME --wait
+
+# Clean up
+k3d cluster delete $CLUSTER_NAME
+rm -rf $TMPDIR
--- images/cilium/tests/main.tf
+++ images/cilium/tests/main.tf
@@ -24,38 +24,8 @@ data "oci_exec_test" "operator-version" {
   digest = var.digests.operator
 }
 
-data "oci_exec_test" "create-cluster" {
-  script = "${path.module}/create-cluster.sh"
+data "oci_exec_test" "cilium-install" {
+  script = "${path.module}/cilium-install.sh"
   digest = var.digests.operator
 }
 
-resource "helm_release" "cilium" {
-  depends_on = [data.oci_exec_test.create-cluster]
-  repository = "https://helm.cilium.io"
-  chart      = "cilium"
-  name       = "cilium"
-  namespace  = "kube-system"
-
-  values = [jsonencode({
-    operator = {
-      image = {
-        override = var.digests.operator
-      }
-      replicas = 1
-    }
-  })]
-}
-
-data "oci_exec_test" "wait-for-pods" {
-  depends_on = [helm_release.cilium]
-  # Due to lack of CNI, all pods in `kube-system` will be in `Pending` state.
-  # If they are 'Ready', then our installation is working.
-  script = "kubectl --context k3d-cilium-test wait --for condition=Ready -n kube-system pods --all --timeout=300s"
-  digest = var.digests.operator
-}
-
-data "oci_exec_test" "delete-cluster" {
-  depends_on = [data.oci_exec_test.wait-for-pods]
-  script     = "k3d cluster delete cilium-test"
-  digest     = var.digests.operator
-}
