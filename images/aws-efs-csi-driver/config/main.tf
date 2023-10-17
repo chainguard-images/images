0f9874bf678689134ac60c0c95f6fa9bfacf5f3c
Jason Hall <jason@chainguard.dev>
2023-10-17 14:39:45 -0400

    review feedback
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/aws-efs-csi-driver/config/main.tf
+++ images/aws-efs-csi-driver/config/main.tf
@@ -4,27 +4,20 @@ variable "extra_packages" {
   default     = ["aws-efs-csi-driver"]
 }
 
-module "accts" { source = "../../../tflib/accts" }
+module "accts" {
+  source = "../../../tflib/accts"
+
+  # This package expects to run as root, context:
+  # https://github.com/kubernetes-sigs/aws-efs-csi-driver/issues/1121#issuecomment-1714003621
+  run-as = 0
+}
 
 output "config" {
   value = jsonencode({
     contents = {
-      packages = concat(var.extra_packages, ["umount", "mount"])
+      packages = var.extra_packages
     }
-    accounts = {
-      groups = [{
-        groupname = "nonroot"
-        gid       = 65532
-      }]
-      users = [{
-        username = "nonroot"
-        uid       = 65532
-        gid       = 65532
-      }]
-      # This package expects to run as root, context:
-      # https://github.com/kubernetes-sigs/aws-efs-csi-driver/issues/1121#issuecomment-1714003621
-      run-as = 0
-    }
+    accounts = module.accts.block
     entrypoint = {
       command = "/usr/bin/aws-efs-csi-driver"
     }
