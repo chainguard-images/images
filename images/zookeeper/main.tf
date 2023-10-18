b264e793a27d7bd48f0bc6160f9e7cb7610ced23
Batuhan Apaydın <batuhan.apaydin@chainguard.dev>
2023-10-19 02:17:08 +0300

    zookeeper 3.8 and 3.9 image (#1646)

--- images/zookeeper/main.tf
+++ images/zookeeper/main.tf
@@ -2,32 +2,50 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+locals {
+  versions = [
+    "3.8",
+    "3.9"
+  ]
+}
+
+module "config" {
+  for_each       = toset(local.versions)
+  source         = "./configs"
+  extra_packages = ["zookeeper-${each.value}", "zookeeper-bitnami-${each.value}-compat"]
+}
+
 module "latest" {
+  for_each          = toset(local.versions)
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
+  config            = module.config[each.key].config
   build-dev         = true
 }
 
+
 module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "zookeeper-3.9"
-  config  = module.latest.config
+  for_each = toset(local.versions)
+  source   = "../../tflib/version-tags"
+  package  = "zookeeper-${each.value}"
+  config   = module.latest[each.key].config
 }
 
 module "test-latest" {
-  source = "./tests"
-  digest = module.latest.image_ref
+  for_each = toset(local.versions)
+  source   = "./tests"
+  digest   = module.latest[each.key].image_ref
 }
 
 module "tagger" {
-  source = "../../tflib/tagger"
+  for_each = toset(local.versions)
+  source   = "../../tflib/tagger"
 
   depends_on = [module.test-latest]
 
   tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest.dev_ref },
+    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : t => module.latest[each.key].image_ref },
+    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : "${t}-dev" => module.latest[each.key].dev_ref },
   )
 }
