54ce3ca59129d270737f2379b2aa47f6b58b9f0b
Jason Hall <jason@chainguard.dev>
2023-10-18 15:52:35 -0400

    migrate deno to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/deno/main.tf
+++ images/deno/main.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
+
+module "config" { source = "./config" }
 
 module "latest" {
   source = "../../tflib/publisher"
@@ -8,13 +16,7 @@ module "latest" {
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
-}
-
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "deno"
-  config  = module.latest.config
+  config            = module.config.config
 }
 
 module "test-latest" {
@@ -22,12 +24,8 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latesT" {
   depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-  )
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
