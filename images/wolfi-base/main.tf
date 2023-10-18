38f426adf1dd3df4d9ea1ce876651acba5fa689e
Jason Hall <jason@chainguard.dev>
2023-10-18 11:10:43 -0400

    refactor wolfi-base to config format
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/wolfi-base/main.tf
+++ images/wolfi-base/main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
+}
+
+module "config" {
+  source = "./config"
 }
 
 module "latest" {
@@ -8,13 +18,7 @@ module "latest" {
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.apko.yaml")
-}
-
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "wolfi-base"
-  config  = module.latest.config
+  config            = module.config.config
 }
 
 module "test-latest" {
@@ -22,12 +26,7 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
-  depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-  )
+resource "oci_tag" "latest" {
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
