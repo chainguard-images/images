763a9c9ee1bda0d3ed6ae777afdabfd178d8b82b
Mritunjay <mritunjay.sharma@chainguard.dev>
2023-10-19 23:31:54 +0530

    initial attempt to refactor spire
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>

--- images/spire/main.tf
+++ images/spire/main.tf
@@ -1,25 +1,30 @@
-locals {
-  components = toset(["server", "agent", "oidc-discovery-provider"])
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
 }
 
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest" {
-  for_each          = local.components
-  source            = "../../tflib/publisher"
-  name              = basename(path.module)
-  target_repository = "${var.target_repository}-${each.key}"
-  config            = file("${path.module}/configs/latest.${each.key}.apko.yaml")
-  build-dev         = true
+locals {
+  components = toset(["server", "agent", "oidc-discovery-provider"])
 }
 
-module "version-tags" {
+module "config" {
   for_each = local.components
-  source   = "../../tflib/version-tags"
-  package  = "spire-${each.key}"
-  config   = module.latest[each.key].config
+  source   = "./config"
+  name     = each.key
+}
+
+module "latest" {
+  for_each = local.components
+  source   = "../../tflib/publisher"
+
+  name              = basename(path.module)
+  target_repository = "${var.target_repository}-${each.key}"
+  config            = module.config[each.key].config
 }
 
 module "test-latest" {
@@ -27,14 +32,9 @@ module "test-latest" {
   digests = { for k, v in module.latest : k => v.image_ref }
 }
 
-module "tagger" {
-  for_each = local.components
-  source   = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
+  for_each   = local.components
   depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : t => module.latest[each.key].image_ref },
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : "${t}-dev" => module.latest[each.key].dev_ref },
-  )
+  digest_ref = module.latest[each.key].image_ref
+  tag        = "latest"
 }
