c0803c163dfb730ca865d0d54787a3eebd37ca17
Nghia Tran <tcnghia@gmail.com>
2023-10-19 09:27:23 -0700

    Cilium agent image
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- images/cilium/tests/main.tf
+++ images/cilium/tests/main.tf
@@ -7,6 +7,7 @@ terraform {
 variable "digests" {
   description = "The digests to run tests over."
   type = object({
+    agent    = string
     operator = string
   })
 }
@@ -16,7 +17,22 @@ data "oci_exec_test" "operator-version" {
   digest = var.digests.operator
 }
 
+data "oci_exec_test" "agent-version" {
+  script = "docker run --rm $IMAGE_NAME --version"
+  digest = var.digests.agent
+}
+
 data "oci_exec_test" "cilium-install" {
   script = "${path.module}/cilium-install.sh"
-  digest = var.digests.operator
+  digest = var.digests.agent
+
+  env {
+    name  = "AGENT_IMAGE"
+    value = var.digests.agent
+  }
+
+  env {
+    name  = "OPERATOR_IMAGE"
+    value = var.digests.operator
+  }
 }
