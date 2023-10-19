e182952ed67f630d47afc48a1224eba07c14699b
Nghia Tran <tcnghia@gmail.com>
2023-10-19 11:23:42 -0700

    cilium-agent image (#1693)
    * Cilium agent image
    
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>
    
    * Explain the root
    
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>
    
    * Update images/cilium/tests/main.tf
    
    Co-authored-by: Josh Wolf <josh@wolfs.io>
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>
    
    ---------
    
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>
    Co-authored-by: Josh Wolf <josh@wolfs.io>

--- images/cilium/tests/cilium-install.sh
+++ images/cilium/tests/cilium-install.sh
@@ -2,17 +2,24 @@
 
 set -o errexit -o errtrace -o pipefail -x
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TMPDIR=$(mktemp -d --tmpdir=$SCRIPT_DIR)
 
 # Create a test cluster.
 export CLUSTER_NAME=cilium-test
 k3d cluster create $CLUSTER_NAME \
     --kubeconfig-switch-context=false \
     --config $SCRIPT_DIR/k3d.yaml
-# Clean up the cluster for local runs even in case of failures.
-# For CI we want it around for diagnostics.
-if [ -z "$CI" ]; then
-    trap "k3d cluster delete $CLUSTER_NAME" EXIT
-fi
+
+cleanup() {
+    rm -rfv $TMPDIR
+    # Clean up the cluster for local runs even in case of failures.
+    # For CI we want it around for diagnostics.
+    if [ -z "$CI" ]; then
+        k3d cluster delete $CLUSTER_NAME
+    fi
+}
+
+trap cleanup EXIT
 
 # These settings come from
 # https://docs.cilium.io/en/latest/installation/rancher-desktop/#configure-rancher-desktop
@@ -27,16 +34,14 @@ for node in $(kubectl get --context=k3d-$CLUSTER_NAME nodes -o jsonpath='{.items
 EOF
 done
 
-TMPDIR=$(mktemp -d --tmpdir=$SCRIPT_DIR)
-trap "rm -rf $TMPDIR" EXIT
 
 # Download the cilium CLI
 pushd $TMPDIR
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
 # These use the platform passed into Docker. It's still better to let Go
 # translate that into its format than do any Bash-based translation here.
-GOOS=$(docker run cgr.dev/chainguard/go env GOOS)
-GOARCH=$(docker run cgr.dev/chainguard/go env GOARCH)
+GOOS=$(go env GOOS || docker run cgr.dev/chainguard/go env GOOS)
+GOARCH=$(go env GOARCH || docker run cgr.dev/chainguard/go env GOARCH)
 curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
 sha256sum --check cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum
 tar -xzvf cilium-${GOOS}-${GOARCH}.tar.gz
@@ -44,7 +49,8 @@ rm cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
 
 # Install cilium
 $TMPDIR/cilium install --context k3d-$CLUSTER_NAME \
-    --helm-set operator.image.override=$IMAGE_NAME
+    --helm-set image.override=$AGENT_IMAGE \
+    --helm-set operator.image.override=$OPERATOR_IMAGE
 
 # Verify the installation
 #
