1fe7f30671c11a2dd4abc99c59659b9c5d6d69af
Jason Hall <jason@chainguard.dev>
2023-10-19 10:01:14 -0400

    disable version tags for dynamic-localpv-provisioner
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/dynamic-localpv-provisioner/main.tf
+++ images/dynamic-localpv-provisioner/main.tf
@@ -23,15 +23,14 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
   depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
 
-  tags = merge(
-    { for t in module.version-tags.tag_list : t => module.latest.image_ref },
-    { for t in module.version-tags.tag_list : "${t}-dev" => module.latest.dev_ref },
-    { "latest" : module.latest.image_ref },
-    { "latest-dev" : module.latest.dev_ref }
-  )
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.dev_ref
+  tag        = "latest-dev"
 }
