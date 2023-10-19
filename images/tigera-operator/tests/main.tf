316b17190f6f8ff3b6d7ed35982909ae0633c693
Jason Hall <jason@chainguard.dev>
2023-10-19 11:40:39 -0400

    tigera-operator deconflict helm chart name (#1685)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/tigera-operator/tests/main.tf
+++ images/tigera-operator/tests/main.tf
@@ -11,12 +11,14 @@ variable "digest" {
 
 data "oci_string" "ref" { input = var.digest }
 
+resource "random_pet" "suffix" {}
+
 resource "helm_release" "tigera-operator" {
-  name = "tigera-operator"
+  name = "tigera-operator-${random_pet.suffix.id}"
 
   repository       = "https://projectcalico.docs.tigera.io/charts"
   chart            = "tigera-operator"
-  namespace        = "tigera-operator"
+  namespace        = "tigera-operator-${random_pet.suffix.id}"
   create_namespace = true
 
   values = [jsonencode({
