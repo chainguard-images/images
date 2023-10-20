03ee708acb001f1217d6d0abe10d3751ffe37471
Matt Moore <mattmoor@chainguard.dev>
2023-10-19 17:13:32 -0700

    Add missing .block
    Signed-off-by: Matt Moore <mattmoor@chainguard.dev>

--- images/jdk/config/main.tf
+++ images/jdk/config/main.tf
@@ -36,8 +36,8 @@ output "config" {
     paths = [{
       path        = "/home/build"
       type        = "directory"
-      uid         = module.accts.run-as
-      gid         = module.accts.run-as
+      uid         = module.accts.block.run-as
+      gid         = module.accts.block.run-as
       permissions = 493 // 0o755 (HCL explicitly does not support octal literals)
     }]
   })
