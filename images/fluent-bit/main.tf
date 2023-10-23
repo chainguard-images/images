aee1e8a01e6ad9e7f39316e6c55731e59368f566
Jason Hall <jason@chainguard.dev>
2023-10-23 13:15:53 -0400

    disable version tags for fluent-bit (#1690)
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/fluent-bit/main.tf
+++ images/fluent-bit/main.tf
@@ -1,8 +1,14 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest-config" { source = "./config" }
+module "config" { source = "./config" }
 
 module "latest" {
   source = "../../tflib/publisher"
@@ -10,13 +16,7 @@ module "latest" {
   name = basename(path.module)
 
   target_repository = var.target_repository
-  config            = module.latest-config.config
-}
-
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "fluent-bit"
-  config  = module.latest.config
+  config            = module.config.config
 }
 
 module "test-latest" {
@@ -24,12 +24,8 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
   depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-  )
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
