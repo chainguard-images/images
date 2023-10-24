ba7331d7a914756ca6ddce569bbaa719d45b8ec7
Mritunjay Kumar Sharma <mritunjay.sharma@chainguard.dev>
2023-10-25 00:33:38 +0530

    refactors spire image (#1694)
    * initial attempt to refactor spire
    
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    
    * tries to add logic to handle different entrypoints
    
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    
    * further refactors of logic in spire image
    
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    
    * adds missing extra packages and minor refactors
    
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    
    ---------
    
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    Signed-off-by: Geoff Newson <geoff.newson@gmail.com>

--- images/spire/main.tf
+++ images/spire/main.tf
@@ -1,25 +1,57 @@
-locals {
-  components = toset(["server", "agent", "oidc-discovery-provider"])
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
 }
 
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest" {
-  for_each          = local.components
-  source            = "../../tflib/publisher"
-  name              = basename(path.module)
-  target_repository = "${var.target_repository}-${each.key}"
-  config            = file("${path.module}/configs/latest.${each.key}.apko.yaml")
-  build-dev         = true
+
+locals {
+  component_data = {
+    "server" = {
+      run-as         = 65532
+      command        = "/usr/bin/spire-server run"
+      extra_packages = []
+      cmd            = ""
+    },
+
+    "agent" = {
+      run-as         = 0
+      command        = "/usr/bin/spire-agent run"
+      extra_packages = ["busybox", "libcap-utils"]
+      cmd            = ""
+    },
+
+    "oidc-discovery-provider" = {
+      run-as         = 65532
+      command        = "/usr/bin/oidc-discovery-provider"
+      extra_packages = []
+      cmd            = "--help"
+    },
+  }
+  components = toset(keys(local.component_data))
 }
 
-module "version-tags" {
+module "config" {
+  for_each       = local.components
+  source         = "./config"
+  name           = each.key
+  run-as         = local.component_data[each.key].run-as
+  command        = local.component_data[each.key].command
+  cmd            = local.component_data[each.key].cmd
+  extra_packages = local.component_data[each.key].extra_packages
+}
+
+module "latest" {
   for_each = local.components
-  source   = "../../tflib/version-tags"
-  package  = "spire-${each.key}"
-  config   = module.latest[each.key].config
+  source   = "../../tflib/publisher"
+
+  name              = basename(path.module)
+  target_repository = "${var.target_repository}-${each.key}"
+  config            = module.config[each.key].config
 }
 
 module "test-latest" {
@@ -27,14 +59,9 @@ module "test-latest" {
   digests = { for k, v in module.latest : k => v.image_ref }
 }
 
-module "tagger" {
-  for_each = local.components
-  source   = "../../tflib/tagger"
-
+resource "oci_tag" "latest" {
+  for_each   = local.components
   depends_on = [module.test-latest]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : t => module.latest[each.key].image_ref },
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : "${t}-dev" => module.latest[each.key].dev_ref },
-  )
+  digest_ref = module.latest[each.key].image_ref
+  tag        = "latest"
 }
