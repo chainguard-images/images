d91713a9f3d9fd8c024a7febae4d48836bd01391
Jason Hall <jason@chainguard.dev>
2023-10-23 21:53:02 -0400

    cedar: update and try to debug failure
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/cedar/config/main.tf
+++ images/cedar/config/main.tf
@@ -1,19 +1,21 @@
-terraform {
-  required_providers {
-    apko = { source = "chainguard-dev/apko" }
-  }
-}
-
 variable "extra_packages" {
-  description = "The additional packages to install (e.g. cedar)."
-  default     = ["cedar"]
+  description = "Additional packages to install."
+  type        = list(string)
+  default     = ["cedar<=2.4.2"] //  Fails
+  //default = ["cedar<2.4.2"] // Works
 }
 
-data "apko_config" "this" {
-  config_contents = file("${path.module}/template.apko.yaml")
-  extra_packages  = var.extra_packages
-}
+module "accts" { source = "../../../tflib/accts" }
 
 output "config" {
-  value = jsonencode(data.apko_config.this.config)
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    entrypoint = {
+      command = "/usr/bin/cedar"
+    }
+    cmd = "help"
+  })
 }
--- images/cedar/tests/main.tf
+++ images/cedar/tests/main.tf
@@ -8,12 +8,7 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "help" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME help"
-}
-
 data "oci_exec_test" "use" {
   digest = var.digest
-  script = "${path.module}/02-use.sh"
+  script = "${path.module}/use.sh"
 }
