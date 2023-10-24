5c6756c0bf3e79feed6151a2b1698850fcd1be8e
Jason Hall <jason@chainguard.dev>
2023-10-24 10:55:57 -0400

    use default JAVA_HOME (#1749)
    * use default JAVA_HOME
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * remove one more
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/gradle/config/main.tf
+++ images/gradle/config/main.tf
@@ -9,20 +9,11 @@ variable "extra_packages" {
   default     = []
 }
 
-variable "java_home" {
-  description = "The JAVA_HOME environment variable."
-  default     = "/usr/lib/jvm/java-17-openjdk"
-}
-
 data "apko_config" "this" {
   config_contents = file("${path.module}/template.apko.yaml")
   extra_packages  = var.extra_packages
 }
 
 output "config" {
-  value = jsonencode(merge(data.apko_config.this.config, {
-    environment : {
-      "JAVA_HOME" : var.java_home
-    }
-  }))
+  value = jsonencode(data.apko_config.this.config)
 }
