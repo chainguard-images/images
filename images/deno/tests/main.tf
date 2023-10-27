53e761387e6f40e35d9b4209ab03621c580cc7ff
Jason Hall <jason@chainguard.dev>
2023-10-27 14:31:36 -0400

    revert deno stuff
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/deno/tests/main.tf
+++ images/deno/tests/main.tf
@@ -8,8 +8,7 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "build" {
-  digest      = var.digest
-  script      = "./build.sh"
-  working_dir = path.module
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "docker run --rm $IMAGE_NAME --version"
 }
