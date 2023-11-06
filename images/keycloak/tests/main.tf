5fae2fcfe7068179ab8fd30c1ec253895961a924
mamccorm <mark.mccormick@chainguard.dev>
2023-11-06 10:36:29 +0000

    Adding additional test to validate keycloak startup in production mode (#1823)
    * Add addtional test to run keycloak in production mode
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    * remove prepended number from test name
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    * reverting a local change which was done for testing and not intended to be pushed in the last commit
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    ---------
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>

--- images/keycloak/tests/main.tf
+++ images/keycloak/tests/main.tf
@@ -42,6 +42,11 @@ resource "helm_release" "test" {
   })]
 }
 
+data "oci_exec_test" "keycloak-production-test" {
+  digest = var.digest
+  script = "${path.module}/keycloak-production-mode.sh"
+}
+
 module "helm_cleanup" {
   source = "../../../tflib/helm-cleanup"
   name   = helm_release.test.id
