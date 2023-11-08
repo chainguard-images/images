9d51873dcde6bbc0e9d8d8f682e7c8bdb09f8dc5
Jason Hall <jason@chainguard.dev>
2023-11-08 17:37:37 -0500

    migrate dex to config module (#1838)
    * migrate dex to config module
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * revert dex/README
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * fmt
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/dex/tests/main.tf
+++ images/dex/tests/main.tf
@@ -1,7 +1,6 @@
 terraform {
   required_providers {
-    oci  = { source = "chainguard-dev/oci" }
-    helm = { source = "hashicorp/helm" }
+    oci = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -9,70 +8,11 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_string" "ref" { input = var.digest }
-
-data "oci_exec_test" "version" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME dex -h"
-}
-
-data "oci_exec_test" "run" {
+// Invoke a script with the test.
+// $IMAGE_NAME is populated with the image name by digest.
+// TODO: Update or remove this test as appropriate.
+data "oci_exec_test" "manifest" {
   digest      = var.digest
   script      = "./runs.sh"
   working_dir = path.module
-}
-
-resource "helm_release" "dex" {
-  name = "dex"
-
-  repository = "https://charts.dexidp.io"
-  chart      = "dex"
-
-  namespace        = "dex"
-  create_namespace = true
-
-  values = [
-    <<EOF
-config:
-  image:
-    tag: ${data.oci_string.ref.pseudo_tag}
-    repository: ${data.oci_string.ref.registry_repo}
-
-  issuer: "http://127.0.0.1:5556/dex"
-
-  storage:
-    type: memory
-
-  web:
-    http: 0.0.0.0:5556
-
-  expiry:
-    deviceRequests: "5m"
-    signingKeys: "6h"
-    idTokens: "24h"
-    authRequests: "24h"
-
-  logger:
-    level: "info"
-    format: "text"
-
-  oauth2:
-    responseTypes: [ "code" ]
-    skipApprovalScreen: false
-    alwaysShowLoginScreen: false
-
-  enablePasswordDB: true
-
-  connectors:
-  - type: mockCallback
-    id: mock
-    name: Example
-    EOF
-  ]
-}
-
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.dex.id
-  namespace = helm_release.dex.namespace
 }
