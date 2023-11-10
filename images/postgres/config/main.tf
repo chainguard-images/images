7af351980c8de6b5806097352e3fffa3df9beac0
Batuhan Apaydın <batuhan.apaydin@chainguard.dev>
2023-11-11 00:20:41 +0300

    new config for postgreql (#1849)

--- images/postgres/config/main.tf
+++ images/postgres/config/main.tf
@@ -9,11 +9,43 @@ variable "extra_packages" {
   default     = ["postgresql-15", "postgresql-15-client", "postgresql-15-oci-entrypoint", "postgresql-15-contrib", "libpq-15"]
 }
 
-data "apko_config" "this" {
-  config_contents = file("${path.module}/template.apko.yaml")
-  extra_packages  = var.extra_packages
+module "accts" {
+  source = "../../../tflib/accts"
+  run-as = 0
+  uid    = 70
+  gid    = 70
+  name   = "postgres"
+}
+
+variable "environment" {
+  default = {}
 }
 
 output "config" {
-  value = jsonencode(data.apko_config.this.config)
+  value = jsonencode({
+    contents = {
+      packages = concat([
+        "glibc-locale-en",
+        "busybox",
+        "su-exec",
+      ], var.extra_packages)
+    }
+    accounts = module.accts.block
+    environment = merge({
+      "PGDATA" : "/var/lib/postgresql/data",
+      "LANG" : "en_US.UTF-8"
+    }, var.environment)
+    entrypoint = {
+      command = "/var/lib/postgres/initdb/postgresql-entrypoint.sh postgres"
+    }
+    work-dir = "/home/postgres"
+    paths = [{
+      path        = "/var/lib/postgresql/data"
+      type        = "directory"
+      uid         = module.accts.block.run-as
+      gid         = module.accts.block.run-as
+      permissions = 511 // 0o777 (HCL explicitly does not support octal literals)
+    }]
+    }
+  )
 }
