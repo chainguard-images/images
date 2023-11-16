ef093ea1e04d108f3e05c9738d96949d4568b450
Mauren Berti <mauren.berti@chainguard.dev>
2023-11-16 11:56:16 -0500

    fix: update kubeflow-katib tests for latest version
    Update the kubeflow-katib tests to work with the latest version of the image.
    
    Signed-off-by: Mauren Berti <mauren.berti@chainguard.dev>

--- images/kubeflow-katib/tests/smoke-test.sh
+++ images/kubeflow-katib/tests/smoke-test.sh
@@ -2,27 +2,29 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+TMPDIR="$(mktemp -d)"
+
+function cleanup {
+  kubectl delete -k "${TMPDIR}"
+
+  rm -v "${TMPDIR}/kubeflow-katib.yaml" || true
+  rm -v "${TMPDIR}/kustomization.yaml" || true
+  rmdir "${TMPDIR}"
+}
+trap cleanup EXIT
+
 # Ref: https://www.kubeflow.org/docs/components/katib/hyperparameter/#installing-katib
-
 NAMESPACE=kubeflow
-KATIB_VERSION=0.15.0
 
-
-function manifests() {
+function manifests {
   # Get the directory path of this script
-  SCRIPT_DIR=$(
-  	cd "$(dirname "$0")"
-  	pwd
-  )
-
-
-  envsubst < $SCRIPT_DIR/kubeflow-katib.yaml.tpl > $SCRIPT_DIR/kubeflow-katib.yaml
-  cat $SCRIPT_DIR/kubeflow-katib.yaml
-  cat <<EOF > kustomization.yaml
+  envsubst < $SCRIPT_DIR/kubeflow-katib.yaml.tpl > "${TMPDIR}/kubeflow-katib.yaml"
+  cat <<EOF > "${TMPDIR}/kustomization.yaml"
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 resources:
-  - github.com/kubeflow/katib.git/manifests/v1beta1/installs/katib-standalone?ref=v${KATIB_VERSION}
+  - github.com/kubeflow/katib.git/manifests/v1beta1/installs/katib-standalone?ref=master
 images:
   - name: docker.io/kubeflowkatib/katib-controller
     newName: ${IMAGE_REPOSITORY_CONTROLLER}
@@ -31,20 +33,18 @@ images:
     newName: ${IMAGE_REPOSITORY_DB_MANAGER}
     newTag: ${IMAGE_REPOSITORY_DB_MANAGER_TAG}
 namespace: ${NAMESPACE}
+configMapGenerator:
+  - name: katib-config
+    behavior: replace
+    files:
+      - katib-config.yaml=kubeflow-katib.yaml
+    options:
+      disableNameSuffixHash: true
 EOF
 }
 
 manifests
-kubectl apply -k .
+kubectl apply -k "${TMPDIR}"
 
 kubectl rollout status -n ${NAMESPACE} deployment/katib-db-manager --timeout=200s 
-kubectl rollout status -n ${NAMESPACE} deployment/katib-controller --timeout=200s 
-
-kubectl delete configmap katib-config -n ${NAMESPACE} || true
-kubectl create configmap katib-config -n ${NAMESPACE} --from-file $SCRIPT_DIR/kubeflow-katib.yaml
-
-kubectl rollout restart -n ${NAMESPACE} deployment/katib-db-manager
-kubectl rollout restart -n ${NAMESPACE} deployment/katib-controller
-
-kubectl rollout status -n ${NAMESPACE} deployment/katib-db-manager --timeout=200s
 kubectl rollout status -n ${NAMESPACE} deployment/katib-controller --timeout=200s
