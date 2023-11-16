484a6546fa1e0a5313d25484fd1e3455c2a45199
Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>
2023-11-16 19:15:23 +0530

    set KUBECONFIG
    Signed-off-by: Shivanshu Raj Shrivastava <shivanshu1333@gmail.com>

--- images/kor/tests/script.sh
+++ images/kor/tests/script.sh
@@ -4,9 +4,11 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 
 git clone https://github.com/yonahd/kor.git
 
+
+export KUBECONFIG=$HOME/.kube/config
+
 helm upgrade -i kor \
     --namespace kor \
     --create-namespace \
     --set cronJob.enabled=true \
     ./kor/charts/kor
-    
