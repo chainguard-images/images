60b3355d27cb3a7d1cdb161ad241df96190ef78c
Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
2023-11-18 20:55:00 +0300

    refactor config module for memcached image
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>

---
+++ images/memcached/configs/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+module "accts" { source = "../../../tflib/accts" }
+
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. memcached)."
+  default     = [""]
+}
+
+output "config" {
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    entrypoint = {
+      command = "/usr/bin/memcached"
+    }
+  })
+}
