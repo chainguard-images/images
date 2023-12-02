c87e20d76ef8306be9a075583d069af8ca85fff7
James Petersen <jpetersenames@gmail.com>
2023-12-01 17:04:05 -0700

    update newrelic-k8s-events-forwarder tests
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/newrelic-k8s-events-forwarder/tests/main.tf
+++ images/newrelic-k8s-events-forwarder/tests/main.tf
@@ -18,6 +18,11 @@ data "oci_string" "ref" {
 
 resource "random_pet" "suffix" {}
 
+resource "random_integer" "random_port" {
+  min = 1025
+  max = 65534
+}
+
 resource "helm_release" "nri-bundle" {
   name             = "newrelic-kef-${random_pet.suffix.id}"
   namespace        = "newrelic-kef-${random_pet.suffix.id}"
@@ -46,6 +51,11 @@ resource "helm_release" "nri-bundle" {
             }
             name = "var-run-newrelic-infra"
           }]
+        }
+        common = {
+          agentConfig = {
+            http_server_port = random_integer.random_port.result
+          }
         }
         images = {
           forwarder = {
