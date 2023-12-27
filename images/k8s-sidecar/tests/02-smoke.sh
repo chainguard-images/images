19242e644ed08fe34362af0ddd56271901d42d87
Furkan Türkal <furkan.turkal@chainguard.dev>
2023-12-27 22:24:16 +0300

    k8s-sidecar: add smoke test
    Signed-off-by: Furkan Türkal <furkan.turkal@chainguard.dev>
    Co-authored-by: Batuhan <batuhan.apaydin@chainguard.dev>

---
+++ images/k8s-sidecar/tests/02-smoke.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefa
+
+tmpdir=$(mktemp -d) && cd "$tmpdir"
+curl -sLO https://raw.githubusercontent.com/fabianlee/blogcode/master/prometheus/pod-count-dashboard.yaml
+sed -i 's/namespace: prom//g' pod-count-dashboard.yaml
+
+kubectl apply -n ${NAMESPACE} -f pod-count-dashboard.yaml
+
+PASSWORD=$(kubectl get secret ${NAME}-grafana -n ${NAMESPACE} -o jsonpath='{.data.admin-password}' | base64 --decode)
+
+kubectl port-forward -n ${NAMESPACE} service/${NAME}-grafana 3000:80 &
+
+sleep 3
+
+RESPONSE=$(curl --retry 5 --retry-all-errors -u admin:${PASSWORD} "http://127.0.0.1:3000/api/search?query=kubernetes%20pod%20count")
+
+if [ "$(echo "$RESPONSE" | jq length)" -eq 0 ]; then
+    echo "Grafana should have at least one Dashboard, exiting..."
+    exit 1
+fi
+
+echo "$RESPONSE" | jq '.[].title' | grep -vi "kubernetes pod count"
--- images/k8s-sidecar/tests/main.tf
+++ images/k8s-sidecar/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
   }
 }
 
@@ -11,4 +12,53 @@ variable "digest" {
 data "oci_exec_test" "runs" {
   digest = var.digest
   script = "${path.module}/01-runs.sh"
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+resource "random_pet" "suffix" {}
+
+resource "helm_release" "k8s-sidecar" {
+  name             = "prometheus-${random_pet.suffix.id}"
+  namespace        = "prometheus-${random_pet.suffix.id}"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  create_namespace = true
+  timeout          = 600
+
+  set {
+    name  = "grafana.sidecar.image.registry"
+    value = data.oci_string.ref.registry
+  }
+  set {
+    name  = "grafana.sidecar.image.repository"
+    value = data.oci_string.ref.repo
+  }
+  set {
+    name  = "grafana.sidecar.image.tag"
+    value = data.oci_string.ref.pseudo_tag
+  }
+}
+
+data "oci_exec_test" "smoke" {
+  digest      = var.digest
+  script      = "./02-smoke.sh"
+  working_dir = path.module
+  depends_on  = [helm_release.k8s-sidecar]
+
+  env {
+    name  = "NAME"
+    value = "prometheus-${random_pet.suffix.id}"
+  }
+  env {
+    name  = "NAMESPACE"
+    value = "prometheus-${random_pet.suffix.id}"
+  }
+}
+
+module "helm_cleanup" {
+  depends_on = [data.oci_exec_test.smoke]
+  source     = "../../../tflib/helm-cleanup"
+  name       = helm_release.k8s-sidecar.id
+  namespace  = helm_release.k8s-sidecar.namespace
 }
