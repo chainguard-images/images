6f4577703ef12fb79f8f2ffcad189584e17c195f
Mark McCormick <mark.mccormick@chainguard.dev>
2023-12-28 11:36:42 +0000

    Update vector tests to allow them to be invoked from another module
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>

--- images/vector/tests/main.tf
+++ images/vector/tests/main.tf
@@ -8,25 +8,36 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+variable "helm_chart_tag" {
+  description = "Whether to pull a specific version of the helm chart for testing."
+  default     = ""
+}
+
 data "oci_string" "ref" { input = var.digest }
 
+resource "random_id" "hex" { byte_length = 4 }
+
 resource "helm_release" "vector" {
-  name       = "vector"
-  repository = "https://helm.vector.dev"
-  chart      = "vector"
+  name              = "vector-${random_id.hex.hex}"
+  repository        = "https://helm.vector.dev"
+  chart             = "vector"
+  create_namespace  = true
+  namespace         = "vector-${random_id.hex.hex}"
+  timeout           = 120
 
   values = [jsonencode({
     localpv = {
       image = {
         registry   = join("", [data.oci_string.ref.registry, "/"])
         repository = data.oci_string.ref.repo
-        tag        = data.oci_string.ref.pseudo_tag
+        tag        = var.helm_chart_tag != "" ? var.helm_chart_tag : data.oci_string.ref.pseudo_tag
       }
     }
   })]
 }
 
 module "helm_cleanup" {
-  source = "../../../tflib/helm-cleanup"
-  name   = helm_release.vector.id
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.vector.id
+  namespace = helm_release.vector.namespace
 }
