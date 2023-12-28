839f2c2c520256f5f9d3d857c1076888051dfa96
Jason Hall <jason@chainguard.dev>
2023-12-28 11:07:07 -0500

    git: refactor to config modules
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/git/wolfi.tf
+++ images/git/wolfi.tf
@@ -1,9 +1,15 @@
+module "wolfi" {
+  for_each = local.accounts
+  source = "./config"
+  root = each.key == "root"
+}
+
 module "latest-wolfi" {
   for_each          = local.accounts
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = file("${path.module}/configs/latest.wolfi.${each.key}.apko.yaml")
+  config            = module.wolfi[each.key].config
   check-sbom        = false # TODO: Not yet conformant: ncurses, libedit, openssh
   build-dev         = true
 }
