d2cceaa6cca8b5e8473ef9561ee7cfcc744e9525
Jason Hall <jason@chainguard.dev>
2023-12-29 15:05:05 -0500

    rust: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/rust/main.tf
+++ images/rust/main.tf
@@ -1,42 +1,44 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest" {
+module "config" { source = "./config" }
+
+module "rust" {
   source             = "../../tflib/publisher"
   name               = basename(path.module)
   target_repository  = var.target_repository
-  config             = file("${path.module}/configs/latest.apko.yaml")
+  config             = module.config.config
+  build-dev          = true
   extra_dev_packages = ["rustup"]
 }
 
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "rust"
-  config  = module.latest.config
-}
-
-module "test-latest" {
+module "test" {
   source = "./tests"
-  digest = module.latest.image_ref
+  digest = module.rust.image_ref
 }
 
-module "test-latest-dev" {
+module "test-dev" {
   source    = "./tests"
-  digest    = module.latest.dev_ref
+  digest    = module.rust.dev_ref
   check-dev = true
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
-  depends_on = [
-    module.test-latest,
-    module.test-latest-dev,
-  ]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : "${t}-dev" => module.latest.dev_ref },
-  )
+resource "oci_tag" "latest" {
+  depends_on = [module.test]
+  digest_ref = module.rust.image_ref
+  tag        = "latest"
 }
+
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test]
+  digest_ref = module.rust.dev_ref
+  tag        = "latest-dev"
+}
+
