c8ec800d814c71b9c6d9fd7831f0f154c76b0ded
Jason Hall <jason@chainguard.dev>
2023-12-30 16:00:39 -0500

    maven: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/maven/main.tf
+++ images/maven/main.tf
@@ -1,22 +1,37 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
+module "config" { source = "./config" }
 
-  depends_on = [
-    module.test-eleven,
-    module.test-seventeen,
-  ]
-
-  tags = merge(
-    { for t in toset(module.version-tags-11.tag_list) : "openjdk-11-${t}" => module.eleven.image_ref },
-    { for t in toset(module.version-tags-11.tag_list) : "openjdk-11-${t}-dev" => module.eleven.dev_ref },
-    { "openjdk-11" : module.eleven.image_ref, "openjdk-11-dev" : module.eleven.dev_ref },
-    { for t in toset(module.version-tags-17.tag_list) : "openjdk-17-${t}" => module.seventeen.image_ref },
-    { for t in toset(module.version-tags-17.tag_list) : "openjdk-17-${t}-dev" => module.seventeen.dev_ref },
-    { "openjdk-17" : module.seventeen.image_ref, "openjdk-17-dev" : module.seventeen.dev_ref },
-    { "latest" : module.seventeen.image_ref, "latest-dev" : module.seventeen.dev_ref },
-  )
+module "maven" {
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  config            = module.config.config
+  build-dev         = true
 }
+
+module "test" {
+  source = "./tests"
+  digest = module.maven.image_ref
+}
+
+resource "oci_tag" "latest" {
+  depends_on = [module.test]
+  digest_ref = module.maven.image_ref
+  tag        = "latest"
+}
+
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test]
+  digest_ref = module.maven.dev_ref
+  tag        = "latest-dev"
+}
+
