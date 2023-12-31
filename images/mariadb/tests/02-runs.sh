ca27f187eecfcfb1b2b6c3978f45cf1b5c98acd6
Jason Hall <jason@chainguard.dev>
2023-12-30 22:23:19 -0500

    mariadb: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/mariadb/tests/02-runs.sh
+++ images/mariadb/tests/02-runs.sh
@@ -3,8 +3,11 @@
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
 # Start up the mariadb container in the background
-docker run -d --name mariadb -e MYSQL_ROOT_PASSWORD=secret "${IMAGE_NAME}"
+name=mariadb-${RANDOM}
+docker run -d --name $name -e MYSQL_ROOT_PASSWORD=secret "${IMAGE_NAME}"
 sleep 5
 
 # Exec into the container and run the mysql command
-docker exec mariadb mysql -u root -psecret -e "SHOW status;" | grep Uptime
+docker exec $name mysql -u root -psecret -e "SHOW status;" | grep Uptime
+
+docker kill $name
