86b413c213fef3094a885b21c36f6b8b60e8cfa7
Jason Hall <jason@chainguard.dev>
2023-12-30 22:53:47 -0500

    migrate more images to config modules
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/powershell/main.tf
+++ images/powershell/main.tf
@@ -1,20 +1,19 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
 module "latest" {
   source = "../../tflib/publisher"
-
   name = basename(path.module)
-
   target_repository = var.target_repository
   config            = file("${path.module}/configs/latest.apko.yaml")
-}
-
-module "version-tags" {
-  source  = "../../tflib/version-tags"
-  package = "powershell"
-  config  = module.latest.config
+  build-dev = true
 }
 
 module "test-latest" {
@@ -22,18 +21,15 @@ module "test-latest" {
   digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source = "../../tflib/tagger"
-
-  depends_on = [
-    module.test-root,
-    module.test-latest,
-  ]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags-root.tag_list)) : "root-${t}" => module.root.image_ref },
-    { "latest-root" : module.root.image_ref },
-
-    { for t in toset(concat(["latest"], module.version-tags.tag_list)) : t => module.latest.image_ref },
-  )
+resource "oci_tag" "latest" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
 }
+
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test-latest]
+  digest_ref = module.latest.dev_ref
+  tag        = "latest-dev"
+}
+
