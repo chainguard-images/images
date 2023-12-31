4a4daf45c119cb87084405c7fd0a54c36d781f45
Jason Hall <jason@chainguard.dev>
2023-12-30 19:14:06 -0500

    nodetaint: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

---
+++ images/nodetaint/config/main.tf
@@ -0,0 +1,20 @@
+variable "extra_packages" {
+  description = "Additional packages to install."
+  type        = list(string)
+  default     = ["nodetaint"]
+}
+
+module "accts" { source = "../../../tflib/accts" }
+
+output "config" {
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    entrypoint = {
+      command = "/usr/bin/nodetaint"
+    }
+  })
+}
+
