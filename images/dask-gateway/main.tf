fe16e94d50382ec2599dff625933ce7fb2acb806
Jason Hall <jason@chainguard.dev>
2024-01-02 09:19:58 -0500

    drop version-tags
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/dask-gateway/main.tf
+++ images/dask-gateway/main.tf
@@ -1,3 +1,9 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
@@ -28,30 +34,21 @@ module "latest" {
   build-dev         = true
 }
 
-module "version-tags" {
-  for_each = local.components
-  source   = "../../tflib/version-tags"
-  package  = each.key
-  config   = module.latest[each.key].config
-}
-
 module "test-latest" {
   source  = "./tests"
   digests = { for k, v in module.latest : k => v.image_ref }
 }
 
-module "tagger" {
-  for_each = local.components
-
-  source = "../../tflib/tagger"
-
-  depends_on = [
-    module.test-latest,
-  ]
-
-  tags = merge(
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : t => module.latest[each.key].image_ref },
-    { for t in toset(concat(["latest"], module.version-tags[each.key].tag_list)) : "${t}-dev" => module.latest[each.key].dev_ref },
-  )
+resource "oci_tag" "latest" {
+  for_each   = local.components
+  depends_on = [module.test-latest]
+  digest_ref = module.latest[each.key].image_ref
+  tag        = "latest"
 }
 
+resource "oci_tag" "latest-dev" {
+  for_each   = local.components
+  depends_on = [module.test-latest]
+  digest_ref = module.latest[each.key].dev_ref
+  tag        = "latest-dev"
+}
