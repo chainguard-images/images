02ea711e8e8ea2dea1216987ef12f4264e15e689
Jason Hall <jason@chainguard.dev>
2024-01-02 08:58:53 -0500

    dask-gateway: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/dask-gateway/main.tf
+++ images/dask-gateway/main.tf
@@ -13,12 +13,18 @@ locals {
   }
 }
 
+module "config" {
+  for_each     = local.components
+  source       = "./config"
+  main_package = each.key
+}
+
 module "latest" {
   for_each          = local.components
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = local.repositories[each.key]
-  config            = file("${path.module}/configs/latest.${each.key}.apko.yaml")
+  config            = module.config[each.key].config
   build-dev         = true
 }
 
