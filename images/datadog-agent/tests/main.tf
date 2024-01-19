afcfba1d7b5a5fbb0a46e6834e87a2369a63dced
Anthony Feddersen <14034769+afeddersen@users.noreply.github.com>
2024-01-19 16:50:56 -0500

    New Image: DataDog Agent (#2086)
    * New Image: DataDog Agent
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * adding metadata.hcl and updating README.md
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * adding correct comments to README file
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * re-running monopod to fix the formatting
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * another shot at monopod
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * anotha one (monopod)
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * anotha one (monopod)
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    * monopod needed to be updated?
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    
    ---------
    
    Signed-off-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>
    Co-authored-by: Anthony Feddersen <afeddersen@Anthonys-MBP.localdomain>

---
+++ images/datadog-agent/tests/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
+variable "digest" {
+  description = "The image digest to run tests over."
+}
+
+data "oci_exec_test" "version" {
+  digest = var.digest
+  script = "docker run --rm $IMAGE_NAME --help"
+}
+
+data "oci_string" "ref" {
+  input = var.digest
+}
