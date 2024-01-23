0d1e22475e3b02c474f525f4f97713f5a93627f8
Jason Hall <jason@chainguard.dev>
2024-01-22 21:25:24 -0500

    flux: migrate to config module
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/flux/main.tf
+++ images/flux/main.tf
@@ -1,3 +1,9 @@
+terraform {
+  required_providers {
+    oci = { source = "chainguard-dev/oci" }
+  }
+}
+
 locals {
   components = toset([
     "cli",
@@ -9,12 +15,9 @@ locals {
     "image-reflector-controller",
   ])
 
-  // Normally the package is named like "flux-{component}"
-  // But some packages are named differently:
-  // - cli -> flux
   packages = merge(
-    { for k, v in local.components : k => "flux-${k}" },
-    { cli = "flux" }
+    {for k in local.components : k => ["flux-${k}"]},
+        {"cli": ["flux", "flux-compat", "busybox"]},
   )
 
   // Normally the repository is named like "flux-{component}"
@@ -30,20 +33,20 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "config" {
+  for_each  = local.repositories
+  source    = "./config"
+  component = each.key
+  packages  = local.packages[each.key]
+}
+
 module "latest" {
   for_each          = local.repositories
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = each.value
-  config            = file("${path.module}/configs/latest.${each.key}.apko.yaml")
+  config            = module.config[each.key].config
   build-dev         = true
-}
-
-module "version-tags" {
-  for_each = local.packages
-  source   = "../../tflib/version-tags"
-  package  = each.value
-  config   = module.latest[each.key].config
 }
 
 module "test-latest" {
@@ -51,21 +54,14 @@ module "test-latest" {
   digests = { for k, v in module.latest : k => v.image_ref }
 }
 
-module "tagger" {
-  for_each = local.components
-  source   = "../../tflib/tagger"
+resource "oci_tag" "latest" {
+  for_each   = local.components
+  digest_ref = module.latest[each.key].image_ref
+  tag        = "latest"
+}
 
-  depends_on = [module.test-latest]
-
-  tags = merge(
-    { "latest" : module.latest[each.key].image_ref },
-    { "latest-dev" : module.latest[each.key].dev_ref },
-    { for t in module.version-tags[each.key].tag_list : t => module.latest[each.key].image_ref },
-    { for t in module.version-tags[each.key].tag_list : "${t}-dev" => module.latest[each.key].dev_ref },
-
-    # This will also tag the image with :v1, :v1.2, :v1.2.3, :v1.2.3-r4, for compatibility with upstream Helm charts.
-    # TODO(jason): Do this for all images, not just flux, and potentially only for `:v1.2.3` and `:v1.2.3-r4` (not `:v1` or `:v1.2`).
-    { for t in module.version-tags[each.key].tag_list : "v${t}" => module.latest[each.key].image_ref },
-    { for t in module.version-tags[each.key].tag_list : "v${t}-dev" => module.latest[each.key].dev_ref },
-  )
+resource "oci_tag" "latest-dev" {
+  for_each   = local.components
+  digest_ref = module.latest[each.key].dev_ref
+  tag        = "latest-dev"
 }
