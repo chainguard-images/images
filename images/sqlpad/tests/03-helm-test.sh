d96dffe1cc3666dfec9be2f3a25973f25c043d86
Josh Wolf <josh@wolfs.io>
2024-01-24 10:48:16 -0500

    use helm's builtin wait so we don't race
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- images/sqlpad/tests/03-helm-test.sh
+++ images/sqlpad/tests/03-helm-test.sh
@@ -12,11 +12,9 @@ cd ${TMPDIR}/sqlpad/sqlpad-charts
 helm dependency update
 helm upgrade --install --namespace sqlpad sqlpad \
     --create-namespace \
+    --wait \
     --set image.repository=$IMAGE_REGISTRY_REPO \
     --set image.tag=$IMAGE_TAG \
     ./
 
 trap "helm uninstall sqlpad -n sqlpad" EXIT
-
-# check that sqlpad is up and running app=sqlpad label
-kubectl wait --for=condition=ready pod -l app=sqlpad -n sqlpad --timeout=300s
