fe0e536157622e75400fc5d182f25eb43a7be113
Mark McCormick <mark.mccormick@chainguard.dev>
2024-02-02 04:41:33 +0000

    update spark-operator image
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>

--- images/spark-operator/tests/main.tf
+++ images/spark-operator/tests/main.tf
@@ -9,26 +9,3 @@ variable "digest" {
 }
 
 data "oci_string" "ref" { input = var.digest }
-
-resource "helm_release" "spark-operator" {
-  name = "spark-operator"
-
-  repository = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
-  chart      = "spark-operator"
-
-  namespace        = "spark-operator"
-  create_namespace = true
-
-  values = [jsonencode({
-    image = {
-      tag        = data.oci_string.ref.pseudo_tag
-      repository = data.oci_string.ref.registry_repo
-    }
-  })]
-}
-
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.spark-operator.id
-  namespace = helm_release.spark-operator.namespace
-}
