189d9cbf91c70abaa05df4d3d4f38396ea38f84d
Batuhan Apaydın <batuhan.apaydin@chainguard.dev>
2024-02-03 21:32:03 +0300

    add openjdk-11 to management-api-for-apache-cassandra (#2161)
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>

--- images/management-api-for-apache-cassandra/config/main.tf
+++ images/management-api-for-apache-cassandra/config/main.tf
@@ -6,7 +6,13 @@ terraform {
 
 variable "extra_packages" {
   description = "The additional packages to install."
-  default     = ["management-api-for-apache-cassandra", "management-api-for-apache-cassandra-compat", "metric-collector-for-apache-cassandra", "cassandra", "cassandra-compat"]
+  default = [
+    "management-api-for-apache-cassandra",
+    "management-api-for-apache-cassandra-compat",
+    "metric-collector-for-apache-cassandra",
+    "cassandra",
+    "cassandra-compat",
+  "openjdk-11-default-jvm"]
 }
 
 data "apko_config" "this" {
