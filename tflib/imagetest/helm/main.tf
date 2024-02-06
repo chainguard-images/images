137ccf76352d2ebc730de887df52ee70d1a1bd33
James Petersen <jpetersenames@gmail.com>
2024-02-06 13:50:51 -0700

    Reference local values and chart with helm module (#2177)
    * Reference local values and chart with helm module
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * fix extra space
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * Update tflib/imagetest/helm/main.tf
    
    Co-authored-by: Josh Wolf <josh@wolfs.io>
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * Update tflib/imagetest/helm/main.tf
    
    Co-authored-by: Josh Wolf <josh@wolfs.io>
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * address feedback
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    ---------
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    Co-authored-by: Josh Wolf <josh@wolfs.io>

--- tflib/imagetest/helm/main.tf
+++ tflib/imagetest/helm/main.tf
@@ -1,29 +1,37 @@
 variable "name" {
-  default     = ""
   description = "The name of the helm release. One will be generated if not provided"
+  default     = ""
 }
 
 variable "namespace" {
   description = "The namespace to install the helm release into."
+  default     = "default"
 }
 
 variable "chart" {
-  description = "The name of the helm chart to install."
+  description = "The name of, or path to the helm chart to install."
 }
 
 variable "repo" {
-  description = "The helm repo to install the chart from."
+  description = "The helm repo to install the chart from. When not specified, a local helm chart will be assumed."
+  default     = ""
 }
 
 variable "chart_version" {
-  default     = ""
   description = "The version of the helm chart to install. The latest will be used if none is provided."
+  default     = ""
 }
 
 variable "values" {
   description = "A map of values to pass to the helm chart."
   type        = any
   default     = {}
+}
+
+variable "values_files" {
+  description = "Path to values file on the local filesystem."
+  type        = list(string)
+  default     = []
 }
 
 variable "timeout" {
@@ -36,11 +44,12 @@ locals {
 apk add helm
 helm install ${var.name} ${var.chart} \
   --namespace ${var.namespace} --create-namespace \
-  --repo ${var.repo} \
-  --wait --wait-for-jobs \
-  --timeout ${var.timeout} \
+  %{if var.repo != ""}--repo ${var.repo}%{endif} \
   %{if var.name == ""}--generate-name%{endif} \
   %{if var.chart_version != ""}--version ${var.chart_version}%{endif} \
+  --wait --wait-for-jobs \
+  --timeout ${var.timeout} \
+  --values ${join(",", var.values_files)} \
   --values <(echo '${jsonencode(var.values)}')
   EOa
 
