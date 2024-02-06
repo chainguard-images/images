cda45160a8fe90d00ef4ffe9e6301bb4bc43b8c5
mamccorm <mark.mccormick@chainguard.dev>
2024-02-06 02:23:37 +0000

    Add Apache spark (dependency), and adding tests (#2160)
    * Adding apache spark as a dependency, and expanding automated tests
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    * fix metadata.hcl to align with readme
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    * test updates
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>
    
    ---------
    
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>

--- images/spark-operator/tests/main.tf
+++ images/spark-operator/tests/main.tf
@@ -8,27 +8,54 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_string" "ref" { input = var.digest }
+data "oci_string" "ref" {
+  input = var.digest
+}
 
-resource "helm_release" "spark-operator" {
-  name = "spark-operator"
+resource "random_pet" "suffix" {}
 
-  repository = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
-  chart      = "spark-operator"
-
-  namespace        = "spark-operator"
+resource "helm_release" "spark" {
+  name             = "spark"
+  repository       = "oci://registry-1.docker.io/bitnamicharts"
+  chart            = "spark"
+  namespace        = "spark-${random_pet.suffix.id}"
   create_namespace = true
 
   values = [jsonencode({
-    image = {
-      tag        = data.oci_string.ref.pseudo_tag
-      repository = data.oci_string.ref.registry_repo
+    worker = {
+      replicaCount = 1
     }
   })]
 }
 
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.spark-operator.id
-  namespace = helm_release.spark-operator.namespace
+resource "helm_release" "operator" {
+  depends_on       = [resource.helm_release.spark]
+  name             = "spark-operator"
+  repository       = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
+  chart            = "spark-operator"
+  namespace        = resource.helm_release.spark.namespace
+  create_namespace = true
+  values = [jsonencode({
+    image = {
+      registry   = ""
+      repository = data.oci_string.ref.registry_repo,
+      tag        = data.oci_string.ref.pseudo_tag
+    },
+  })]
+}
+
+data "oci_exec_test" "run-tests" {
+  depends_on  = [resource.helm_release.operator]
+  digest      = var.digest
+  script      = "./test-spark.sh"
+  working_dir = path.module
+
+  env {
+    name  = "NAMESPACE"
+    value = resource.helm_release.spark.namespace
+  }
+  env {
+    name  = "IMAGE"
+    value = "${data.oci_string.ref.registry_repo}:${data.oci_string.ref.pseudo_tag}"
+  }
 }
