dc9389f2a9eef71a1cf14bde58cb3f6677e138d0
James Petersen <jpetersenames@gmail.com>
2024-02-06 09:24:42 -0700

    Add prometheus-logstash-exporter image (#2169)
    * add prometheus-logstash-exporter image
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * add some comments on tests
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * terraform fmt told me to
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    * fix readme
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>
    
    ---------
    
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

---
+++ images/prometheus-logstash-exporter/tests/main.tf
@@ -0,0 +1,142 @@
+terraform {
+  required_providers {
+    oci       = { source = "chainguard-dev/oci" }
+    helm      = { source = "hashicorp/helm" }
+    imagetest = { source = "chainguard-dev/imagetest" }
+  }
+}
+
+variable "digest" {
+  description = "The image digests to run tests over."
+}
+
+data "oci_string" "ref" {
+  input = var.digest
+}
+
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_k3s" "this" {
+  name      = "prometheus-logstash-exporter"
+  inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
+}
+
+module "helm_prometheus" {
+  source = "../../../tflib/imagetest/helm"
+
+  namespace = "default"
+  chart     = "prometheus"
+  repo      = "https://prometheus-community.github.io/helm-charts"
+  name      = "prometheus"
+
+  values = {
+    server = {
+      global = {
+        scrape_interval     = "5s",
+        scrape_timeout      = "4s",
+        evaluation_interval = "1m",
+      }
+      persistentVolume = {
+        enabled = false
+      }
+    }
+    serverFiles = {
+      "prometheus.yml" = {
+        scrape_configs = [
+          {
+            job_name        = "logstash-exporter"
+            scrape_interval = "5s"
+            scrape_timeout  = "5s"
+            metrics_path    = "/metrics"
+            scheme          = "http"
+            static_configs = [
+              {
+                targets = ["exporter-logstash-exporter:9198"]
+              }
+            ]
+          }
+        ]
+      }
+    }
+    alertmanager             = { enabled = false }
+    kube-state-metrics       = { enabled = false }
+    prometheus-node-exporter = { enabled = false }
+    prometheus-pushgateway   = { enabled = false }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  harness     = imagetest_harness_k3s.this
+  name        = "Basic"
+  description = "Basic functionality of the logstash image."
+
+  steps = [
+    {
+      name = "Helm install prometheus"
+      cmd  = module.helm_prometheus.install_cmd
+    },
+    # logstash has a weird values file that didn't quite work with the helm
+    # module for imagetest
+    # TODO: make it so this can work with imagestest/helm
+    {
+      name = "Helm install logstash"
+      cmd  = <<EOF
+        apk add helm
+        helm repo add elastic https://helm.elastic.co
+        helm upgrade --install logstash elastic/logstash \
+            --values /tests/values/logstash.values.yaml \
+            --wait \
+            --wait-for-jobs
+      EOF
+    },
+
+    # The logstash-exporter project does not have an official helm chart. There
+    # was progress on the official prometheus charts repo but that PR has been
+    # stale for a while. This developer has taken over development for the
+    # logstash-exporter and has included a chart but has not published it yet.
+    # There is an issue to publish the chart https://github.com/kuskoman/logstash-exporter/issues/76
+    # For now we just need to clone the repo and use the chart
+    {
+      name = "Helm install logstash-exporter"
+      cmd  = <<EOF
+        apk add git helm
+        git clone https://github.com/kuskoman/logstash-exporter.git
+        helm upgrade --install exporter ./logstash-exporter/chart \
+          --set logstash.urls[0]="http://logstash-logstash-headless:9600" \
+          --set logstash.logging.level="debug" \
+          --set image.repository="${data.oci_string.ref.registry_repo}" \
+          --set image.tag="${data.oci_string.ref.pseudo_tag}" \
+          --set image.pullPolicy="Always" \
+          --wait \
+          --wait-for-jobs
+      EOF
+    },
+    {
+      name = "Query metrics"
+      cmd  = <<EOF
+        apk add kubectl curl jq
+        kubectl port-forward svc/prometheus-server 9090:80 & 
+
+        # Wait for api to become available
+        until curl http://localhost:9090/api/v1/label/__name__/values; do sleep 1; done
+
+        # Ensure we see logstash metrics
+        curl -L http://localhost:9090/api/v1/label/__name__/values | \
+            jq -r '.data[]' | grep -E '^logstash_'
+      EOF
+    },
+  ]
+
+  labels = {
+    type = "k8s"
+  }
+}
