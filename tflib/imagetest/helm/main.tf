0d9c82a55ab077eab3482db4eaa9ead8abdf8fbd
Josh Wolf <josh@wolfs.io>
2024-02-09 13:15:38 -0500

    emit more useful logs from failed imagetest helm installs
    Signed-off-by: Josh Wolf <josh@wolfs.io>

--- tflib/imagetest/helm/main.tf
+++ tflib/imagetest/helm/main.tf
@@ -1,69 +1,45 @@
-variable "name" {
-  description = "The name of the helm release. One will be generated if not provided"
-  default     = ""
-}
-
-variable "namespace" {
-  description = "The namespace to install the helm release into."
-  default     = "default"
-}
-
-variable "chart" {
-  description = "The name of, or path to the helm chart to install."
-}
-
-variable "repo" {
-  description = "The helm repo to install the chart from. When not specified, a local helm chart will be assumed."
-  default     = ""
-}
-
-variable "chart_version" {
-  description = "The version of the helm chart to install. The latest will be used if none is provided."
-  default     = ""
-}
-
-variable "values" {
-  description = "A map of values to pass to the helm chart."
-  type        = any
-  default     = {}
-}
-
-variable "values_files" {
-  description = "Path to values file on the local filesystem."
-  type        = list(string)
-  default     = []
-}
-
-variable "timeout" {
-  description = "The timeout on the helm install."
-  default     = "5m"
+terraform {
+  required_providers {
+    random = { source = "hashicorp/random" }
+  }
 }
 
 locals {
-  install_cmd = <<-EOa
+  name = var.name != "" ? var.name : random_pet.name.id
+}
+
+resource "random_pet" "name" {}
+
+locals {
+  install_cmd = <<-EOinstall
 apk add helm
-helm install ${var.name} ${var.chart} \
+if ! helm install ${local.name} ${var.chart} \
   --namespace ${var.namespace} --create-namespace \
   %{if var.repo != ""}--repo ${var.repo}%{endif} \
-  %{if var.name == ""}--generate-name%{endif} \
   %{if var.chart_version != ""}--version ${var.chart_version}%{endif} \
   --wait --wait-for-jobs \
   --timeout ${var.timeout} \
   %{if length(var.values_files) != 0}--values ${join(",", var.values_files)}%{endif} \
-  --values <(echo '${jsonencode(var.values)}')
-  EOa
+  --values <(echo '${jsonencode(var.values)}'); then
 
-  uninstall_cmd = <<-EOb
+  printf "\\nFailed to install helm chart\\n"
+
+  printf "\\n\\nGet Pods:\\n\\n"
+  kubectl get pods -n ${var.namespace} -l app.kubernetes.io/instance=${local.name} || true
+
+  printf "\\n\\nDescribe Pods:\\n\\n"
+  kubectl describe pods -n ${var.namespace} -l app.kubernetes.io/instance=${local.name} || true
+
+  printf "\\n\\nEvents:\\n\\n"
+  kubectl get events --field-selector type!=Normal --sort-by=.metadata.creationTimestamp -o wide -n ${var.namespace} || true
+
+  exit 1
+fi
+  EOinstall
+
+  uninstall_cmd = <<-EOuninstall
 helm uninstall ${var.name} --namespace ${var.namespace} \
   --wait \
   --timeout ${var.timeout}
-  EOb
-}
-
-output "install_cmd" {
-  value = local.install_cmd
-}
-
-output "uninstall_cmd" {
-  value = local.uninstall_cmd
+  EOuninstall
 }
