a54cba58bca61fb02f5f1db50f65205de4d82bac
James Petersen <jpetersenames@gmail.com>
2024-02-13 15:49:48 -0700

    add missing packages for non-dev variant
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/logstash-oss-with-opensearch-output-plugin/config/main.tf
+++ images/logstash-oss-with-opensearch-output-plugin/config/main.tf
@@ -14,6 +14,12 @@ variable "extra_packages" {
     "logstash-with-output-opensearch",
     "logstash-env2yaml",
     "logstash-compat",
+    # Logstash is basically composed of a bunch of wrapper scripts to set up an
+    # environment and then execute the proper code. It fails with a bunch of
+    # 'command not found' errors if busybox is missing and a 'docker-entrypoint
+    # not found' if bash is missing.
+    "busybox",
+    "bash",
   ]
 }
 
