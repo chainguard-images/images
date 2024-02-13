236ba28ff05279c48d7bf3537b08399b149c7908
James Petersen <jpetersenames@gmail.com>
2024-02-13 16:48:51 -0700

    convert to full terraform config
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/logstash-oss-with-opensearch-output-plugin/config/main.tf
+++ images/logstash-oss-with-opensearch-output-plugin/config/main.tf
@@ -23,11 +23,76 @@ variable "extra_packages" {
   ]
 }
 
-data "apko_config" "this" {
-  config_contents = file("${path.module}/template.apko.yaml")
-  extra_packages  = var.extra_packages
+variable "entrypoint" {
+  description = "The image entrypoint"
+  default = {
+    command = "/usr/bin/docker-entrypoint"
+  }
+}
+
+variable "paths" {
+  description = "File paths for the image"
+  default     = []
+}
+
+variable "environment" {
+  description = "Environment variables to add to the image"
+  default     = {}
+}
+
+module "accts" {
+  source = "../../../tflib/accts"
+  name   = "logstash"
+  gid    = 1000
+  uid    = 1000
+  run-as = 1000
 }
 
 output "config" {
-  value = jsonencode(data.apko_config.this.config)
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    environment = merge(var.environment, {
+      "LS_JAVA_HOME"      = "/usr/lib/jvm/default-jvm",
+      "ELASTIC_CONTAINER" = "true"
+      "LANG"              = "en_US.UTF-8"
+    })
+    paths = concat(var.paths, [
+      {
+        path        = "/usr/share/logstash",
+        type        = "directory",
+        uid         = module.accts.block.run-as,
+        gid         = module.accts.block.run-as,
+        permissions = 502, # o766
+        recursive   = true,
+      },
+      {
+        path        = "/usr/share/logstash/logs",
+        type        = "directory",
+        uid         = 1000,
+        gid         = 1000,
+        permissions = 502, # o766
+        recursive   = true,
+      },
+      {
+        path        = "/usr/share/logstash/bin",
+        type        = "directory",
+        uid         = 1000,
+        gid         = 1000,
+        permissions = 493, # o755
+        recursive   = true,
+      },
+      {
+        path        = "/opt/logstash",
+        type        = "hardlink",
+        source      = "/usr/share/logstash",
+        uid         = 1000,
+        gid         = 1000,
+        permissions = 502, # o766
+      },
+    ])
+    entrypoint = var.entrypoint
+  })
 }
