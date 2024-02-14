c6910b7cded58de3e6f1f2b9198bc5363b4f45e8
Jason Hall <jason@chainguard.dev>
2024-02-14 21:31:46 +0000

    terraform: move latest to BUSL version, add :1.5.7 for last pre-BUSL version
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/terraform/main.tf
+++ images/terraform/main.tf
@@ -8,32 +8,66 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest-config" {
+module "pre-busl-config" {
   source         = "./config"
   extra_packages = ["terraform<1.6"]
 }
 
-module "latest" {
+module "latest-busl-config" {
+  source         = "./config"
+  extra_packages = ["terraform"]
+}
+
+module "pre-busl" {
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = module.latest-config.config
+  config            = module.pre-busl-config.config
   build-dev         = true
 }
 
-module "test-latest" {
+module "test-pre-busl" {
   source = "./tests"
-  digest = module.latest.image_ref
+  digest = module.pre-busl.image_ref
 }
 
-resource "oci_tag" "latest" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.image_ref
+module "latest-busl" {
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  config            = module.latest-busl-config.config
+  build-dev         = true
+}
+
+module "test-latest-busl" {
+  source = "./tests"
+  digest = module.latest-busl.image_ref
+}
+
+resource "oci_tag" "latest-busl" {
+  depends_on = [module.test-latest-busl]
+  digest_ref = module.latest-busl.image_ref
   tag        = "latest"
 }
 
-resource "oci_tag" "latest-dev" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.dev_ref
+resource "oci_tag" "latest-busl-dev" {
+  depends_on = [module.test-latest-busl]
+  digest_ref = module.latest-busl.dev_ref
   tag        = "latest-dev"
+}
+
+locals { pre_busl_tags = toset(["1.5.7", "1.5"]) }
+
+resource "oci_tag" "pre-busl" {
+  for_each   = local.pre_busl_tags
+  depends_on = [module.test-pre-busl]
+  digest_ref = module.pre-busl.image_ref
+  tag        = each.key
+}
+
+resource "oci_tag" "pre-busl-dev" {
+  for_each   = local.pre_busl_tags
+  depends_on = [module.test-pre-busl]
+  digest_ref = module.pre-busl.dev_ref
+  tag        = "${each.key}-dev"
 }
