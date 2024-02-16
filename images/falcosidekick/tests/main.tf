16b37464f82c7692797d4a432ace5080ac778247
Aditvil-Dev <aditya.tirmanwar@chainguard.dev>
2024-02-16 13:30:49 -0500

    Helm charts repo added
    Signed-off-by: Aditvil-Dev <aditya.tirmanwar@chainguard.dev>

--- images/falcosidekick/tests/main.tf
+++ images/falcosidekick/tests/main.tf
@@ -15,7 +15,7 @@ resource "random_pet" "suffix" {}
 resource "helm_release" "helm" {
   name             = "falcosidekick-${random_pet.suffix.id}"
   namespace        = "falcosidekick"
-  repository       = "falcosecurity/falcosidekick"
+  repository       = "https://falcosecurity.github.io/charts/"
   chart            = "falcosidekick"
   create_namespace = true
 
