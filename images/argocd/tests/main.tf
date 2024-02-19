99d285957aa5a855e3188e123047d878b1358412
James Petersen <jpetersenames@gmail.com>
2024-02-19 12:00:28 -0700

    migrate argocd tests
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/argocd/tests/main.tf
+++ images/argocd/tests/main.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
-    oci  = { source = "chainguard-dev/oci" }
-    helm = { source = "hashicorp/helm" }
+    oci       = { source = "chainguard-dev/oci" }
+    helm      = { source = "hashicorp/helm" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+data "imagetest_inventory" "this" {}
 
 variable "digests" {
   description = "The image digests to run tests over."
@@ -18,57 +21,52 @@ data "oci_string" "ref" {
   input    = each.value
 }
 
-resource "helm_release" "argocd" {
-  name = "argocd"
+resource "imagetest_harness_k3s" "this" {
+  name      = "argocd"
+  inventory = data.imagetest_inventory.this
 
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-
-  namespace        = "argocd"
-  create_namespace = true
-
-  # The argocd helm chart installs CRDs from the `templates/` directory,
-  # and the default value (`false`) conflicts with the providers
-  # assumption of them being in `crds/`
-  skip_crds = true
-
-  values = [
-    jsonencode({
-      image = {
-        tag        = data.oci_string.ref["server"].pseudo_tag
-        repository = data.oci_string.ref["server"].registry_repo
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
       }
-      repoServer = {
-        image = {
-          tag        = data.oci_string.ref["repo-server"].pseudo_tag
-          repository = data.oci_string.ref["repo-server"].registry_repo
-        }
-      }
-      # redis-ha = {
-      #   enabled = true
-      #   #   image = {
-      #   #     repository = "cgr.dev/chainguard/redis"
-      #   #     tag        = "latest"
-      #   #   }
-      #   haproxy = {
-      #     image = {
-      #       repository = "k3d-k3d.localhost:5005/cg/haproxy"
-      #       tag        = "2.8@sha256:d9a67188af5cb89f5398399f704e75079a25e241481d22205de38637bd9052c0"
-      #     }
-      #
-      #     containerSecurityContext = {
-      #       capabilities = {
-      #         add = ["NET_BIND_SERVICE"]
-      #       }
-      #     }
-      #   }
-      # }
-    }),
-  ]
+    ]
+  }
 }
 
-# module "helm_cleanup" {
-#   source    = "../../../tflib/helm-cleanup"
-#   name      = helm_release.argocd.id
-#   namespace = helm_release.argocd.namespace
-# }
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+  chart  = "argo-cd"
+  repo   = "https://argoproj.github.io/argo-helm"
+  name   = "argocd"
+  values = {
+    image = {
+      tag        = data.oci_string.ref["server"].pseudo_tag
+      repository = data.oci_string.ref["server"].registry_repo
+    }
+    repoServer = {
+      image = {
+        tag        = data.oci_string.ref["repo-server"].pseudo_tag
+        repository = data.oci_string.ref["repo-server"].registry_repo
+      }
+    }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  harness     = imagetest_harness_k3s.this
+  name        = "Basic"
+  description = "Basic functionality of the argocd image."
+
+  steps = [
+    {
+      name = "Helm install argocd"
+      cmd  = module.helm.install_cmd
+    },
+  ]
+
+  labels = {
+    type = "k8s"
+  }
+}
