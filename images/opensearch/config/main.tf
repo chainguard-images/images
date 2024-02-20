69f32faa8e5b21342660e7617dfcb9dffb0481e3
Mritunjay Kumar Sharma <mritunjay.sharma@chainguard.dev>
2024-02-20 18:51:51 +0530

    Support passing in environment variables for opensearch (#2248)
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>
    Signed-off-by: Sergio Gonzalez <sergio.alfredo@chainguard.dev>

--- images/opensearch/config/main.tf
+++ images/opensearch/config/main.tf
@@ -9,9 +9,19 @@ variable "extra_packages" {
   default     = ["opensearch-2"]
 }
 
+variable "environment" {
+  description = "The additional environment variables to set"
+  default     = {}
+  type        = map(string)
+}
+
+locals { decoded = yamldecode(file("${path.module}/template.apko.yaml")) }
 data "apko_config" "this" {
-  config_contents = file("${path.module}/template.apko.yaml")
-  extra_packages  = var.extra_packages
+  config_contents = yamlencode(merge(
+    local.decoded,
+    { environment = merge(local.decoded.environment, var.environment) },
+  ))
+  extra_packages = var.extra_packages
 }
 
 output "config" {
