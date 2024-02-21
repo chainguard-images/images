a7838e21ebf01a26d920e7818ee20839042cc296
James Rawlings <jrawlings@chainguard.dev>
2024-02-21 18:51:11 +0000

    opensearch-dashboards: add empty extra_packages variable block to we can override it
    Signed-off-by: James Rawlings <jrawlings@chainguard.dev>

--- images/opensearch-dashboards/config/main.tf
+++ images/opensearch-dashboards/config/main.tf
@@ -4,8 +4,14 @@ terraform {
   }
 }
 
+variable "extra_packages" {
+  description = "The additional packages to install (e.g. opensearch-dashboards-2)."
+  default     = []
+}
+
 data "apko_config" "this" {
   config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
 }
 
 output "config" {
