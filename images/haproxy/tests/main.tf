db112957c0474705e78c332d68822ce0dec323c1
Jamon Camisso <jamon@chainguard.dev>
2024-02-21 16:47:52 -0500

    Update haproxy image test to use haproxy helm chart
    Signed-off-by: Jamon Camisso <jamon@chainguard.dev>

--- images/haproxy/tests/main.tf
+++ images/haproxy/tests/main.tf
@@ -14,32 +14,27 @@ data "oci_string" "ref" {
 
 resource "random_pet" "suffix" {}
 
-resource "helm_release" "redis-ha" {
-  name             = "haproxy-redis-${random_pet.suffix.id}"
-  namespace        = "haproxy-system-${random_pet.suffix.id}"
-  repository       = "https://dandydeveloper.github.io/charts"
-  chart            = "redis-ha"
+resource "helm_release" "haproxy" {
+  name             = "haproxy-${random_pet.suffix.id}"
+  namespace        = "haproxy-${random_pet.suffix.id}"
+  repository       = "https://haproxytech.github.io/helm-charts"
+  chart            = "haproxy"
   create_namespace = true
   timeout          = 300
 
   values = [
     jsonencode({
       image = {
-        registry = "cgr.dev/chainguard/redis"
-        tag      = "latest"
-      }
-      hardAntiAffinity = false
-      haproxy = {
-        enabled          = true
-        hardAntiAffinity = false
-        image = {
-          repository = data.oci_string.ref.registry_repo
-          tag        = data.oci_string.ref.pseudo_tag
-        }
-        containerSecurityContext = {
-          capabilities = { add = ["NET_BIND_SERVICE"] }
-        }
+        repository = data.oci_string.ref.registry_repo
+        tag      = data.oci_string.ref.pseudo_tag
       }
     })
   ]
+}
+
+
+module "helm_cleanup" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.haproxy.id
+  namespace = helm_release.haproxy.namespace
 }
