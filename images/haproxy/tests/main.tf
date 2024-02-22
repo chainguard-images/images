02d7fa28066c99f55d19577618e6cc236da7e740
Jamon Camisso <jamon@chainguard.dev>
2024-02-22 09:45:27 -0500

    Add haproxy helm chart for extra test coverage
    Signed-off-by: Jamon Camisso <jamon@chainguard.dev>

--- images/haproxy/tests/main.tf
+++ images/haproxy/tests/main.tf
@@ -14,6 +14,7 @@ data "oci_string" "ref" {
 
 resource "random_pet" "suffix" {}
 
+
 resource "helm_release" "haproxy" {
   name             = "haproxy-${random_pet.suffix.id}"
   namespace        = "haproxy-${random_pet.suffix.id}"
@@ -26,15 +27,50 @@ resource "helm_release" "haproxy" {
     jsonencode({
       image = {
         repository = data.oci_string.ref.registry_repo
-        tag      = data.oci_string.ref.pseudo_tag
+        tag        = data.oci_string.ref.pseudo_tag
       }
     })
   ]
 }
 
-
-module "helm_cleanup" {
+module "helm_cleanup_haproxy" {
   source    = "../../../tflib/helm-cleanup"
   name      = helm_release.haproxy.id
   namespace = helm_release.haproxy.namespace
+}
+
+resource "helm_release" "redis-ha" {
+  name             = "haproxy-redis-${random_pet.suffix.id}"
+  namespace        = "haproxy-system-${random_pet.suffix.id}"
+  repository       = "https://dandydeveloper.github.io/charts"
+  chart            = "redis-ha"
+  create_namespace = true
+  timeout          = 300
+
+  values = [
+    jsonencode({
+      image = {
+        registry = "cgr.dev/chainguard/redis"
+        tag      = "latest"
+      }
+      hardAntiAffinity = false
+      haproxy = {
+        enabled          = true
+        hardAntiAffinity = false
+        image = {
+          repository = data.oci_string.ref.registry_repo
+          tag        = data.oci_string.ref.pseudo_tag
+        }
+        containerSecurityContext = {
+          capabilities = { add = ["NET_BIND_SERVICE"] }
+        }
+      }
+    })
+  ]
+}
+
+module "helm_cleanup_redis_ha" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.redis-ha.id
+  namespace = helm_release.redis-ha.namespace
 }
