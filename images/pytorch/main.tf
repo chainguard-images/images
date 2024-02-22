f9b3e5d0bc2e446c86431c54000dd3983eea28a9
RJ Sampson <rj.sampson@chainguard.dev>
2024-02-22 16:51:47 -0700

    fix(pytorch): Tag with latest and latest-dev
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- images/pytorch/main.tf
+++ images/pytorch/main.tf
@@ -4,42 +4,36 @@ terraform {
   }
 }
 
-locals {
-  versions = [
-    "pytorch-cuda12",
-  ]
-}
-
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-
 module "config" {
-  for_each       = toset(local.versions)
   source         = "./config"
-  extra_packages = [each.key, "busybox", "bash"]
+  extra_packages = ["pytorch-cuda12", "busybox", "bash"]
 }
 
-module "versioned" {
-  for_each = toset(local.versions)
-  source   = "../../tflib/publisher"
-
+module "latest" {
+  source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = module.config[each.key].config
+  config            = module.config.config
   build-dev         = true
-  main_package      = each.key
 }
 
 module "test" {
-  for_each = toset(local.versions)
-  source   = "./tests"
-  digest   = module.versioned[each.key].image_ref
+  source = "./tests"
+  digest = module.latest.image_ref
 }
 
-module "tagger" {
-  source     = "../../tflib/tagger"
+resource "oci_tag" "latest" {
   depends_on = [module.test]
-  tags       = merge([for v in local.versions : module.versioned[v].latest_tag_map]...)
+  digest_ref = module.latest.image_ref
+  tag        = "latest"
+}
+
+resource "oci_tag" "latest-dev" {
+  depends_on = [module.test]
+  digest_ref = module.latest.dev_ref
+  tag        = "latest-dev"
 }
