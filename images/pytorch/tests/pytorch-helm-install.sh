57bb9b2e4af9301728a4c9ad706cb8eaaa200c20
RJ Trujillo <rj.sampson@chainguard.dev>
2024-02-23 03:52:49 -0700

    chore: Move pytorch to images (#2263)

---
+++ images/pytorch/tests/pytorch-helm-install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# Tests pytorch with its helm chart and runs sample scripts to 
+# confirm its functionality
+#
+# This helm installation is intendended to run on devices with only cpu;
+# to test features utilizing GPU, please refer TESTING.md
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+my_d=$(cd "${0%/*}" && pwd)
+test_script="/home/runner/work/images-private/images-private/images/pytorch/tests/torch_optim.py"
+exit_code=1
+
+run_scripts() {
+  pod_name=$(kubectl get pods -n ${RELEASE_NAMESPACE} -l "app.kubernetes.io/instance=${RELEASE_NAME}" -o custom-columns=:metadata.name --no-headers | head -n 1)
+  kubectl cp "${my_d}/torch_optim.py" "$pod_name":/tmp/pytorch.py -n ${RELEASE_NAMESPACE}
+  kubectl exec "$pod_name" -n ${RELEASE_NAMESPACE} -- python /tmp/pytorch.py
+  exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+      printf "Test ran successfully"
+    return 0
+  fi
+  echo "FAILED: Unable to run test scripts"
+  exit 1
+}
+
+run_scripts
+
