507ac804c31c0b9b01c7c5fb6ada482ed42f70e7
Sergio Gonzalez <sergio.alfredo@chainguard.dev>
2024-02-28 12:01:16 -0600

    Change smoke test to create a secret
    Signed-off-by: Sergio Gonzalez <sergio.alfredo@chainguard.dev>

--- images/doppler-kubernetes-operator/tests/smoke_test.sh
+++ images/doppler-kubernetes-operator/tests/smoke_test.sh
@@ -6,23 +6,46 @@ sleep 5
 
 DELAY=5
 NAMESPACE="doppler-operator-system"
-POD_SELECTOR="doppler-operator-controller-manager"
+CONTROLLER_POD=$(kubectl get pods -n "$NAMESPACE" -o=jsonpath='{.items[0].metadata.name}')
+SECRET_NAME="doppler-token-secret"
+DUMMY_SECRET_NAME="my-doppler-secret"
 TIMER=0
 TIMEOUT=60
 
-# Loop until the current time is greater than the end time
+echo "Step 1: Create a dummy secret in Kubernetes"
+kubectl create secret generic "$SECRET_NAME" --from-literal=token="$DUMMY_SECRET_NAME" -n "$NAMESPACE"
+
+echo "Step 2: Checking the operator status..."
+kubectl wait --for=condition=Ready pod -n "$NAMESPACE" "$CONTROLLER_POD" --timeout="$TIMEOUT"s
+
+echo "Step 3: Creating the DopplerHQ secret resource..."
+cat <<EOF | kubectl apply -f -
+apiVersion: secrets.doppler.com/v1alpha1
+kind: DopplerSecret
+metadata:
+  name: $SECRET_NAME
+  namespace: $NAMESPACE
+spec:
+  tokenSecret:
+    name: "$SECRET_NAME"
+  managedSecret:
+    name: "$SECRET_NAME"
+    namespace: "$NAMESPACE"
+EOF
+
+# Loop until the secret is created or the time limit is reached
 while [ $TIMER -lt $TIMEOUT ]; do
-  # Run the command and check for the pod is running
-  STATUS=$(kubectl get pods -n "$NAMESPACE" -o jsonpath='{.items[*].status.phase}')
-  if echo "$STATUS" | grep -q "Running"; then
-    echo "All good, its running"
-      exit 0
+  # Check if the secret exists in the namespace
+  SECRET=$(kubectl get secret -n "$NAMESPACE" "$SECRET_NAME" --no-headers 2>/dev/null)
+  if [ -n "$SECRET" ]; then
+    echo "¡The secret $SECRET_NAME was found in the namespace $NAMESPACE!"
+    exit 0
   fi
 
-	# Sleep for the specified delay, then increment the timer
-	sleep $DELAY
-	TIMER=$(($TIMER + $DELAY))
+  # Wait the specified delay and then increment the timer
+  sleep $DELAY
+  TIMER=$(($TIMER + $DELAY))
 done
 
-echo "Failed to start within 3 minutes"
+echo "Could not create secret $SECRET_NAME within $TIMEOUT seconds!"
 exit 1
