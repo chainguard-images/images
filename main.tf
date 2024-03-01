ff74d1f04af30879ae67e29e71d869bbc4f0872a
srishtih <srishti.hegde.sh@gmail.com>
2024-03-01 06:00:20 -0800

    Add latest nemo image (#2287)

--- main.tf
+++ main.tf
@@ -855,6 +855,11 @@ module "nats" {
   target_repository = "${var.target_repository}/nats"
 }
 
+module "nemo" {
+  source            = "./images/nemo"
+  target_repository = "${var.target_repository}/nemo"
+}
+
 module "netcat" {
   source            = "./images/netcat"
   target_repository = "${var.target_repository}/netcat"
