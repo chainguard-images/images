3979cf66ab322e4e477b3250297b29e57c0018de
Carly <carly.maggiolo@gmail.com>
2024-03-04 14:54:19 -0500

    add imagetest
    Signed-off-by: Carly <carly.maggiolo@gmail.com>

--- images/helm-operator/tests/main.tf
+++ images/helm-operator/tests/main.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     oci  = { source = "chainguard-dev/oci" }
     helm = { source = "hashicorp/helm" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -9,24 +10,50 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-resource "random_pet" "suffix" {}
-
-resource "helm_release" "helm" {
-  name  = "helm-operator-${random_pet.suffix.id}"
-  chart = "https://bsgrigorov.github.io/helm-operator/helm-operator-0.0.2.tgz"
-
-  values = [
-    jsonencode({
-      image = {
-        operator   = "data.oci_string.ref.registry_repo:data.oci_string.ref.pseudo_tag"
-        pullPolicy = "Always"
-      }
-    })
-  ]
+data "oci_string" "ref" {
+  input = var.digest
 }
 
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.helm.id
-  namespace = helm_release.helm.namespace
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_k3s" "this" {
+  name      = "helm-operator"
+  inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+  chart  = "https://bsgrigorov.github.io/helm-operator/helm-operator-0.0.2.tgz"
+  name   = "helm-operator"
+  values = {
+        image = {
+          operator   = "data.oci_string.ref.registry_repo:data.oci_string.ref.pseudo_tag"
+        }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  harness     = imagetest_harness_k3s.this
+  name        = "Basic"
+  description = "Basic functionality of the helm operator helm chart."
+
+  steps = [
+    {
+      name = "Helm install"
+      cmd  = module.helm.install_cmd
+    },
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
