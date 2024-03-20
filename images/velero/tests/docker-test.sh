e320678d4b1b4e8b8b299edf950643eb5668b6df
Aditvil-Dev <aditya.tirmanwar@chainguard.dev>
2024-03-20 12:01:40 -0400

    Adding test functionality
    Signed-off-by: Aditvil-Dev <aditya.tirmanwar@chainguard.dev>

--- images/velero/tests/docker-test.sh
+++ images/velero/tests/docker-test.sh
@@ -11,17 +11,61 @@ install_velero(){
                --image ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}
 }
 
+wait_for_pod() {
+  local label=$1
+  local namespace=$2
+  local timeout=$3
+  local start_time=$(date +%s)
+  local end_time=$((start_time + timeout))
+
+  while true; do
+    if [ "$(date +%s)" -gt "$end_time" ]; then
+      echo "Timeout waiting for pod with label $label to be created"
+      exit 1
+    fi
+
+    if kubectl get pods -n "$namespace" -l "$label" 2>/dev/null | grep -q Running; then
+      break
+    fi
+
+    sleep 5
+  done
+}
+
 test_velero(){
-  
-  CONTAINER_NAME = $(docker ps --format "{{.Names}}" | grep "velero" | head -n 1)
-  
-  docker exec ${CONTAINER_NAME} /velero backup create my-backup --include-namespace default
 
-  docker exec ${CONTAINER_NAME} /velero restore create --from-backup my-backup
+  wait_for_pod "component=velero" "velero" 300
 
-  docker exec ${CONTAINER_NAME} /velero restore get
+  VELERO_POD_NAME=$(kubectl get pods -n velero -l component=velero -o jsonpath='{.items[0].metadata.name}')
+  STATUS=$(kubectl get pod $VELERO_POD_NAME -n velero -o jsonpath='{.status.phase}')
+
+  if [ "$STATUS" == "Running" ]; then
+    echo "Velero pod is running"
+  else
+    echo "Velero pod is not running"
+  fi
+
+  # Create a test backup
+  velero backup create my-backup --include-namespaces default
+
+  # Check if the backup is created
+  if ! velero backup get | grep -q "my-backup"; then
+    echo "Backup creation failed"
+    exit 1
+  fi
+
+  # Restore the test backup
+  velero restore create --from-backup my-backup
+
+  # Check if the restore backup created anything
+  if kubectl get restore -n velero; then
+    echo "Restore backup created successfully"
+  else
+    echo "Restore backup not created"
+    exit 1
+  fi
 }
 
 apk add velero velero-compat velero-restore-helper
 install_velero
-# test_velero
+test_velero
