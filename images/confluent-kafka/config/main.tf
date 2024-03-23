3316ff146329c125dd51b7c0f5fd800c1a25ab17
RJ Sampson <rj.sampson@chainguard.dev>
2024-03-22 18:14:23 -0600

    fix(confluent-kafka): Add busybox
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- images/confluent-kafka/config/main.tf
+++ images/confluent-kafka/config/main.tf
@@ -7,7 +7,7 @@ terraform {
 variable "extra_packages" {
   description = "Additional packages to install."
   type        = list(string)
-  default     = ["confluent-kafka"]
+  default     = ["busybox", "confluent-kafka"]
 }
 
 variable "environment" {
