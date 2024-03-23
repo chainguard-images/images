c46944e80dab4b1abebf9d166c78b5afa479b926
RJ Sampson <rj.sampson@chainguard.dev>
2024-03-22 18:21:48 -0600

    fix(confluent-kafka): Add openjdk-17
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- images/confluent-kafka/config/main.tf
+++ images/confluent-kafka/config/main.tf
@@ -7,7 +7,7 @@ terraform {
 variable "extra_packages" {
   description = "Additional packages to install."
   type        = list(string)
-  default     = ["busybox", "confluent-kafka"]
+  default     = ["busybox", "confluent-kafka", "openjdk-17-default-jvm"]
 }
 
 variable "environment" {
