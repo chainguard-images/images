006c3b2d254eb0c197ce66392971040173310832
Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
2024-03-25 20:58:40 +0300

    velero plugins
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
    Co-authored-by: Furkan Turkal <furkan.turkal@chainguard.dev>
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>

---
+++ images/velero-plugin-for-aws/README.md
@@ -0,0 +1,42 @@
+<!--monopod:start-->
+# velero-plugin-for-aws
+| | |
+| - | - |
+| **OCI Reference** | `cgr.dev/chainguard/velero-plugin-for-aws` |
+
+
+* [View Image in Chainguard Academy](https://edu.chainguard.dev/chainguard/chainguard-images/reference/velero-plugin-for-aws/overview/)
+* [View Image Catalog](https://console.enforce.dev/images/catalog) for a full list of available tags.
+* [Contact Chainguard](https://www.chainguard.dev/chainguard-images) for enterprise support, SLAs, and access to older tags.*
+
+---
+<!--monopod:end-->
+
+<!--overview:start-->
+Plugins to support Velero on AWS
+<!--overview:end-->
+
+<!--getting:start-->
+## Download this Image
+The image is available on `cgr.dev`:
+
+```
+docker pull cgr.dev/chainguard/velero-plugin-for-aws:latest
+```
+<!--getting:end-->
+
+<!--body:start-->
+
+##  Usage
+
+The [Velero plugin for AWS documentation](https://velero.io/plugins/) can be found on Velero's official website. Velero have a guide on [getting started with Velero plugin for AWS](https://github.com/vmware-tanzu/velero-plugin-for-aws#setup), which demonstrates how to the plugin to backup and restore your Kubernetes cluster resources to AWS S3.  
+
+To use the Chainguard Image version of the plugin, follow the Velero instructions and replace the `velero/velero-plugin-for-aws` image with `cgr.dev/chainguard/velero-plugin-for-aws` in the Velero's install command.  
+
+```bash
+$ velero install \
+    --provider aws \
+    --plugins cgr.dev/chainguard/velero-plugin-for-aws:latest \
+   ...
+```
+<!--body:end-->
---
+++ images/velero-plugin-for-aws/tests/docker-test.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail -x
+
+
+# Function to install Velero using Minio as the backup storage
+install_velero(){
+  kubectl apply -f https://raw.githubusercontent.com/vmware-tanzu/velero/main/examples/minio/00-minio-deployment.yaml
+
+  # Create a credentials file for Minio
+  cat <<EOF > credentials-velero
+  [default]
+  aws_access_key_id = minio
+  aws_secret_access_key = minio123
+EOF
+
+  velero install --bucket velero \
+                 --secret-file ./credentials-velero \
+                 --use-volume-snapshots=false \
+                 --backup-location-config region=minio,s3ForcePathStyle="true",s3Url=http://minio.velero.svc:9000 \
+                 --provider aws \
+                 --plugins ${IMAGE_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG} \
+                 --image ${VELERO_IMAGE_REGISTRY}/${VELERO_IMAGE_REPOSITORY}:${VELERO_IMAGE_TAG} --dry-run -oyaml > velero-install.yaml
+              
+  awk '
+  BEGIN {change=0}
+  /initContainers:/ {inInit=1}
+  inInit && /name: testing-velero-plugin-for-aws:unused/ && !change {print "          name: velero-plugin-for-aws"; change=1; next}
+  {print}
+  ' velero-install.yaml > updated-velero-install.yaml
+
+  cat updated-velero-install.yaml
+
+  # this is a workaround to fix the issue with the CRD not being created
+  set +e
+  kubectl apply -f updated-velero-install.yaml
+  sleep 10
+  set -e
+  kubectl apply -f updated-velero-install.yaml
+}
+
+# Function to wait for a pod with a specific label to be running
+wait_for_pod() {
+  local label=$1
+  local namespace=$2
+  local timeout=$3
+  local start_time=$(date +%s)
+  local end_time=$((start_time + timeout))
+
+  while true; do
+    if [ "$(date +%s)" -gt "$end_time" ]; then
+      echo "Timeout waiting for pod with label $label to be created"
+      exit 1
+    fi
+
+    if kubectl get pods -n "$namespace" -l "$label" 2>/dev/null | grep -q Running; then
+      break
+    fi
+
+    sleep 5
+  done
+}
+
+# Function to test Velero by creating a backup and restoring it
+test_velero(){
+
+  wait_for_pod "component=velero" "velero" 300
+
+  VELERO_POD_NAME=$(kubectl get pods -n velero -l component=velero -o jsonpath='{.items[0].metadata.name}')
+  STATUS=$(kubectl get pod $VELERO_POD_NAME -n velero -o jsonpath='{.status.phase}')
+
+  if [ "$STATUS" == "Running" ]; then
+    echo "Velero pod is running"
+  else
+    echo "Velero pod is not running"
+  fi
+
+
+  kubectl apply -f https://raw.githubusercontent.com/vmware-tanzu/velero/main/examples/nginx-app/base.yaml
+
+
+  # Create a test backup
+  velero backup create nginx-backup --selector app=nginx -w
+
+  # Simulate a disaster
+  kubectl delete namespace nginx-example
+
+  # Restore from backup
+  velero restore create --from-backup nginx-backup -w
+  
+  restore_status=$(velero restore get -o json | jq -r '.status.phase')
+  if [ "$restore_status" == "Completed" ]; then
+    echo "Backup creation successful"
+    break
+  elif [ "$restore_status" == "Failed" ]; then
+    echo "Backup creation failed"
+    exit 1
+  fi
+}
+
+apk add velero velero-compat velero-restore-helper jq
+install_velero
+test_velero
