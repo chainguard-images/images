296127c455a6d8757964d821dbffee761771313e
Sergio Gonzalez <sergio.alfredo@chainguard.dev>
2024-03-25 15:50:15 -0600

    Add helm
    Signed-off-by: Sergio Gonzalez <sergio.alfredo@chainguard.dev>

--- images/multus-cni/tests/deploy.sh
+++ images/multus-cni/tests/deploy.sh
@@ -2,4 +2,47 @@
 
 set -o errexit -o nounset -o errtrace -o pipefail -x
 
-# TODO: Implement this test.
+# Replace "multus-cni" with the name of your Helm release
+HELM_RELEASE="multus-cni"
+
+POD_SPEC='apiVersion: v1
+kind: Pod
+metadata:
+  name: test-pod
+spec:
+  containers:
+  - name: test-container
+    image: busybox
+    command:
+      - /bin/sh
+      - -c
+      - "trap : TERM INT; (while true; do sleep 1; done) & wait"
+    stdin: true
+    tty: true
+'
+
+# Create the test pod
+kubectl apply -f - <<< "$POD_SPEC"
+
+# Wait until the pod is in "Running" state
+echo "Waiting for pod to be in 'Running' state..."
+while true; do
+  POD_STATUS=$(kubectl get pod test-pod -o jsonpath='{.status.phase}')
+  if [ "$POD_STATUS" = "Running" ]; then
+    break
+  fi
+  sleep 1
+done
+echo "Pod is now in 'Running' state."
+
+# Get the IP address of the pod
+POD_IP=$(kubectl get pod test-pod -o jsonpath='{.status.podIPs[0].ip}')
+
+# Test network connectivity
+echo "Testing connectivity to Google..."
+ping -c 3 google.com
+
+# Delete the test pod
+kubectl delete pod test-pod
+
+exit 0
--- images/multus-cni/tests/main.tf
+++ images/multus-cni/tests/main.tf
@@ -16,23 +16,21 @@ data "oci_exec_test" "manifest" {
 
 resource "random_pet" "suffix" {}
 
-# resource "helm_release" "helm" {
-#   name             = "multus-cni-${random_pet.suffix.id}"
-#   namespace        = "multus-cni-${random_pet.suffix.id}"
-#   repository       = "https://startechnica.github.io/apps"
-#   chart            = "multus-cni"
-#   version          = "0.1.4"
-#   create_namespace = true
+resource "helm_release" "helm" {
+  name             = "multus-cni-${random_pet.suffix.id}"
+  namespace        = "multus-cni-${random_pet.suffix.id}"
+  repository       = "https://startechnica.github.io/apps"
+  chart            = "multus-cni"
+  version          = "0.1.4"
+  create_namespace = true
 
-#   values = [
-#     jsonencode({
-#       // TODO:
-#     })
-#   ]
-# }
+  values = [file("${path.module}/values.yaml")]
 
-# module "helm_cleanup" {
-#   source    = "../../../tflib/helm-cleanup"
-#   name      = helm_release.helm.id
-#   namespace = helm_release.helm.namespace
-# }
+}
+
+module "helm_cleanup" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.helm.id
+  namespace = helm_release.helm.namespace
+  depends_on = [ helm_release.helm ]
+}
