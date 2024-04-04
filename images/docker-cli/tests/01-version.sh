6499af0705efca310f483cf0822ffc828247332d
Mauren Berti <mauren.berti@chainguard.dev>
2024-04-04 12:01:47 -0400

    fix(docker-cli): remove pipefail option from test script
    Signed-off-by: Mauren Berti <mauren.berti@chainguard.dev>

--- images/docker-cli/tests/01-version.sh
+++ images/docker-cli/tests/01-version.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit -o nounset -o errtrace -o pipefail -x
+# Cannot use -o pipefail here because that causes the docker run command to fail immediately
+set -o errexit -o nounset -o errtrace -x
 
-docker run --rm ${IMAGE_NAME} version 2>&1 | grep -qi "Cannot connect to the Docker daemon"
+docker run --rm ${IMAGE_NAME} version 2>&1 | grep -i 'cannot connect to the Docker daemon'
