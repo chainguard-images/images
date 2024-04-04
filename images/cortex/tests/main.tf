05a8c11630ef68285ca3e4f1dcaa039ef2c3cffc
Sam Singh Anantha <sam.singh@chainguard.dev>
2024-04-04 11:08:53 -0400

    add helm test
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>

--- images/cortex/tests/main.tf
+++ images/cortex/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     oci = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,33 +9,78 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-// Invoke a script with the test.
-// $IMAGE_NAME is populated with the image name by digest.
-// TODO: Update or remove this test as appropriate.
-data "oci_exec_test" "manifest" {
-  digest      = var.digest
-  script      = "./EXAMPLE_TEST.sh"
-  working_dir = path.module
+data "oci_string" "ref" { input = var.digest }
+
+# // Invoke a script with the test.
+# // $IMAGE_NAME is populated with the image name by digest.
+# // TODO: Update or remove this test as appropriate.
+# data "oci_exec_test" "manifest" {
+#   digest      = var.digest
+#   script      = "./EXAMPLE_TEST.sh"
+#   working_dir = path.module
+# }
+
+# resource "random_pet" "suffix" {}
+
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_k3s" "this" {
+  name      = "cortex"
+  inventory = data.imagetest_inventory.this
+    sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
 }
 
-resource "random_pet" "suffix" {}
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
 
-resource "helm_release" "helm" {
-  name             = "cortex-${random_pet.suffix.id}"
-  namespace        = "cortex-${random_pet.suffix.id}"
-  repository       = "https://cortexproject.github.io/cortex-helm-chart" // TODO
-  chart            = "cortex-helm/cortex" // TODO
-  create_namespace = true
+  name          = "cortex"
+  namespace     = "cortex"
+  repo          = "https://cortexproject.github.io/cortex-helm-chart"
+  chart         = "cortex"
 
-  values = [
-    jsonencode({
-      // TODO:
-    })
+  values = {
+    image = {
+      tag        = data.oci_string.ref.pseudo_tag
+      repository = data.oci_string.ref.registry_repo
+    },
+    config = {
+      blocks_storage= {
+        backend = "filesystem"
+        filesystem = {
+          dir = "/data/tsdb"
+        }
+      }
+    },
+    alertmanager = {
+      enabled = false
+    },
+    ruler = {
+      enabled = false
+    }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "Basic"
+  description = "Test a basic installation of Cortex Metrics"
+  harness     = imagetest_harness_k3s.this
+
+  steps = [
+    {
+      name = "Helm install"
+      cmd  = module.helm.install_cmd
+    }
   ]
+
+  labels = {
+    type = "k8s",
+  }
 }
 
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.helm.id
-  namespace = helm_release.helm.namespace
-}
