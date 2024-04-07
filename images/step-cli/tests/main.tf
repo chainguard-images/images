76233471bffc0fa0a7918fc62e4b1438aa7bb7c9
Steve Garf <garf@chainguard.dev>
2024-04-07 16:05:54 -0700

    Add functional test for step-cli
    Signed-off-by: Steve Garf <garf@chainguard.dev>

--- images/step-cli/tests/main.tf
+++ images/step-cli/tests/main.tf
@@ -8,7 +8,7 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "version" {
+data "oci_exec_test" "generate_otp" {
   digest = var.digest
-  script = "docker run --rm $IMAGE_NAME version"
+  script = "docker run --rm -v $PWD/step:/home/step $IMAGE_NAME crypto otp generate --issuer smallstep.com --account name@smallstep.com --qr /home/step/smallstep.png"
 }
