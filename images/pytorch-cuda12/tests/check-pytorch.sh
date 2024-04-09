e6f1396d6bedec777eb10ef4603e1242a9269fa1
RJ Trujillo <rj.sampson@chainguard.dev>
2024-04-09 15:07:28 -0600

    fix(pytorch-cuda12): Correct failing tests (#2459)
    * fix(pytorch-cuda12): Disable read-only filesystem during tests
    
    Security defaults changed in Bitnami's Helm chart: https://github.com/bitnami/charts/commit/f15941902ff7f289f7897dbec5884a4e1f1bbb8f
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>
    
    * fix(pytorch-cuda12): Set GID in tests to 0
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>
    
    * fix(pytorch-cuda12): Increase persistant memory
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>
    
    * fix(pytorch-cuda12): Disable resource requests on container init
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>
    
    * chore(pytorch-cuda12): Move to imagetest harness
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>
    
    ---------
    
    Signed-off-by: RJ Sampson <rj.sampson@chainguard.dev>

--- images/pytorch-cuda12/tests/check-pytorch.sh
+++ images/pytorch-cuda12/tests/check-pytorch.sh
@@ -13,12 +13,12 @@ my_d=$(cd "${0%/*}" && pwd)
 exit_code=1
 
 run_scripts() {
-  pod_name=$(kubectl get pods -n ${RELEASE_NAMESPACE} -l "app.kubernetes.io/instance=${RELEASE_NAME}" -o custom-columns=:metadata.name --no-headers | head -n 1)
-  kubectl cp "${my_d}/quickstart.py" "$pod_name":/tmp/pytorch.py -n ${RELEASE_NAMESPACE}
-  kubectl exec "$pod_name" -n ${RELEASE_NAMESPACE} -- python /tmp/pytorch.py
+  pod_name=$(kubectl get pods -n pytorch -l "app.kubernetes.io/instance=pytorch" -o custom-columns=:metadata.name --no-headers | head -n 1)
+  kubectl cp "${my_d}/quickstart.py" "$pod_name":/tmp/pytorch.py -n pytorch
+  kubectl exec "$pod_name" -n pytorch -- python /tmp/pytorch.py
   exit_code=$?
   if [ $exit_code -eq 0 ]; then
-      printf "Test ran successfully"
+    printf "Test ran successfully"
     return 0
   fi
   echo "FAILED: Unable to run test scripts"
