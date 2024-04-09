de241029c9606d9e55453e887ae24094d6d75c04
Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>
2024-04-09 11:22:34 +0100

    tesseract: add new image
    Add a new image for tesseract - an OCR software. Test case ensures
    that English OCR works.
    
    Signed-off-by: Dimitri John Ledkov <dimitri.ledkov@chainguard.dev>

---
+++ images/tesseract/config/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install"
+  # Install tesseract itself, orientation & size & english datasets
+  default = ["tesseract", "tesseract-osd", "tesseract-eng"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
