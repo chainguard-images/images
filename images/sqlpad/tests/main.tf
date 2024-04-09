716377881ffc4324a5e0a9a900990245ba884d72
James Petersen <jpetersenames@gmail.com>
2024-04-09 15:36:58 -0600

    migrate sqlpad to imagetest
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/sqlpad/tests/main.tf
+++ images/sqlpad/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,25 +9,64 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+data "oci_string" "ref" { input = var.digest }
+
+# TODO: Convert to imagetest_harness_container when available
 data "oci_exec_test" "manifest" {
   digest      = var.digest
   script      = "./02-curl-test.sh"
   working_dir = path.module
 }
 
-data "oci_string" "ref" { input = var.digest }
-# Will work only when we use updated version postgresql 13.2.23 in upstream charts https://github.com/sqlpad/sqlpad/blob/f42ce1a84e1327e69178f38e7b3a2462dcaeeb7c/sqlpad-charts/requirements.yaml#L3
-# Have created PR for this in upstream https://github.com/sqlpad/sqlpad/pull/1212
-data "oci_exec_test" "helm-install" {
-  digest = var.digest
-  script = "${path.module}/03-helm-test.sh"
+data "imagetest_inventory" "this" {}
 
-  env {
-    name  = "IMAGE_REGISTRY_REPO"
-    value = data.oci_string.ref.registry_repo
+resource "imagetest_harness_k3s" "this" {
+  name      = "sqlpad"
+  inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
   }
-  env {
-    name  = "IMAGE_TAG"
-    value = data.oci_string.ref.pseudo_tag
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+  chart  = "."
+  name   = "sqlpad"
+  values = {
+    image = {
+      repository = data.oci_string.ref.registry_repo
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  }
+}
+
+# Will work only when we use updated version postgresql 13.2.23 in upstream
+# charts https://github.com/sqlpad/sqlpad/blob/master/sqlpad-charts/requirements.yaml#L3
+resource "imagetest_feature" "basic" {
+  harness     = imagetest_harness_k3s.this
+  name        = "Basic"
+  description = "Basic functionality of the logstash image."
+
+  steps = [
+    {
+      name = "Helm install"
+      cmd  = <<EOF
+        apk add helm
+        git clone https://github.com/sqlpad/sqlpad.git
+        cd ./sqlpad/sqlpad-charts
+        helm dependency update
+        ${module.helm.install_cmd}
+      EOF
+    },
+  ]
+
+  labels = {
+    type = "k8s"
   }
 }
