f63f2fa33a2adf9095740facafa6cbba3872afe5
Jamon Camisso <jamonation@users.noreply.github.com>
2024-04-15 13:14:28 -0400

    Migrate jenkins to use imagetest_harness_k3s for tests (#2499)
    Signed-off-by: Jamon <jamon@chainguard.dev>

---
+++ images/jenkins/tests/controller/main.tf
@@ -0,0 +1,47 @@
+// default values - these get overridden when the resource is called
+variable "values" {
+  type = any
+  default = {
+    namespace        = "jenkins"
+    fullnameOverride = "jenkins"
+    nameOverride     = "jenkins"
+    create_namespace = true
+
+    controller = {
+      javaOpts    = ""
+      jenkinsOpts = ""
+      admin = {
+        createSecret = false
+      }
+      installPlugins = false
+      sidecars = {
+        configAutoReload = {
+          enabled = false
+        }
+      }
+      image = {
+        registry   = "cgr.dev"
+        repository = "chainguard/jenkins"
+        tag        = "latest"
+      }
+    }
+  }
+}
+
+module "helm" {
+  source = "../../../../tflib/imagetest/helm"
+
+  namespace = "jenkins"
+  chart     = "jenkins"
+  repo      = "https://charts.jenkins.io"
+
+  values = var.values
+}
+
+output "install_cmd" {
+  value = module.helm.install_cmd
+}
+
+output "release_name" {
+  value = module.helm.release_name
+}
--- images/jenkins/tests/main.tf
+++ images/jenkins/tests/main.tf
@@ -1,19 +1,78 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
+}
+
+locals {
+  namespace = "jenkins"
 }
 
 variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "version" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME --version"
+data "oci_string" "ref" { input = var.digest }
+
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_k3s" "this" {
+  name      = "jenkins"
+  inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
 }
 
-data "oci_exec_test" "run-tests" {
-  digest = var.digest
-  script = "${path.module}/run-tests.sh"
+module "helm_controller" {
+  source = "./controller"
+  values = {
+    namespace        = local.namespace
+    fullnameOverride = local.namespace
+    nameOverride     = local.namespace
+    create_namespace = true
+
+    controller = {
+      javaOpts       = ""
+      jenkinsOpts    = ""
+      installPlugins = false
+      admin = {
+        createSecret = false
+      }
+      sidecars = {
+        configAutoReload = {
+          enabled = false
+        }
+      }
+      image = {
+        registry   = data.oci_string.ref.registry
+        repository = data.oci_string.ref.repo
+        tag        = data.oci_string.ref.pseudo_tag
+      }
+    }
+  }
+}
+
+resource "imagetest_feature" "this" {
+  harness     = imagetest_harness_k3s.this
+  name        = "jenkins"
+  description = "Test jenkins functionality"
+
+  steps = [
+    {
+      name = "Install controller",
+      cmd  = module.helm_controller.install_cmd
+    },
+    {
+      name = "Test running a job",
+      cmd  = "/tests/hello-world.sh"
+    },
+  ]
 }
