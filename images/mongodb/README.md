6a71b8fdd5a284b14beaaa57036088236f6f5bb2
Carly <carly.maggiolo@gmail.com>
2024-04-17 12:35:47 -0400

    public image
    Signed-off-by: Carly <carly.maggiolo@gmail.com>

---
+++ images/mongodb/README.md
@@ -0,0 +1,28 @@
+<!--monopod:start-->
+# mongodb
+| | |
+| - | - |
+| **OCI Reference** | `cgr.dev/chainguard/mongodb` |
+
+
+* [View Image in Chainguard Academy](https://edu.chainguard.dev/chainguard/chainguard-images/reference/mongodb/overview/)
+* [View Image Catalog](https://console.enforce.dev/images/catalog) for a full list of available tags.
+* [Contact Chainguard](https://www.chainguard.dev/chainguard-images) for enterprise support, SLAs, and access to older tags.*
+
+---
+<!--monopod:end-->
+
+<!--overview:start-->
+
+<!--overview:end-->
+
+<!--getting:start-->
+## Download this Image
+The image is available on `cgr.dev`:
+
+```
+docker pull cgr.dev/chainguard/mongodb:latest
+```
+<!--getting:end-->
+
+<!--body:start--><!--body:end-->
---
+++ images/mongodb/config/main.tf
@@ -0,0 +1,34 @@
+
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install"
+  // TODO: Add any other packages here you want to conditionally include,
+  // or update this default to [] if this isn't a version stream image.
+  default = [
+    "mongodb-7.3",
+  ]
+}
+
+variable "extra_repositories" {
+  description = "The additional repositores to install from (e.g. extras)."
+  default     = ["https://packages.cgr.dev/extras"]
+}
+
+variable "extra_keyring" {
+  description = "The additional keys to use (e.g. extras)."
+  default     = ["https://packages.cgr.dev/extras/chainguard-extras.rsa.pub"]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
