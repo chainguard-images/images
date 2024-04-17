537ea8e43a6a0d07c375a07b22ef605f4d5776d6
Carly <carly.maggiolo@gmail.com>
2024-04-17 13:04:03 -0400

    minor change
    Signed-off-by: Carly <carly.maggiolo@gmail.com>

--- images/mongodb/tests/main.tf
+++ images/mongodb/tests/main.tf
@@ -8,9 +8,6 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-// Invoke a script with the test.
-// $IMAGE_NAME is populated with the image name by digest.
-// TODO: Update or remove this test as appropriate.
 data "oci_exec_test" "manifest" {
   digest      = var.digest
   script      = "test.sh"
