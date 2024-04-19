cae72287ef83b6af8357c857ebffd42644033d89
Josh Dolitsky <josh@dolit.ski>
2024-04-19 10:02:32 -0500

    Add lights versions tflib, convert zookeeper to use
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/zookeeper/main.tf
+++ images/zookeeper/main.tf
@@ -1,36 +1,42 @@
-terraform {
-  required_providers {
-    oci = { source = "chainguard-dev/oci" }
-  }
-}
-
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest-config" { source = "./config" }
+module "versions" {
+  source  = "../../tflib/versions"
+  package = "zookeeper"
+}
 
-module "latest" {
+module "config" {
+  for_each = module.versions.versions
+  source   = "./config"
+  extra_packages = [
+    each.key,
+    "${replace(each.key, "zookeeper", "zookeeper-bitnami")}-compat",
+  ]
+}
+
+module "versioned" {
+  for_each          = module.versions.versions
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = module.latest-config.config
+  config            = module.config[each.key].config
   build-dev         = true
+  main_package      = each.value.main
+  update-repo       = each.value.is_latest
 }
 
-module "test-latest" {
-  source = "./tests"
-  digest = module.latest.image_ref
-}
+#module "test" {
+#  for_each = module.versions.versions
+#  source   = "./tests"
+#  digest   = module.versioned[each.key].image_ref
+#}
 
-resource "oci_tag" "latest" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.image_ref
-  tag        = "latest"
-}
-
-resource "oci_tag" "latest-dev" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.dev_ref
-  tag        = "latest-dev"
+module "tagger" {
+  source = "../../tflib/tagger"
+  #depends_on = [module.test]
+  tags = merge(
+    [for v in module.versioned : v.latest_tag_map]...
+  )
 }
