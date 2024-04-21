c1e4f470132d71f08b954851ae2cbda5ed18d64b
Sam Singh Anantha <30288538+sanghanan@users.noreply.github.com>
2024-04-21 15:22:19 -0400

    fix(kube-fluentd-operator): update helm chart and config (#2510)
    * port helm_release to imagetest/helm
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>
    
    * bump the correct version
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>
    
    * resolve reloader dependency
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>
    
    * update gem path
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>
    
    * remove redundant gem path
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>
    
    ---------
    
    Signed-off-by: Sam Singh Anantha <sam.singh@chainguard.dev>

--- images/kube-fluentd-operator/tests/main.tf
+++ images/kube-fluentd-operator/tests/main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
-    oci    = { source = "chainguard-dev/oci" }
-    helm   = { source = "hashicorp/helm" }
-    random = { source = "hashicorp/random" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -10,39 +9,61 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_string" "ref" { input = var.digest }
+data "oci_string" "ref" {
+  input = var.digest
+}
 
-resource "helm_release" "kfo" {
-  name             = "kfo"
-  namespace        = "kfo"
-  chart            = "https://github.com/vmware/kube-fluentd-operator/releases/download/v1.17.4/log-router-0.4.0.tgz" # not sure how we can get the correct version of the chart rather than hardcoding a version below
-  create_namespace = true
-  timeout          = 120
+data "imagetest_inventory" "this" {}
 
-  values = [
-    <<EOF
-rbac:
-  create: true
-image:
-  repository: "${data.oci_string.ref.registry_repo}"
-  tag: "${data.oci_string.ref.pseudo_tag}"
-EOF
+resource "imagetest_harness_k3s" "this" {
+  name      = "kfo"
+  inventory = data.imagetest_inventory.this
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
+}
+
+module "helm" {
+  source    = "../../../tflib/imagetest/helm"
+  name      = "kfo"
+  namespace = "kfo"
+  chart     = "https://github.com/vmware/kube-fluentd-operator/releases/download/v1.18.1/log-router-0.4.0.tgz"
+  values = {
+    rbac = {
+      create = true
+    },
+    image = {
+      repository = data.oci_string.ref.registry_repo,
+      tag        = data.oci_string.ref.pseudo_tag
+    }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "Basic"
+  description = "Test a basic installation of kube-fluentd-operator"
+  harness     = imagetest_harness_k3s.this
+
+  steps = [
+    {
+      name = "Helm install"
+      cmd  = module.helm.install_cmd
+    },
+    {
+      name    = "Smoke test"
+      workdir = "/tests"
+      cmd     = <<EOF
+        ./smoke-test.sh
+      EOF
+    },
   ]
-}
 
-data "oci_exec_test" "smoke" {
-  digest = var.digest # This doesn't actually matter here, just pass it something valid
-
-  # This script calls other files in the same relative directory
-  working_dir = path.module
-  script      = "./smoke-test.sh"
-
-  depends_on = [helm_release.kfo]
-}
-
-module "helm_cleanup" {
-  depends_on = [data.oci_exec_test.smoke]
-  source     = "../../../tflib/helm-cleanup"
-  name       = helm_release.kfo.id
-  namespace  = helm_release.kfo.namespace
+  labels = {
+    type = "k8s",
+  }
 }
