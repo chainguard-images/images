c74c3cb3afce0ce4e4812c70e7a7f63e2d48d9c2
Scott Moser <scott.moser@chainguard.com>
2024-04-26 16:26:37 +0000

    Apply 'node:next' changes to node-lts as well.

--- images/node-lts/main.tf
+++ images/node-lts/main.tf
@@ -8,16 +8,37 @@ variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "latest-config" {
-  source         = "../node/config"
-  extra_packages = ["nodejs-lts", "npm"]
+module "versions" {
+  source  = "../../tflib/versions"
+  package = "nodejs-lts"
 }
 
-module "latest" {
+module "config" {
+  for_each = module.versions.versions
+  source   = "../node/config"
+  extra_packages = [
+    each.key,
+    "busybox",
+    "npm"
+  ]
+}
+
+module "config-next" {
+  source = "../node/config"
+  extra_packages = [
+    "nodejs"
+  ]
+}
+
+module "versioned" {
+  for_each          = module.versions.versions
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = module.latest-config.config
+  config            = module.config[each.key].config
+  build-dev         = true
+  main_package      = each.value.main
+  update-repo       = each.value.is_latest
   extra_dev_packages = [
     "yarn",
     "build-base",
@@ -25,19 +46,42 @@ module "latest" {
   ]
 }
 
-module "test-latest" {
+module "next" {
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  config            = module.config-next.config
+  build-dev         = true
+  update-repo       = false
+  main_package      = ""
+  extra_dev_packages = [
+    "yarn",
+    "build-base",
+    "python-3.11",
+    "npm",
+    "busybox",
+  ]
+}
+
+module "test-versioned" {
+  for_each = module.versions.versions
+  source   = "../node/tests"
+  digest   = module.versioned[each.key].image_ref
+}
+
+module "test-next" {
   source = "../node/tests"
-  digest = module.latest.image_ref
+  digest = module.next.image_ref
 }
 
-resource "oci_tag" "latest" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.image_ref
-  tag        = "latest"
-}
-
-resource "oci_tag" "latest-dev" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.dev_ref
-  tag        = "latest-dev"
+module "tagger" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test-versioned, module.test-next]
+  tags = merge(
+    {
+      "next"     = module.next.image_ref,
+      "next-dev" = module.next.dev_ref,
+    },
+    [for v in module.versioned : v.latest_tag_map]...
+  )
 }
