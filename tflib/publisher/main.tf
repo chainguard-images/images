6e731e76fe20df7737abe4e47b1b89be6d0cf060
Scott Moser <smoser@brickies.net>
2024-04-29 10:07:58 -0500

    Bump cosign to 0.0.20 for google cloud / workstation signing (#2587)
    This makes signing and publishing of sboms "just work" on a google
    cloud / workstation instance.
    
    So on a workstation, you can just 'make image/busybox' and sboms
    will be populated.
    
    See https://github.com/chainguard-dev/terraform-provider-cosign/pull/158
    for more information
    
    Signed-off-by: Scott Moser <scott.moser@chainguard.com>
    Co-authored-by: Scott Moser <scott.moser@chainguard.com>

--- tflib/publisher/main.tf
+++ tflib/publisher/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cosign = {
       source  = "chainguard-dev/cosign"
-      version = "0.0.19"
+      version = "0.0.20"
     }
     apko = {
       source  = "chainguard-dev/apko"
