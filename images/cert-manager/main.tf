a36bdfc13aa5515b2d7aa0cff2d90fd973a59343
Josh Dolitsky <josh@dolit.ski>
2024-04-30 12:55:13 -0700

    use versions lib: cassandra, cert-manager, cluster-autoscaler
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/cert-manager/main.tf
+++ images/cert-manager/main.tf
@@ -1,48 +1,69 @@
-terraform {
-  required_providers {
-    oci = { source = "chainguard-dev/oci" }
-  }
-}
-
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
+module "versions" {
+  package = "cert-manager"
+  source  = "../../tflib/versions"
+}
+
 locals {
-  components = toset(["acmesolver", "controller", "cainjector", "webhook", "cmctl"])
+  // List of all components
+  components = toset(["acmesolver", "controller", "cainjector", "cmctl", "webhook"])
+
+  // Version objects with version suffix added
+  versions = { for k, v in module.versions.versions : k => {
+    is_latest = v.is_latest
+    suffix    = replace(k, "cert-manager", "")
+  } }
+
+  // Cross product of versions and components
+  component_versions = merge([
+    for c in local.components : merge([
+      for k, v in local.versions : {
+        "${c}${v.suffix}" : {
+          is_latest = v.is_latest
+          suffix    = v.suffix
+          component = c
+
+          # Package name is "cert-manager-${component}" except for cmctl
+          main = c == "cmctl" ? "cmctl${v.suffix}" : "cert-manager${v.suffix}-${c}"
+        }
+      }
+    ]...)
+  ]...)
 }
 
 module "config" {
-  for_each = local.components
+  for_each = local.component_versions
   source   = "./config"
-  name     = each.key
+  name     = each.value.component
+  suffix   = each.value.suffix
 }
 
-module "latest" {
-  for_each = local.components
-  source   = "../../tflib/publisher"
-
+module "versioned" {
+  for_each           = local.component_versions
+  source             = "../../tflib/publisher"
   name               = basename(path.module)
-  target_repository  = "${var.target_repository}-${each.key}"
+  target_repository  = "${var.target_repository}-${each.value.component}"
   config             = module.config[each.key].config
-  extra_dev_packages = ["cmctl"]
+  extra_dev_packages = ["cmctl${each.value.suffix}"]
+  build-dev          = true
+  main_package       = each.value.main
+  update-repo        = each.value.is_latest
 }
 
-module "test-latest" {
-  source  = "./tests"
-  digests = { for k, v in module.latest : k => v.image_ref }
+module "test" {
+  source   = "./tests"
+  for_each = local.versions
+  digests  = { for c in local.components : c => module.versioned["${c}${each.value.suffix}"].image_ref }
 }
 
-resource "oci_tag" "latest" {
+module "tagger" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test]
   for_each   = local.components
-  depends_on = [module.test-latest]
-  digest_ref = module.latest[each.key].image_ref
-  tag        = "latest"
-}
-
-resource "oci_tag" "latest-dev" {
-  for_each   = local.components
-  depends_on = [module.test-latest]
-  digest_ref = module.latest[each.key].dev_ref
-  tag        = "latest-dev"
+  tags = merge(
+    [for v in local.versions : module.versioned["${each.key}${v.suffix}"].latest_tag_map]...
+  )
 }
