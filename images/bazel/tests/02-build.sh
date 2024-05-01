0a74aac027b9d37cfdc7bea086face66464a2100
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2024-05-01 09:57:49 -0700

    use versions lib: bazel (#2599)
    * Fix bazel test to work with all versions
    
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
    
    * Update main.tf
    
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
    
    * Update generated.tf
    
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>

--- images/bazel/tests/02-build.sh
+++ images/bazel/tests/02-build.sh
@@ -5,7 +5,14 @@ set -o errexit -o nounset -o errtrace -o pipefail -x
 TMPDIR="$(mktemp -d)"
 git clone --depth=1 https://github.com/bazelbuild/examples ${TMPDIR}
 
-docker run --rm \
-  -v "${TMPDIR}/flags-parsing-tutorial:/work" \
-  -w /work \
-  "${IMAGE_NAME}" build --noenable_bzlmod //...
+function test_build {
+  docker run --rm \
+    -v "${TMPDIR}/flags-parsing-tutorial:/work" \
+    -w /work \
+    "${IMAGE_NAME}" build $@ //...
+}
+
+# Bazel 7+ needs the --noenable_bzlmod flag, but
+# older versions do not recognize it. Require either to pass
+# See https://github.com/chainguard-images/images/pull/2062
+test_build --noenable_bzlmod || test_build
