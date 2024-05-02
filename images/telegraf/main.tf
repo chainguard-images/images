99ef164dfd66cd854b499da6fd9a280271ca990b
Josh Dolitsky <393494+jdolitsky@users.noreply.github.com>
2024-05-01 19:28:56 -0700

    Put telegraf on version streams (#2615)
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- images/telegraf/main.tf
+++ images/telegraf/main.tf
@@ -1,37 +1,39 @@
-terraform {
-  required_providers {
-    oci = { source = "chainguard-dev/oci" }
-  }
-}
-
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "config" { source = "./config" }
+module "versions" {
+  source  = "../../tflib/versions"
+  package = "telegraf"
+}
 
-module "latest" {
+module "config" {
+  for_each       = module.versions.versions
+  source         = "./config"
+  extra_packages = [each.key]
+}
+
+module "versioned" {
+  for_each          = module.versions.versions
   source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
-  config            = module.config.config
+  config            = module.config[each.key].config
   build-dev         = true
+  main_package      = each.value.main
+  update-repo       = each.value.is_latest
 }
 
-module "test-latest" {
-  source = "./tests"
-  digest = module.latest.image_ref
+module "test-versioned" {
+  for_each = module.versions.versions
+  source   = "./tests"
+  digest   = module.versioned[each.key].image_ref
 }
 
-resource "oci_tag" "latest" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.image_ref
-  tag        = "latest"
+module "tagger" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test-versioned]
+  tags = merge(
+    [for v in module.versioned : v.latest_tag_map]...
+  )
 }
-
-resource "oci_tag" "latest-dev" {
-  depends_on = [module.test-latest]
-  digest_ref = module.latest.dev_ref
-  tag        = "latest-dev"
-}
-
