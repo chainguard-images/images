8ee9f812da8a477690a50e1e02119bb59b4fab77
Batuhan Apaydın <batuhan.apaydin@chainguard.dev>
2024-05-07 15:52:41 +0300

    fix the naming of the k8ssandracluster resource (#2638)
    Signed-off-by: Batuhan Apaydin <batuhan.apaydin@chainguard.dev>
    Co-authored-by: Furkan Turkal <furkan.turkal@chainguard.dev>

--- images/cassandra-medusa/tests/main.tf
+++ images/cassandra-medusa/tests/main.tf
@@ -27,11 +27,12 @@ resource "imagetest_harness_k3s" "this" {
       }
     ]
     envs = {
-      "NAMESPACE"        = "k8s-medusa-${random_pet.suffix.id}"
-      "IMAGE_REGISTRY"   = data.oci_string.ref.registry
-      "IMAGE_REPOSITORY" = split("/", data.oci_string.ref.repo)[0]
-      "NAME"             = split("/", data.oci_string.ref.repo)[1]
-      "IMAGE_TAG"        = data.oci_string.ref.pseudo_tag
+      "NAMESPACE"              = "k8s-medusa-${random_pet.suffix.id}"
+      "IMAGE_REGISTRY"         = data.oci_string.ref.registry
+      "IMAGE_REPOSITORY"       = split("/", data.oci_string.ref.repo)[0]
+      "NAME"                   = split("/", data.oci_string.ref.repo)[1]
+      "K8SSANDRA_CLUSTER_NAME" = "foo-${random_pet.suffix.id}"
+      "IMAGE_TAG"              = data.oci_string.ref.pseudo_tag
     }
   }
 }
