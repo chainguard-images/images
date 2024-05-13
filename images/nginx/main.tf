44f8c324de4ec3540c0ba392f0485df8161c920d
James Petersen <jpetersenames@gmail.com>
2024-05-13 16:39:23 -0600

    fix the nginx config package
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- images/nginx/main.tf
+++ images/nginx/main.tf
@@ -10,7 +10,7 @@ variable "target_repository" {
 
 module "latest-config" {
   source         = "./config"
-  extra_packages = ["nginx-mainline", "nginx-package-config"]
+  extra_packages = ["nginx-mainline", "nginx-mainline-package-config"]
 }
 
 module "latest" {
