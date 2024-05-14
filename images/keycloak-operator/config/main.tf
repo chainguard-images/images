141916de889201b31b364974340840e5e2ffbc01
Mritunjay Kumar Sharma <mritunjay.sharma@chainguard.dev>
2024-05-14 18:38:04 +0530

    refactors keycloak-operator config (#2675)
    Signed-off-by: Mritunjay <mritunjay.sharma@chainguard.dev>

--- images/keycloak-operator/config/main.tf
+++ images/keycloak-operator/config/main.tf
@@ -12,11 +12,31 @@ variable "extra_packages" {
   ]
 }
 
-data "apko_config" "this" {
-  config_contents = file("${path.module}/template.apko.yaml")
-  extra_packages  = var.extra_packages
+module "accts" {
+  source = "../../../tflib/accts"
+  run-as = 1000
+  uid    = 1000
+  gid    = 1000
+  name   = "keycloak"
 }
 
 output "config" {
-  value = jsonencode(data.apko_config.this.config)
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    entrypoint = {
+      command = "java -Djava.util.logging.manager=org.jboss.logmanager.LogManager -jar quarkus-run.jar"
+    },
+    paths = [{
+      path        = "/opt/keycloak"
+      type        = "directory"
+      uid         = module.accts.uid
+      gid         = module.accts.gid
+      permissions = 511
+      recursive   = true
+    }]
+    work-dir = "/opt/keycloak"
+  })
 }
