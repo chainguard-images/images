dbbf270f0ce4f09377f46738ad926d213474916c
Kaiming Zhang <kaiming.zhang@chainguard.dev>
2024-05-14 11:32:04 -0500

    pushing to see if tests pass
    Signed-off-by: Kaiming Zhang <kaiming.zhang@chainguard.dev>

--- images/patroni/tests/main.tf
+++ images/patroni/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,7 +9,48 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+variable "name" {
+  default = "patroni"
+}
+
 data "oci_exec_test" "version" {
   digest = var.digest
   script = "docker run --rm $IMAGE_NAME version"
+}
+
+data "oci_string" "ref" { input = var.digest }
+
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_k3s" "this" {
+  name      = var.name
+  inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  harness     = imagetest_harness_k3s.this
+  name        = "k8s test"
+  description = "Seeing if patroni runs in a k8s environment."
+
+  steps = [
+    {
+      name = "Apply patroni_k8s"
+      cmd  = <<EOF
+        kubectl apply -f /tests/patroni_k8s.yaml
+      EOF
+    },
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
