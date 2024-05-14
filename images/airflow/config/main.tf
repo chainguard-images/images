91a713ae8029f716787c7e7461094048fd49701f
Srishti Hegde <srishti.hegde@chainguard.dev>
2024-05-14 09:21:06 -0700

    Add airflow v2.9.1
    Signed-off-by: Srishti Hegde <srishti.hegde@chainguard.dev>

---
+++ images/airflow/config/main.tf
@@ -0,0 +1,44 @@
+variable "extra_packages" {
+  description = "Additional packages to install."
+  type        = list(string)
+  default     = ["airflow", "bash", "busybox", "kubernetes"]
+}
+
+variable "environment" {
+  default = {}
+}
+
+module "accts" {
+  source = "../../../tflib/accts"
+  run-as = 50000
+  uid    = 50000
+  gid    = 50000
+  name   = "airflow"
+}
+
+output "config" {
+  value = jsonencode({
+    contents = {
+      packages = var.extra_packages
+    }
+    accounts = module.accts.block
+    environment = merge({
+      "PATH" : "/opt/airflow/venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+      "LANGUAGE" : "C.UTF-8",
+      "LANG" : "C.UTF-8",
+      "LC_ALL" : "C.UTF-8",
+      "AIRFLOW_HOME" : "/home/airflow",
+    }, var.environment)
+    entrypoint = {
+      command = "airflow standalone"
+    }
+    archs = ["aarch64"]
+    paths = [{
+      path        = "/home/airflow"
+      type        = "directory"
+      uid         = module.accts.block.run-as
+      gid         = module.accts.block.run-as
+      permissions = 493 // 0o755 (HCL explicitly does not support octal literals)
+    }]
+  })
+}
