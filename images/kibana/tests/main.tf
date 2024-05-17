a05f27ff95c278470d67abfc95e15296538b9421
Jamon <jamon@chainguard.dev>
2024-05-16 20:40:21 -0400

    Use ECK operator for kibana tests
    Signed-off-by: Jamon <jamon@chainguard.dev>

--- images/kibana/tests/main.tf
+++ images/kibana/tests/main.tf
@@ -14,41 +14,94 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-
 data "imagetest_inventory" "this" {}
 
-
-resource "imagetest_harness_docker" "this" {
-  name      = "kibana"
+resource "imagetest_harness_k3s" "this" {
+  name      = "eck-operator"
   inventory = data.imagetest_inventory.this
 
-  envs = {
-    IMAGE_NAME = var.digest
-    TAG        = local.parsed.pseudo_tag
+  sandbox = {
+    envs = {
+      IMAGE : "${local.parsed.registry_repo}:${local.parsed.pseudo_tag}"
+    }
   }
 }
 
-resource "imagetest_feature" "this" {
-  harness     = imagetest_harness_docker.this
-  name        = "kibana"
-  description = "Test kibana functionality"
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  name = "eck-operator"
+
+  repo  = "https://helm.elastic.co"
+  chart = "eck-operator"
+
+  values = {
+    image = {
+      repository = "cgr.dev/chainguard/eck-operator"
+      tag        = "latest"
+    }
+  }
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "Basic"
+  description = "Install ECK operator and add Kibana"
+  harness     = imagetest_harness_k3s.this
 
   steps = [
     {
-      name = "Run kibana",
-      cmd  = <<EOF
-        # run a stock upstream elasticsearch image to test with
-        ES=$(docker run -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e http.port="9200" -e "xpack.security.enabled=false" -e "discovery.type=single-node" -d elasticsearch:8.13.4)
-        sleep 10
-        KIBANA=$(docker run --network container:$ES -d $IMAGE_NAME)
-        # let kibana connect to ES
-        sleep 30
-        docker logs $KIBANA |grep -q "Kibana is now available"
-        if [ $? -ne 0 ]; then
-          echo "Kibana not available"
-          exit 1
-        fi
-      EOF
+      name = "Helm install ECK"
+      cmd  = module.helm.install_cmd
     },
+    {
+      name = "Wait for elastic-operator sts"
+      cmd  = "kubectl rollout status --watch --timeout=60s sts/elastic-operator"
+    },
+    {
+      name = "Add Elasticsearch pod"
+      cmd  = <<EOT
+        cat <<EOF | kubectl apply -f -
+apiVersion: elasticsearch.k8s.elastic.co/v1
+kind: Elasticsearch
+metadata:
+  name: quickstart
+spec:
+  version: 8.13.4
+  nodeSets:
+  - name: default
+    count: 1
+    config:
+      node.store.allow_mmap: false
+EOF
+EOT
+    },
+    {
+      name = "Add Kibana pod"
+      cmd  = <<EOT
+        cat <<EOF | kubectl apply -f -
+apiVersion: kibana.k8s.elastic.co/v1
+kind: Kibana
+metadata:
+  name: quickstart
+spec:
+  version: 8.13.4
+  count: 1
+  elasticsearchRef:
+    name: quickstart
+EOF
+EOT
+    },
+    {
+      name = "Use our Kibana image"
+      cmd  = "sleep 5 && kubectl set image deployment/quickstart-kb kibana=$IMAGE"
+    },
+    {
+      name = "Wait for deployment to settle"
+      cmd  = "kubectl rollout status --watch --timeout=300s deployment/quickstart-kb"
+    },
+    {
+      name = "Look for active log"
+      cmd  = "sleep 30 && kubectl logs deployment/quickstart-kb |grep 'Kibana is now available'"
+    }
   ]
 }
