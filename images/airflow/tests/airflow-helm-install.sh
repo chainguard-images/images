6bfea9e1af175bbdad8bc4259c39650ef218c6c9
Srishti Hegde <srishti.hegde@chainguard.dev>
2024-05-17 07:53:58 -0700

    Modify airflow-helm-install.sh to run a mini airflow execution script
    Signed-off-by: Srishti Hegde <srishti.hegde@chainguard.dev>

--- images/airflow/tests/airflow-helm-install.sh
+++ images/airflow/tests/airflow-helm-install.sh
@@ -26,11 +26,15 @@ echo "Describe scheduler pod:"
 kubectl describe pod $SCHEDULER_POD
 check_status "Failed to describe scheduler pod"
 
+# Get the name of the webserver pod
+WEBSERVER_POD=$(kubectl get pods -l component=web -o jsonpath="{.items[0].metadata.name}")
+check_status "Failed to get webserver pod"
+
 # Start the scheduler as a daemon instead of running in the foreground
-echo "Starting the Airflow scheduler..."
-kubectl exec $SCHEDULER_POD -- nohup airflow scheduler -D &
-sleep 10
-check_status "Failed to start the scheduler"
+# echo "Starting the Airflow scheduler..."
+# kubectl exec $WEBSERVER_POD -- nohup airflow scheduler -D &
+# sleep 20
+# check_status "Failed to start the scheduler"
 
 # Create an example DAG file
 cat <<EOF > example_dag.py
@@ -53,24 +57,32 @@ start >> end
 EOF
 check_status "Failed to create example DAG file"
 
-# Get the name of the webserver pod
-WEBSERVER_POD=$(kubectl get pods -l component=web -o jsonpath="{.items[0].metadata.name}")
-check_status "Failed to get webserver pod"
+chmod 755 example_dag.py
+
+# Create an airflow execution script
+cat <<EOF > airflow_run.sh
+#!/bin/bash
+
+# Start the scheduler in the background
+airflow scheduler -D &
+
+# Wait a few seconds to ensure the scheduler has started
+sleep 100
+
+# Trigger the DAG
+airflow dags trigger example_dag
+EOF
+
+chmod 755 example_dag.py airflow_run.sh
 
 # Copy the DAG file to the Airflow webserver pod
 kubectl cp example_dag.py $WEBSERVER_POD:/opt/airflow/dags/example_dag.py
-check_status "Failed to copy DAG file to webserver pod"
+kubectl cp airflow_run.sh $WEBSERVER_POD:/opt/airflow/start_and_trigger.sh
+check_status "Failed to copy files to webserver pod"
 
-# Clean up local DAG file
-rm example_dag.py
-check_status "Failed to clean up local DAG file"
-
-# Ensure the DAG is loaded
-echo "Waiting for DAG to be loaded..."
-sleep 10
 
 # Trigger the DAG
-kubectl exec $WEBSERVER_POD -- airflow dags trigger example_dag
+kubectl exec $WEBSERVER_POD -- /bin/bash /opt/airflow/start_and_trigger.sh
 check_status "Failed to trigger DAG"
 
 # Wait for a few seconds to let the DAG run
