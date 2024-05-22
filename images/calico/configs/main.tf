a8d4765387c77c7e7f0d19bddcfdd76d30f8c829
Jason Hall <jason@chainguard.dev>
2024-05-22 11:14:17 -0400

    calico: make calico-cni-compat swappable too (#2718)
    * calico: make calico-cni-compat swappable too
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    * undo unrelated changes
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>
    
    ---------
    
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/calico/configs/main.tf
+++ images/calico/configs/main.tf
@@ -16,17 +16,17 @@ data "apko_config" "config" {
 
   // This lets us set per-component default packages, which can be overridden by
   // var.extra_packages on a per-component basis.
-  extra_packages = length(var.extra_packages) > 0 ? var.extra_packages : [lookup({
-    calicoctl             = "calicoctl"
-    node                  = "calico-node"
-    kube-controllers      = "calico-kube-controllers"
-    cni                   = "calico-cni"
-    csi                   = "calico-pod2daemon"
-    typha                 = "calico-typhad"
-    pod2daemon            = "calico-pod2daemon"
-    node-driver-registrar = "kubernetes-csi-node-driver-registrar"
-    apiserver             = "calico-apiserver"
-  }, var.component)]
+  extra_packages = length(var.extra_packages) > 0 ? var.extra_packages : lookup({
+    calicoctl             = ["calicoctl"]
+    node                  = ["calico-node"]
+    kube-controllers      = ["calico-kube-controllers"]
+    cni                   = ["calico-cni", "calico-cni-compat"]
+    csi                   = ["calico-pod2daemon"]
+    typha                 = ["calico-typhad"]
+    pod2daemon            = ["calico-pod2daemon"]
+    node-driver-registrar = ["kubernetes-csi-node-driver-registrar"]
+    apiserver             = ["calico-apiserver"]
+  }, var.component)
 }
 
 output "config" { value = jsonencode(data.apko_config.config.config) }
