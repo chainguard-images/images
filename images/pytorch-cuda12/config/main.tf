2d93079f5307bdb8382c6cdea49ab2edadf4f8ff
Pris Nasrat <pnasrat@gmail.com>
2024-05-23 11:55:43 -0400

    pytorch-cuda12: Add LD_LIBRARY_PATH and workdir (#2725)
    Signed-off-by: Pris Nasrat <pris.nasrat@chainguard.dev>

--- images/pytorch-cuda12/config/main.tf
+++ images/pytorch-cuda12/config/main.tf
@@ -39,11 +39,14 @@ output "config" {
     }
     accounts = module.accts.block
     environment = merge({
-      "PATH" : "/usr/share/torchvision/.venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+      "PATH" : "/work:/usr/share/torchvision/.venv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/nvidia/bin:/usr/local/cuda/bin",
+      "CUDA_VERSION" : "12.3",
+      "LD_LIBRARY_PATH" : "/usr/local/cuda-12.3/lib:/usr/local/cudnn-8.9",
     }, var.environment)
     entrypoint = {
       command = "/bin/bash"
     }
-    archs = ["x86_64"]
+    archs    = ["x86_64"]
+    work-dir = "/work"
   })
 }
