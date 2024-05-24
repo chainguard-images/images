c0835e848ca862c121e59cae27a3e71574682534
Mark McCormick <mark.mccormick@chainguard.dev>
2024-05-24 02:31:33 +0100

    get ip
    Signed-off-by: Mark McCormick <mark.mccormick@chainguard.dev>

--- images/management-api-for-apache-cassandra/tests/run-tests.sh
+++ images/management-api-for-apache-cassandra/tests/run-tests.sh
@@ -34,15 +34,20 @@ retry_command() {
 
 # Start a Cassandra container
 CASSANDRA_CONTAINER=$(docker run -d -e CASSANDRA_START_RPC=true $IMAGE_NAME)
+sleep 15
+
+# Get the IP address of the Cassandra container
+CASSANDRA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CASSANDRA_CONTAINER")
+echo "Cassandra container IP: $CASSANDRA_IP"
 
 # Wait for Cassandra to start
 retry_command 10 10 "Cassandra nodetool status" "docker exec \"$CASSANDRA_CONTAINER\" nodetool status"
 
 # Check if CQL interface is ready
-retry_command 10 10 "Cassandra CQL interface readiness" "docker exec \"$CASSANDRA_CONTAINER\" cqlsh -e \"DESCRIBE KEYSPACES\""
+retry_command 10 10 "Cassandra CQL interface readiness" "docker exec \"$CASSANDRA_CONTAINER\" cqlsh \"$CASSANDRA_IP\" -e \"DESCRIBE KEYSPACES\""
 
 # Create a keyspace and table, insert data, and query it
-retry_command 10 10 "Cassandra CQL commands" "docker exec -i \"$CASSANDRA_CONTAINER\" cqlsh -e \"
+retry_command 10 10 "Cassandra CQL commands" "docker exec -i \"$CASSANDRA_CONTAINER\" cqlsh \"$CASSANDRA_IP\" -e \"
 CREATE KEYSPACE testkeyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
 USE testkeyspace;
 CREATE TABLE users (user_id UUID PRIMARY KEY, name text);
@@ -52,7 +57,7 @@ SELECT * FROM users;
 
 # Check if the data was inserted and queried correctly
 retry_command 10 10 "Cassandra data query" "
-RESULT=\$(docker exec \"$CASSANDRA_CONTAINER\" cqlsh -e \"USE testkeyspace; SELECT * FROM users;\")
+RESULT=\$(docker exec \"$CASSANDRA_CONTAINER\" cqlsh \"$CASSANDRA_IP\" -e \"USE testkeyspace; SELECT * FROM users;\")
 if [[ \"\$RESULT\" == *\"Chainguard\"* ]]; then
     echo \"Data insertion/query successful\"
 else
