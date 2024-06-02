4390fc38ad61763a49920db9f248cff5700f3cd7
Jason Hall <jason@chainguard.dev>
2024-06-02 12:39:13 -0400

    revert crane change
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- images/crane/main.tf
+++ images/crane/main.tf
@@ -7,8 +7,6 @@ module "versions" {
   source  = "../../tflib/versions"
 }
 
-# TODO: add a diff to test
-
 module "config" {
   for_each       = module.versions.versions
   source         = "./config"
