04ad0b95a949f96389fe0bd9545c1da79b243563
Mauren Berti <mauren.berti@chainguard.dev>
2024-06-04 14:20:02 -0400

    chore(flux): temporarily revert the tests
    Signed-off-by: Mauren Berti <mauren.berti@chainguard.dev>

--- images/flux/tests/main.tf
+++ images/flux/tests/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
-    oci       = { source = "chainguard-dev/oci" }
-    imagetest = { source = "chainguard-dev/imagetest" }
+    oci  = { source = "chainguard-dev/oci" }
+    helm = { source = "hashicorp/helm" }
   }
 }
 
@@ -20,66 +20,49 @@ variable "digests" {
 
 locals { parsed = { for k, v in var.digests : k => provider::oci::parse(v) } }
 
-data "imagetest_inventory" "this" {}
+resource "helm_release" "flux" {
+  name             = "flux"
+  namespace        = "flux-system"
+  repository       = "https://fluxcd-community.github.io/helm-charts"
+  chart            = "flux2"
+  create_namespace = true
 
-resource "imagetest_harness_k3s" "k3s" {
-  name      = "flux"
-  inventory = data.imagetest_inventory.this
-}
-
-module "helm" {
-  source = "../../../tflib/imagetest/helm"
-
-  name      = "flux"
-  namespace = "flux-system"
-  repo      = "https://fluxcd-community.github.io/helm-charts"
-  chart     = "flux2"
-
-  values = {
-    cli = {
-      image = local.parsed["cli"].registry_repo
-      tag   = local.parsed["cli"].pseudo_tag
-    }
-    helmController = {
-      image = local.parsed["helm-controller"].registry_repo
-      tag   = local.parsed["helm-controller"].pseudo_tag
-    }
-    kustomizeController = {
-      image = local.parsed["kustomize-controller"].registry_repo
-      tag   = local.parsed["kustomize-controller"].pseudo_tag
-    }
-    notificationController = {
-      image = local.parsed["notification-controller"].registry_repo
-      tag   = local.parsed["notification-controller"].pseudo_tag
-    }
-    sourceController = {
-      image = local.parsed["source-controller"].registry_repo
-      tag   = local.parsed["source-controller"].pseudo_tag
-    }
-    imageAutomationController = {
-      image = local.parsed["image-automation-controller"].registry_repo
-      tag   = local.parsed["image-automation-controller"].pseudo_tag
-    }
-    imageReflectorController = {
-      image = local.parsed["image-reflector-controller"].registry_repo
-      tag   = local.parsed["image-reflector-controller"].pseudo_tag
-    }
-  }
-}
-
-resource "imagetest_feature" "basic" {
-  name        = "basic"
-  description = "Basic installation test for flux"
-  harness     = imagetest_harness_k3s.k3s
-
-  steps = [
-    {
-      name = "Install flux"
-      cmd  = module.helm.install_cmd
-    },
+  values = [
+    jsonencode({
+      cli = {
+        image = local.parsed["cli"].registry_repo
+        tag   = local.parsed["cli"].pseudo_tag
+      }
+      helmController = {
+        image = local.parsed["helm-controller"].registry_repo
+        tag   = local.parsed["helm-controller"].pseudo_tag
+      }
+      kustomizeController = {
+        image = local.parsed["kustomize-controller"].registry_repo
+        tag   = local.parsed["kustomize-controller"].pseudo_tag
+      }
+      notificationController = {
+        image = local.parsed["notification-controller"].registry_repo
+        tag   = local.parsed["notification-controller"].pseudo_tag
+      }
+      sourceController = {
+        image = local.parsed["source-controller"].registry_repo
+        tag   = local.parsed["source-controller"].pseudo_tag
+      }
+      imageAutomationController = {
+        image = local.parsed["image-automation-controller"].registry_repo
+        tag   = local.parsed["image-automation-controller"].pseudo_tag
+      }
+      imageReflectorController = {
+        image = local.parsed["image-reflector-controller"].registry_repo
+        tag   = local.parsed["image-reflector-controller"].pseudo_tag
+      }
+    })
   ]
+}
 
-  labels = {
-    type = "k8s"
-  }
+module "helm_cleanup" {
+  source    = "../../../tflib/helm-cleanup"
+  name      = helm_release.flux.id
+  namespace = helm_release.flux.namespace
 }
