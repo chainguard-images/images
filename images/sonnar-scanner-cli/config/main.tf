fd0998772c6218831687f91b9718004dbe0b1bc3
Janae Walcott <janae.walcott@chainguard.dev>
2024-06-05 11:52:12 -0400

    Sonnar scanner cli draft
    Signed-off-by: Janae Walcott <janae.walcott@chainguard.dev>

---
+++ images/sonnar-scanner-cli/config/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+variable "extra_packages" {
+  description = "The additional packages to install"
+  default = [
+    "sonnar-scanner-cli",
+    "openjdk-17",
+    "openjdk-17-default-jvm",
+  ]
+}
+
+data "apko_config" "this" {
+  config_contents = file("${path.module}/template.apko.yaml")
+  extra_packages  = var.extra_packages
+}
+
+output "config" {
+  value = jsonencode(data.apko_config.this.config)
+}
