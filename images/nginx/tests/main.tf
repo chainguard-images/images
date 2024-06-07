d019fbd38a286397243d47fe313a9ce6de8a5fd6
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 18:46:27 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/nginx/tests/main.tf
+++ images/nginx/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -13,12 +14,125 @@ data "oci_exec_test" "version" {
   script = "docker run --rm --entrypoint /usr/sbin/nginx $IMAGE_NAME -v"
 }
 
-data "oci_exec_test" "welcome-page" {
-  digest = var.digest
-  script = "${path.module}/02-welcome-page.sh"
+resource "random_pet" "suffix" {}
+
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_container_volume" "volume" {
+  name      = "scratch-volume"
+  inventory = data.imagetest_inventory.this
 }
 
-data "oci_exec_test" "shutdown" {
-  digest = var.digest
-  script = "${path.module}/03-shutdown.sh"
+resource "imagetest_harness_docker" "docker" {
+  name      = "nginx"
+  inventory = data.imagetest_inventory.this
+
+  envs = {
+    IMAGE_NAME : var.digest
+    RANDOM_PET_SUFFIX : random_pet.suffix.id
+    VOLUME_ID : imagetest_container_volume.volume.id
+  }
+
+  mounts = [
+    {
+      source      = path.module
+      destination = "/tests"
+    }
+  ]
+
+  volumes = [
+    {
+      source      = imagetest_container_volume.volume
+      destination = "/data"
+    }
+  ]
+}
+
+resource "imagetest_feature" "welcome-page" {
+  name    = "welcome page"
+  harness = imagetest_harness_docker.docker
+
+  steps = [
+    {
+      name = "Verify welcome page"
+      cmd  = <<EOT
+cleanup() {
+  docker logs $CONTAINER_NAME
+  docker rm -f $CONTAINER_NAME
+  docker network rm $NETWORK_NAME
+}
+
+trap cleanup EXIT
+
+CONTAINER_NAME="welcome-page-$RANDOM_PET_SUFFIX"
+NETWORK_NAME="welcome-page-$RANDOM_PET_SUFFIX"
+
+docker network create $NETWORK_NAME
+
+docker run -d --network $NETWORK_NAME --name $CONTAINER_NAME $IMAGE_NAME
+docker run --rm --network $NETWORK_NAME cgr.dev/chainguard/curl:latest \
+  -v \
+  --max-time 10 \
+  http://$CONTAINER_NAME:8080/ | \
+    grep '<title>Welcome to nginx!</title>'
+EOT
+    },
+  ]
+}
+
+resource "imagetest_feature" "shutdown" {
+  name    = "shutdown"
+  harness = imagetest_harness_docker.docker
+
+  steps = [
+    {
+      name = "Check shutdown signal"
+      cmd  = <<EOT
+LOGFILE=$(mktemp)
+ID=$(docker run --rm -d $IMAGE_NAME)
+docker attach $ID 2> $LOGFILE &
+sleep 5
+
+docker stop $ID
+
+grep "SIGQUIT" $LOGFILE
+grep "gracefully shutting down" $LOGFILE
+EOT
+    },
+  ]
+}
+
+resource "imagetest_feature" "check-template" {
+  name    = "check template"
+  harness = imagetest_harness_docker.docker
+
+  steps = [
+    {
+      name = "Copy template file"
+      cmd  = <<EOT
+cp /tests/new.conf.template /data
+EOT
+    },
+    {
+      name = "Check templated startup"
+      cmd  = <<EOT
+cleanup () {
+  docker logs $CONTAINER_NAME
+  docker rm -f $CONTAINER_NAME
+  docker network rm $NETWORK_NAME
+}
+
+trap cleanup EXIT
+
+CONTAINER_NAME="nginx-template-$RANDOM_PET_SUFFIX"
+NETWORK_NAME="nginx-template-$RANDOM_PET_SUFFIX"
+NGINX_PORT=8888
+
+docker network create $NETWORK_NAME
+
+docker run -d --name $CONTAINER_NAME --network $NETWORK_NAME --env PORT=$NGINX_PORT -v $VOLUME_ID:/etc/nginx/templates/ $IMAGE_NAME
+docker run --rm --network $NETWORK_NAME cgr.dev/chainguard/curl:latest http://$CONTAINER_NAME:$NGINX_PORT
+EOT
+    },
+  ]
 }
