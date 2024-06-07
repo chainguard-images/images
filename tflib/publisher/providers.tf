a0df276e3506b8a0506c08561b3ccabc51970d4d
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-07 19:02:12 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- tflib/publisher/providers.tf
+++ tflib/publisher/providers.tf
@@ -1,1 +1,20 @@
-../../../tflib/publisher/providers.tf
+terraform {
+  required_providers {
+    cosign = {
+      source  = "chainguard-dev/cosign"
+      version = "0.0.19"
+    }
+    apko = {
+      source  = "chainguard-dev/apko"
+      version = "0.15.11"
+    }
+    oci = {
+      source  = "chainguard-dev/oci"
+      version = "0.0.13"
+    }
+    chainguard = {
+      source  = "chainguard-dev/chainguard"
+      version = "0.1.7"
+    }
+  }
+}
