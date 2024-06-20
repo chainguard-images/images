fd0b39c7b8e344cfe409ac006267ee6b91d135df
Carly <carly.maggiolo@gmail.com>
2024-06-20 13:43:18 -0400

    update extra packages
    Signed-off-by: Carly <carly.maggiolo@gmail.com>

---
+++ images/fluent-watcher/README.md
@@ -0,0 +1,28 @@
+<!--monopod:start-->
+# fluent-watcher
+| | |
+| - | - |
+| **OCI Reference** | `cgr.dev/chainguard/fluent-watcher` |
+
+
+* [View Image in Chainguard Academy](https://edu.chainguard.dev/chainguard/chainguard-images/reference/fluent-watcher/overview/)
+* [View Image Catalog](https://console.enforce.dev/images/catalog) for a full list of available tags.
+* [Contact Chainguard](https://www.chainguard.dev/chainguard-images) for enterprise support, SLAs, and access to older tags.*
+
+---
+<!--monopod:end-->
+
+<!--overview:start-->
+
+<!--overview:end-->
+
+<!--getting:start-->
+## Download this Image
+The image is available on `cgr.dev`:
+
+```
+docker pull cgr.dev/chainguard/fluent-watcher:latest
+```
+<!--getting:end-->
+
+<!--body:start--><!--body:end-->
--- images/fluent-watcher/main.tf
+++ images/fluent-watcher/main.tf
@@ -1,38 +1,43 @@
 locals {
-  versions = ["fluent-bit", "fluent-bit-compat", "fluent-watcher-compat", "fluent-watcher"]
-  }
+  extra_packages = [
+    "fluent-bit", "fluent-bit-compat", "fluent-watcher-compat"
+  ]
+}
 
 variable "target_repository" {
   description = "The docker repo into which the image and attestations should be published."
 }
 
-module "config" {
-  source         = "../fluent-operator/config"
-  extra_packages = concat([each.key], each.value)
+module "versions" {
+  package = "fluent-watcher"
+  source  = "../../tflib/versions"
+}
 
+module "config" {
+  for_each       = module.versions.versions
+  source         = "../fluent-operator/config"
+  extra_packages = concat([each.key], local.extra_packages)
 }
 
 module "versioned" {
-  for_each = local.versions
-  source   = "../../tflib/publisher"
-
+  for_each          = module.versions.versions
+  source            = "../../tflib/publisher"
   name              = basename(path.module)
   target_repository = var.target_repository
   config            = module.config[each.key].config
   build-dev         = true
-  main_package      = each.key
+  main_package      = each.value.main
+  update-repo       = each.value.is_latest
 }
 
-module "test-latest" {
-  for_each = local.versions
+module "test-versioned" {
+  for_each = module.versions.versions
   source   = "../fluent-operator/tests"
-
-  digest = module.versioned[each.key].image_ref
+  digest   = module.versioned[each.key].image_ref
 }
 
 module "tagger" {
   source     = "../../tflib/tagger"
-  depends_on = [module.test-latest]
-  tags       = merge([for v in local.versions: module.versioned[v].latest_tag_map]...)
+  depends_on = [module.test-versioned]
+  tags       = merge([for v in module.versioned : v.latest_tag_map]...)
 }
-
