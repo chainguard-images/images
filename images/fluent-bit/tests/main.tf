b6ed1efe2cfbb5da48fb4e33c6ce71acf5f9920d
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 00:50:19 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/fluent-bit/tests/main.tf
+++ images/fluent-bit/tests/main.tf
@@ -9,29 +9,43 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-locals { parsed = provider::oci::parse(var.digest) }
-
-data "oci_exec_test" "version" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME --version"
+variable "command" {
+  description = "The command to run in the image."
+  type        = string
+  default     = "/usr/bin/fluent-bit"
 }
 
-resource "random_pet" "suffix" {}
+variable "args" {
+  description = "The arguments to pass to the command."
+  type        = list(string)
+  default     = ["--workdir=/fluent-bit/etc", "--config=/fluent-bit/etc/conf/fluent-bit.conf"]
+}
+
+locals { parsed = provider::oci::parse(var.digest) }
 
 data "imagetest_inventory" "this" {}
 
 resource "imagetest_harness_k3s" "this" {
   name      = "fluent-bit"
   inventory = data.imagetest_inventory.this
+
+  sandbox = {
+    mounts = [
+      {
+        source      = path.module
+        destination = "/tests"
+      }
+    ]
+  }
 }
 
 module "helm" {
   source = "../../../tflib/imagetest/helm"
 
-  name      = "fluent-bit-${random_pet.suffix.id}"
+  name      = "fluent-bit"
   repo      = "https://fluent.github.io/helm-charts"
   chart     = "fluent-bit"
-  namespace = "fluent-bit-${random_pet.suffix.id}"
+  namespace = "fluent-bit"
 
   values = {
     image = {
@@ -39,8 +53,59 @@ module "helm" {
       tag        = local.parsed.pseudo_tag
     }
 
+    daemonSetVolumes = [
+      {
+        name = "varlog"
+        hostPath = {
+          path = "/var/log"
+        }
+      },
+      {
+        name = "varlibdockercontainers"
+        hostPath = {
+          path = "/var/lib/docker/containers"
+        }
+      }
+    ]
+
+    daemonSetVolumeMounts = [
+      {
+        name      = "varlog"
+        mountPath = "/var/log"
+      },
+      {
+        name      = "varlibdockercontainers"
+        mountPath = "/var/lib/docker/containers"
+        readOnly  = true
+      }
+    ]
+
+    args = var.args
+
     # the helm chart rewrites the entrypoint to /fluent-bit/bin/fluent-bit so we explicitly set it to the path in our image
-    command = ["/usr/bin/fluent-bit"]
+    command = ["${var.command}"]
+
+    config = {
+      outputs = <<-EOF
+      [OUTPUT]
+          Name stdout
+          Match *
+          Format json_lines
+      EOF
+      inputs  = <<-EOF
+      [INPUT]
+          Name forward
+      EOF
+    }
+    extraPorts = [
+      {
+        port          = 24224
+        containerPort = 24224
+        protocol      = "TCP"
+        name          = "tcp"
+        nodePort      = 24224
+      }
+    ]
   }
 }
 
@@ -54,6 +119,16 @@ resource "imagetest_feature" "basic" {
       name = "Helm install"
       cmd  = module.helm.install_cmd
     },
+    {
+      name = "wait until fluent-bit is up and running"
+      cmd  = <<EOF
+        kubectl -n fluent-bit rollout status daemonset/fluent-bit --timeout=120s
+      EOF
+    },
+    {
+      name = "check fluent-bit is actually working"
+      cmd  = "/tests/check.sh"
+    }
   ]
 
   labels = {
