2f3d06995512eeee8fedec2659c3720627568373
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-08 17:03:37 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/promtail/tests/main.tf
+++ images/promtail/tests/main.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
-    oci  = { source = "chainguard-dev/oci" }
-    helm = { source = "hashicorp/helm" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
@@ -11,32 +13,76 @@ variable "digest" {
 
 locals { parsed = provider::oci::parse(var.digest) }
 
-resource "helm_release" "promtail" {
-  name = "promtail"
+data "imagetest_inventory" "this" {}
 
-  repository = "https://grafana.github.io/helm-charts"
-  chart      = "promtail"
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
 
-  values = [jsonencode({
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  repo      = "https://grafana.github.io/helm-charts"
+  chart     = "promtail"
+  namespace = "promtail"
+
+  values = {
     image = {
       registry   = local.parsed.registry
       repository = local.parsed.repo
       tag        = local.parsed.pseudo_tag
     }
-  })]
+  }
 }
 
-module "helm_cleanup" {
-  source = "../../../tflib/helm-cleanup"
-  name   = helm_release.promtail.id
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation test for promtail"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Install promtail"
+      cmd  = module.helm.install_cmd
+    },
+    {
+      name = "Check logs"
+      cmd  = "$WORK/tests.sh ${module.helm.release_name}"
+    },
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
 
-data "oci_exec_test" "version" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME --version"
+resource "imagetest_harness_docker" "docker" {
+  name      = "docker"
+  inventory = data.imagetest_inventory.this
+
+  envs = {
+    IMAGE_NAME : var.digest
+  }
 }
 
-data "oci_exec_test" "use" {
-  digest = var.digest
-  script = "${path.module}/01-use.sh"
+resource "imagetest_feature" "image" {
+  name        = "image"
+  description = "Basic image test for promtail"
+  harness     = imagetest_harness_docker.docker
+
+  steps = [
+    {
+      name = "--version"
+      cmd  = "docker run --rm $IMAGE_NAME --version"
+    },
+  ]
+
+  labels = {
+    type = "container"
+  }
 }
