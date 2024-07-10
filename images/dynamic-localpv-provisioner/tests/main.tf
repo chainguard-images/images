58ff995ca8f89e20deb81ad23d52518366dd8696
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 20:19:27 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/dynamic-localpv-provisioner/tests/main.tf
+++ images/dynamic-localpv-provisioner/tests/main.tf
@@ -1,8 +1,11 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
@@ -10,13 +13,24 @@ variable "digest" {
 
 locals { parsed = provider::oci::parse(var.digest) }
 
-resource "helm_release" "dynamic-localpv-provisioner" {
-  name = "dynamic-localpv-provisioner"
+data "imagetest_inventory" "this" {}
 
-  repository = "https://openebs.github.io/dynamic-localpv-provisioner"
-  chart      = "localpv-provisioner"
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
 
-  values = [jsonencode({
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  repo  = "https://openebs.github.io/dynamic-localpv-provisioner"
+  chart = "localpv-provisioner"
+
+  values = {
     localpv = {
       image = {
         registry   = join("", [local.parsed.registry, "/"])
@@ -24,10 +38,22 @@ resource "helm_release" "dynamic-localpv-provisioner" {
         tag        = local.parsed.pseudo_tag
       }
     }
-  })]
+  }
 }
 
-module "helm_cleanup" {
-  source = "../../../tflib/helm-cleanup"
-  name   = helm_release.dynamic-localpv-provisioner.id
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Helm Install"
+      cmd  = module.helm.install_cmd
+    }
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
