89f0f245f9841f62881865e207bd6335c01494af
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-10 20:16:36 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/haproxy-ingress/tests/main.tf
+++ images/haproxy-ingress/tests/main.tf
@@ -1,9 +1,11 @@
 terraform {
   required_providers {
-    oci  = { source = "chainguard-dev/oci" }
-    helm = { source = "hashicorp/helm" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
@@ -11,27 +13,46 @@ variable "digest" {
 
 locals { parsed = provider::oci::parse(var.digest) }
 
-resource "helm_release" "haproxy-ingress" {
-  name = "haproxy-ingress"
+data "imagetest_inventory" "this" {}
 
-  repository = "https://haproxy-ingress.github.io/charts"
-  chart      = "haproxy-ingress"
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
 
-  namespace        = "haproxy-ingress"
-  create_namespace = true
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
 
-  values = [jsonencode({
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  repo  = "https://haproxy-ingress.github.io/charts"
+  chart = "haproxy-ingress"
+
+  values = {
     controller = {
       image = {
         repository = local.parsed.registry_repo
         tag        = local.parsed.pseudo_tag
       }
     }
-  })]
+  }
 }
 
-module "helm_cleanup" {
-  source    = "../../../tflib/helm-cleanup"
-  name      = helm_release.haproxy-ingress.id
-  namespace = helm_release.haproxy-ingress.namespace
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Helm Install"
+      cmd  = module.helm.install_cmd
+    }
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
