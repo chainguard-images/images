1791fbc66ddeebf70297f699aa6d2c6c548b5cfc
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-12 15:03:05 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/prometheus-statsd-exporter/tests/main.tf
+++ images/prometheus-statsd-exporter/tests/main.tf
@@ -1,36 +1,78 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "version" {
-  digest = var.digest
-  script = "docker run --rm $IMAGE_NAME --version"
-}
-
 locals { parsed = provider::oci::parse(var.digest) }
 
-resource "random_pet" "suffix" {}
+data "imagetest_inventory" "this" {}
 
-resource "helm_release" "test" {
-  name       = "prometheus-statsd-exporter-${random_pet.suffix.id}"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-statsd-exporter"
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
 
-  values = [jsonencode({
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  repo  = "https://prometheus-community.github.io/helm-charts"
+  chart = "prometheus-statsd-exporter"
+
+  values = {
     image = {
       repository = local.parsed.registry_repo
       tag        = local.parsed.pseudo_tag
     }
-  })]
+  }
 }
 
-module "helm_cleanup" {
-  source = "../../../tflib/helm-cleanup"
-  name   = helm_release.test.id
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Helm Install"
+      cmd  = module.helm.install_cmd
+    }
+  ]
+
+  labels = {
+    type = "k8s"
+  }
+}
+
+resource "imagetest_harness_docker" "docker" {
+  name      = "docker"
+  inventory = data.imagetest_inventory.this
+}
+
+resource "imagetest_feature" "image" {
+  name        = "image"
+  description = "Basic image test"
+  harness     = imagetest_harness_docker.docker
+
+  steps = [
+    {
+      name = "--version"
+      cmd  = "docker run --rm ${var.digest} --version"
+    },
+  ]
+
+  labels = {
+    type = "container"
+  }
 }
