bbc3076353e17c0537e9ff19787745db7e709cb1
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-29 18:41:53 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/pytorch-cuda12/tests/main.tf
+++ images/pytorch-cuda12/tests/main.tf
@@ -9,3 +9,28 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
+data "imagetest_inventory" "this" {
+}
+
+resource "imagetest_harness_docker" "this" {
+  inventory = data.imagetest_inventory.this
+  name      = "pytorch"
+}
+
+resource "imagetest_feature" "basic" {
+  description = "Placeholder"
+  harness     = imagetest_harness_docker.this
+  labels = {
+    type = "container"
+    # Group this image into a pytorch only shard
+    "shard::group" = "pytorch"
+  }
+  name = "Placeholder"
+  steps = [
+    {
+      name = "Placeholder"
+      cmd  = "exit 0"
+    },
+  ]
+}
+
