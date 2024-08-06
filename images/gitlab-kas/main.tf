4b58e22f8b45995b6edb4ed673208be8b10aedcd
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-06 02:32:09 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/gitlab-kas/main.tf
+++ images/gitlab-kas/main.tf
@@ -13,7 +13,7 @@ variable "target_repository" {
 }
 
 module "config" {
-  extra_packages = ["${local.name}-${each.value.version}"]
+  extra_packages = [each.key]
   for_each       = module.versions.versions
   source         = "./config"
 }
@@ -22,10 +22,11 @@ module "versioned" {
   build-dev         = true
   config            = module.config[each.key].config
   for_each          = module.versions.versions
-  main_package      = "${local.name}-${each.value.version}"
+  main_package      = each.value.main
   name              = basename(path.module)
   source            = "../../tflib/publisher"
   target_repository = var.target_repository
+  update-repo       = each.value.is_latest
 }
 
 module "test" {
