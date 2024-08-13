0b0d1f73feabe2d55d4d286d61bd661becc173d7
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-13 16:55:25 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ images/k3s/static.tf
@@ -0,0 +1,30 @@
+module "config-static" {
+  source         = "./config"
+  extra_packages = ["k3s-static"]
+}
+
+module "latest-static" {
+  source            = "../../tflib/publisher"
+  name              = basename(path.module)
+  target_repository = "${var.target_repository}-static"
+  config            = module.config-static.config
+  build-dev         = true
+  main_package      = "k3s-static"
+}
+
+# Statically linked checks are performed at the package level, so just verify
+# the same functionality as the regular image here.
+module "test-latest-static" {
+  source = "./tests"
+  digest = module.latest-static.image_ref
+}
+
+module "tagger-static" {
+  source     = "../../tflib/tagger"
+  depends_on = [module.test-latest-static]
+  tags = {
+    "latest" : module.latest-static.image_ref,
+    "latest-dev" : module.latest-static.dev_ref,
+  }
+}
+
