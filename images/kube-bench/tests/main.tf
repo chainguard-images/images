a2d6697243c3b8dfce5d94f140991a9ba2730a5b
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-15 18:44:25 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/kube-bench/tests/main.tf
+++ images/kube-bench/tests/main.tf
@@ -1,14 +1,54 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "run" {
-  digest = var.digest
-  script = "${path.module}/run.sh"
+data "imagetest_inventory" "this" {}
+
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
+
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation test"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Install kube-bench"
+      cmd  = <<EOF
+apk add curl yq
+
+tmp="$(mktemp -d)"
+
+# Retrieve the upstream-recommended job.yaml
+job_file="$tmp/job.yaml"
+curl -sSL -o "$job_file" 'https://raw.githubusercontent.com/aquasecurity/kube-bench/main/job.yaml'
+
+yq -i ".spec.template.spec.containers[0].image |= \"${var.digest}\"" "$job_file"
+
+kubectl apply -f "$job_file" -n default
+
+kubectl wait --for=condition=complete job.batch/kube-bench -n default --timeout=120s
+      EOF
+    }
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
--- images/kubernetes-csi-external-snapshot-controller/tests/main.tf
+++ images/kubernetes-csi-external-snapshot-controller/tests/main.tf
@@ -1,14 +1,53 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "deploy" {
-  digest = var.digest
-  script = "${path.module}/deploy.sh"
+data "imagetest_inventory" "this" {}
+
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
+
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation test"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Install"
+      cmd  = <<EOF
+version="v6.2.2"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$version/client/config/crd/snapshot.storage.k8s.io_volumesnapshots.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$version/client/config/crd/snapshot.storage.k8s.io_volumesnapshotclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$version/client/config/crd/snapshot.storage.k8s.io_volumesnapshotcontents.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$version/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml
+
+curl https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/$version/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml | \
+    sed "s|registry.k8s.io/sig-storage/snapshot-controller:.*|${var.digest}|g" | \
+    kubectl apply -f -
+
+kubectl rollout status deployment snapshot-controller -n kube-system --timeout=120s
+      EOF
+    }
+  ]
+
+  labels = {
+    type = "k8s"
+  }
 }
