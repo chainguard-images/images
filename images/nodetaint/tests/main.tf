204c3e79ed0f143b6561897eb72580aefdfed5d3
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-08-20 13:57:35 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/nodetaint/tests/main.tf
+++ images/nodetaint/tests/main.tf
@@ -1,8 +1,11 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
+
+variable "target_repository" {}
 
 variable "digest" {
   description = "The image digest to run tests over."
@@ -10,20 +13,48 @@ variable "digest" {
 
 locals { parsed = provider::oci::parse(var.digest) }
 
-data "oci_exec_test" "helm" {
-  digest = var.digest
-  script = "${path.module}/02-helm.sh"
+data "imagetest_inventory" "this" {}
 
-  env {
-    name  = "IMAGE_REGISTRY"
-    value = local.parsed.registry
+module "cluster_harness" {
+  source = "../../../tflib/imagetest/harnesses/k3s/"
+
+  inventory         = data.imagetest_inventory.this
+  name              = basename(path.module)
+  target_repository = var.target_repository
+  cwd               = path.module
+}
+
+module "helm" {
+  source = "../../../tflib/imagetest/helm"
+
+  name      = "nodetaint"
+  namespace = "nodetaint"
+  git_repo  = "https://github.com/wish/nodetaint"
+  chart     = "./chart"
+  patches   = ["$WORK/0001-use-v1-pod-disruption-budget.patch"]
+
+  values = {
+    image = {
+      registry   = "${local.parsed.registry}/"
+      repository = local.parsed.repo
+      tag        = local.parsed.pseudo_tag
+    }
   }
-  env {
-    name  = "IMAGE_REPOSITORY"
-    value = local.parsed.repo
-  }
-  env {
-    name  = "IMAGE_TAG"
-    value = local.parsed.pseudo_tag
+}
+
+resource "imagetest_feature" "basic" {
+  name        = "basic"
+  description = "Basic installation test"
+  harness     = module.cluster_harness.harness
+
+  steps = [
+    {
+      name = "Install"
+      cmd  = module.helm.install_cmd
+    },
+  ]
+
+  labels = {
+    type = "k8s"
   }
 }
