bdffbc296fcd99fa02d5d55464098824c72dac7e
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 11:21:30 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/tomcat/main.tf
+++ images/tomcat/main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+  }
+}
+
+data "apko_config" "package-info" {
+  config_contents = jsonencode({
+    contents = {
+      packages = ["tomcat"]
+    }
+  })
+}
+
+locals {
+  // This parses the package in the dummy config from where we extract the version
+  package_conf          = [for p in data.apko_config.package-info.config.contents.packages : p if startswith(p, "tomcat")][0]
+  package_version       = split("=", local.package_conf)[1]    // This extracts the full version
+  version_without_epoch = split("-", local.package_version)[0] // This extracts the version without the epoch
+  major_minor_version   = join(".", slice(split(".", local.version_without_epoch), 0, 2))
+}
+
 module "versions" {
   package = "tomcat"
   source  = "../../tflib/versions"
@@ -12,7 +34,7 @@ locals {
 }
 
 module "config" {
-  extra_packages = ["tomcat-${each.value.version}-openjdk-${local.latest_jdk}"]
+  extra_packages = ["tomcat-${local.major_minor_version}-openjdk-${local.latest_jdk}"]
   for_each       = module.versions.versions
   source         = "./config"
 }
