c5072f755e0e16f07675d6ffb692bcc297f2631e
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:58:04 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/wait-for-it/tests/01-ping-google.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o nounset -o errtrace -o pipefail -x -x
-
-docker run --rm "${IMAGE_NAME}" -h google.com -p 80
--- images/wait-for-it/tests/main.tf
+++ images/wait-for-it/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,7 +9,27 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "ping-google" {
-  digest = var.digest
-  script = "${path.module}/01-ping-google.sh"
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_docker" "this" {
+  name      = "wait-for-it"
+  inventory = data.imagetest_inventory.this
+
+  envs = {
+    "IMAGE_NAME" : var.digest
+  }
+}
+
+resource "imagetest_feature" "docker" {
+  name    = "Test waiting for it"
+  harness = imagetest_harness_docker.this
+
+  steps = [
+    {
+      name = "wait for google"
+      cmd  = <<EOF
+        set -o errexit -o nounset -o errtrace -o pipefail -x -x
+        docker run --rm $IMAGE_NAME -h google.com -p 80
+      EOF
+  }]
 }
