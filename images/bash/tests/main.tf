c938a3e4d46670c594eea73f1255b29cfdafa584
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-06 14:59:56 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/bash/tests/main.tf
+++ images/bash/tests/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    oci       = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,7 +9,27 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "runs" {
-  digest = var.digest
-  script = "${path.module}/runs.sh"
+data "imagetest_inventory" "this" {}
+
+resource "imagetest_harness_docker" "this" {
+  name      = "bash"
+  inventory = data.imagetest_inventory.this
+
+  envs = {
+    "IMAGE_NAME" : var.digest
+  }
+}
+
+resource "imagetest_feature" "docker" {
+  name    = "Test bash"
+  harness = imagetest_harness_docker.this
+
+  steps = [
+    {
+      name = "run a bash function"
+      cmd  = <<EOF
+        docker run --rm $IMAGE_NAME \
+          "function hello() { echo 'hello world'; }; hello" |grep "hello world"
+      EOF
+  }]
 }
--- images/bash/tests/runs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit -o nounset -o errtrace -o pipefail -x
-
-docker run --rm $IMAGE_NAME ls > /dev/null
-
-docker run --rm $IMAGE_NAME whoami | grep root
