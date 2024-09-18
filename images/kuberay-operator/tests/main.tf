c2b22c0f82a6199028ed31c845ef65905a99087b
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-18 13:05:13 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/kuberay-operator/tests/main.tf
+++ images/kuberay-operator/tests/main.tf
@@ -25,11 +25,11 @@ module "cluster_harness" {
 }
 
 module "helm" {
-  source = "../../../tflib/imagetest/helm"
-
+  source    = "../../../tflib/imagetest/helm"
+  name      = "kuberay-operator"
   repo      = "https://ray-project.github.io/kuberay-helm/"
   chart     = "kuberay-operator"
-  namespace = "kuberay-operator"
+  namespace = "kuberay-system"
 
   values = {
     image = {
@@ -37,6 +37,18 @@ module "helm" {
       tag        = local.parsed.pseudo_tag
     }
   }
+
+  timeout = "5m"
+}
+
+module "helm-ray-cluster" {
+  source    = "../../../tflib/imagetest/helm"
+  name      = "ray-cluster"
+  repo      = "https://ray-project.github.io/kuberay-helm/"
+  chart     = "ray-cluster"
+  namespace = "kuberay-system"
+
+  timeout = "10m"
 }
 
 resource "imagetest_feature" "basic" {
@@ -50,8 +62,34 @@ resource "imagetest_feature" "basic" {
       cmd  = module.helm.install_cmd
     },
     {
-      name = "Pods are running"
-      cmd  = "kubectl wait --for=jsonpath='{.status.phase}'=Running --all=true --namespace=kuberay-operator pod"
+      name = "Helm Install RayCluster"
+      cmd  = module.helm-ray-cluster.install_cmd
+    },
+    {
+      name = "Let's wait until a RayCluster starts"
+      cmd  = <<EOF
+      kubectl get pods --selector=ray.io/cluster=ray-cluster-kuberay -n kuberay-system
+      kubectl wait --for=condition=Ready pod --selector=ray.io/cluster=ray-cluster-kuberay --timeout 600s -n kuberay-system
+EOF
+    },
+    {
+      name = "Execute a Ray job in the head Pod"
+      cmd  = <<EOF
+      export HEAD_POD=$(kubectl get pods -n kuberay-system --selector=ray.io/node-type=head -o custom-columns=POD:metadata.name --no-headers)
+      echo $HEAD_POD
+
+      kubectl exec -n kuberay-system -it $HEAD_POD -- python -c "import ray; ray.init(); print(ray.cluster_resources())"
+      sleep 3
+      logs=$(kubectl logs --tail=-1 -n kuberay-system $HEAD_POD)
+
+      if echo "$logs" | grep -iq 'Ray runtime started.'; then
+        echo "Ray cluster connected successfully."
+      else
+        echo "Failed to connect to Ray cluster."
+        echo "actual logs: $logs"
+        exit 1
+      fi
+EOF
     }
   ]
 
