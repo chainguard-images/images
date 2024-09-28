458bad924cb57212b4e746242a40df702d9d74d8
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-09-28 10:14:38 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/kuberay-operator/config/main.tf
+++ images/kuberay-operator/config/main.tf
@@ -20,9 +20,7 @@ variable "extra_packages" {
 output "config" {
   value = jsonencode({
     "contents" : {
-      // TODO: remove the need for using hardcoded local.baseline_packages by plumbing
-      // these packages through var.extra_packages in all callers of this config module
-      "packages" : distinct(concat(local.baseline_packages, var.extra_packages))
+      "packages" : var.extra_packages
     },
     "entrypoint" : {
       "command" : "manager"
