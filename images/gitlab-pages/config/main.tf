88927e6cf5d1179a09e8f4b04bc930aec9f52b2c
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2024-10-12 13:38:28 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/gitlab-pages/config/main.tf
+++ images/gitlab-pages/config/main.tf
@@ -18,23 +18,41 @@ output "config" {
       packages = var.extra_packages
     }
     accounts = module.accts.block
+    environment = {
+      "HOME"                      = "/home/git"
+      "LANG"                      = "C.UTF-8"
+      "CONFIG_TEMPLATE_DIRECTORY" = "/etc/gitlab-pages"
+    }
     entrypoint = {
-      command = "/scripts/start-pages"
+      command = "/scripts/entrypoint.sh"
     }
     paths = [{
       path        = "/etc/gitlab-pages"
       type        = "directory"
       permissions = 493
+      uid         = module.accts.uid
+      gid         = module.accts.gid
       recursive   = true
       }, {
       path        = "/srv/gitlab-pages"
       type        = "directory"
       permissions = 493
+      uid         = module.accts.uid
+      gid         = module.accts.gid
       recursive   = true
       }, {
       path        = "/var/log/gitlab-pages"
       type        = "directory"
       permissions = 493
+      uid         = module.accts.uid
+      gid         = module.accts.gid
+      recursive   = true
+      }, {
+      path        = "/home/git"
+      type        = "directory"
+      permissions = 493
+      uid         = module.accts.uid
+      gid         = module.accts.gid
       recursive   = true
     }]
   })
