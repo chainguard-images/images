c8751f72e921ff4438c6eb31bec4ae34ff09b4c7
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-04 19:47:31 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/wolfi-base/tests/main.tf
+++ images/wolfi-base/tests/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    oci = { source = "chainguard-dev/oci" }
+    imagetest = { source = "chainguard-dev/imagetest" }
   }
 }
 
@@ -8,7 +8,24 @@ variable "digest" {
   description = "The image digest to run tests over."
 }
 
-data "oci_exec_test" "runs" {
-  digest = var.digest
-  script = "${path.module}/runs.sh"
+variable "target_repository" {}
+
+module "bash_sandbox" {
+  source            = "../../../../tflib/imagetest/sandboxes/bash"
+  target_repository = var.target_repository
+}
+
+module "dind_test" {
+  source = "../../../../tflib/imagetest/tests/docker-in-docker"
+
+  images = { "chainguard-base" = var.digest }
+
+  tests = [
+    {
+      name    = "wolfi-base-tests"
+      image   = module.bash_sandbox.image_ref
+      content = [{ source = path.module }]
+      cmd     = "./test.sh"
+    }
+  ]
 }
