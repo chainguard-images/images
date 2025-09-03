42122261c13cdb788212de74904f735a49fd5c9e
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2025-09-03 15:11:10 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- tflib/publisher/chainguard.tf
+++ tflib/publisher/chainguard.tf
@@ -18,9 +18,9 @@ locals {
   metadata_filepath = "images/${var.name}/metadata.yaml"
   metadata_public   = fileexists(local.metadata_filepath) ? yamldecode(file(local.metadata_filepath)) : null
 
-  keywords = local.metadata_public != null ? local.metadata_public.keywords : []
-  aliases  = local.metadata_public != null ? lookup(local.metadata_public, "aliases", []) : []
-
+  keywords     = local.metadata_public != null ? local.metadata_public.keywords : []
+  aliases      = local.metadata_public != null ? lookup(local.metadata_public, "aliases", []) : []
+  catalog_tier = local.metadata_public != null ? lookup(local.metadata_public, "tier", "BASE") : "BASE"
 
   // If the repo name ends with "-fips", add "fips" as a keyword (if not already present)
   keywords_updated = (endswith(local.repo_name, "-fips") && !contains(local.keywords, "fips")) ? concat(local.keywords, ["fips"]) : local.keywords
@@ -35,14 +35,26 @@ data "chainguard_group" "group" {
   name  = local.group
 }
 
+// Look up the repo so that we don't lose active_tags.
+// Note that this is using the plural source to allow for the repo to not exist yet.
+data "chainguard_image_repos" "existing" {
+  count     = local.should_run_repo_update ? 1 : 0
+  parent_id = local.group_is_uidp ? local.group : (local.should_run_group_lookup ? data.chainguard_group.group[0].id : local.groups[local.group])
+  name      = local.repo_name
+}
+
 // Create or update the repo metadata.
-resource "chainguard_image_repo" "repo" {
-  count = local.should_run_repo_update ? 1 : 0
-  // Use either the known group ID, or the one we looked up.
+module "repo" {
+  source = "../../tflib/repo"
+  count  = local.should_run_repo_update ? 1 : 0
+
+  // If the group is a UIDP, use it directly. Otherwise use known value or value from the group lookup
   parent_id = local.group_is_uidp ? local.group : (local.should_run_group_lookup ? data.chainguard_group.group[0].id : local.groups[local.group])
   readme    = file(local.readme_filepath)
   name      = local.repo_name
   // keywords in the metadata file maps to bundles in the Chainguard API
-  bundles = local.keywords_updated
-  aliases = local.aliases
+  bundles     = local.keywords_updated
+  tier        = local.catalog_tier
+  aliases     = local.aliases
+  active_tags = length(data.chainguard_image_repos.existing) > 0 && length(data.chainguard_image_repos.existing[0].items) > 0 ? data.chainguard_image_repos.existing[0].items[0].active_tags : [] // Use existing active tags if available, otherwise start with an empty list
 }
--- tflib/tagger/chainguard.tf
+++ tflib/tagger/chainguard.tf
@@ -1,10 +1,6 @@
 // This config updates Chainguard repo metadata if the target repository is
 // a Chainguard repo. Otherwise (e.g., ttl.sh, localhost), it does nothing.
 //
-// When testing this locally, make sure you set the target_repository to a
-// URL with the UIDP rather than the name, i.e. cgr.dev/2d727bdf1a624e9ed5606a2ff4bb3f7c2f83ee06
-// for chainguard-eng. Also note to run "make enable-active-tag-update".
-//
 // NOTE: If you find yourself changing the group stuff below, make sure to keep this aligned
 // with publisher/chainguard.tf.
 locals {
@@ -12,22 +8,13 @@ locals {
   group     = local.parts[1]
   repo_name = local.parts[length(local.parts) - 1]
 
-  is_chainguard_prod    = startswith(local.repo, "cgr.dev/")
-  is_chainguard_staging = startswith(local.repo, "chainreg.biz/")
+  is_chainguard = startswith(local.repo, "cgr.dev/")
 
   // Attempt to determine if the group looks like a valid UIDP
   group_is_uidp = length(regexall("^[a-f0-9]{40}$", local.group)) > 0
 
-  // These can be found here: https://github.com/chainguard-dev/mono/blob/main/env/enforce.dev/registry-config.yaml
-  groups_prod = {
-    "chainguard-private" = "ce2d1984a010471142503340d670612d63ffb9f6"
-    "custom-images"      = "8d00810b0a029559e52399855031958ef48be6c8"
-  }
-
-  // These can be found here: https://github.com/chainguard-dev/mono/blob/main/env/chainops.dev/registry-config.yaml
-  groups_staging = {
-    "chainguard-private" = "e2709b80f26b43cf0b8a30d75e8a19815560d206"
-    "custom-images"      = "8e32b609b7021a0c931a4d97041ddd611e20dc54"
+  groups = {
+    "chainguard" : "720909c9f5279097d847ad02a2f24ba8f59de36a",
   }
 }
 
@@ -36,7 +23,7 @@ locals {
 // If not, that's a bug we should look out for.
 data "chainguard_image_repo" "repo" {
   count     = var.update_active_tags ? 1 : 0
-  parent_id = local.group_is_uidp ? local.group : (local.is_chainguard_prod ? local.groups_prod[local.group] : local.groups_staging[local.group])
+  parent_id = local.group_is_uidp ? local.group : local.groups[local.group]
   name      = local.repo_name
 }
 
@@ -46,7 +33,7 @@ module "repo" {
   count      = var.update_active_tags ? 1 : 0
   depends_on = [oci_tags.this] # Only update if the tags got successfully pushed.
 
-  parent_id   = local.group_is_uidp ? local.group : (local.is_chainguard_prod ? local.groups_prod[local.group] : local.groups_staging[local.group])
+  parent_id   = local.group_is_uidp ? local.group : local.groups[local.group]
   name        = data.chainguard_image_repo.repo[0].name
   readme      = data.chainguard_image_repo.repo[0].items[0].readme
   bundles     = data.chainguard_image_repo.repo[0].items[0].bundles
