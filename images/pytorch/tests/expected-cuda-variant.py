bf6eececa34eb1046744a402aebf0f955d15e0e3
Public copy <41898282+github-actions[bot]@users.noreply.github.com>
2026-04-15 11:15:38 +0000

    automated commit
    Signed-off-by: Public copy <41898282+github-actions[bot]@users.noreply.github.com>

--- images/pytorch/tests/expected-cuda-variant.py
+++ images/pytorch/tests/expected-cuda-variant.py
@@ -7,13 +7,14 @@ import sys
 import torch
 
 # pytorch - cuda mapping can be found in the dockerhub image source, eg:
-# https://hub.docker.com/layers/pytorch/pytorch/2.10.0-cuda12.8-cudnn9-runtime/images/sha256-b85566342b86d13a67712e9315d40cdc2dad7f8d86df1aff3831f80835edbcca
+# https://hub.docker.com/layers/pytorch/pytorch/2.11.0-cuda12.8-cudnn9-runtime/images/sha256-eee11b3b3872a8c838e35ef48f08b2d5def2080902c7f666831310ca1a0ef2be
 PyTorchLatestStableCudaVariants = [
     ('2.6.', '+cu118'), # for request-5190 arm64
     ('2.7.', '+cu126'), # CUDA 12.6
     ('2.8.', '+cu128'), # CUDA 12.8
     ('2.9.', '+cu128'), # CUDA 12.8
     ('2.10.', '+cu128'), # CUDA 12.8
+    ('2.11.', '+cu128'), # CUDA 12.8
 ]
 
 if __name__ == '__main__':
