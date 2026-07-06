01e1bf13af35015b53f0d10a10858112a7feebaf
Sync public container images <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-06 16:07:02 +0000

    Update images and build configurations
    Signed-off-by: Sync public container images <41898282+github-actions[bot]@users.noreply.github.com>

--- tflib/publisher/providers.tf
+++ tflib/publisher/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     cosign     = { source = "chainguard-dev/cosign", version = "0.4.5" }
-    apko       = { source = "chainguard-dev/apko", version = "1.2.11" }
+    apko       = { source = "chainguard-dev/apko", version = "1.2.14" }
     oci        = { source = "chainguard-dev/oci", version = "0.1.6" }
     chainguard = { source = "chainguard-dev/chainguard", version = "0.2.13" }
     imagetest  = { source = "chainguard-dev/imagetest", version = "0.2.0" }
